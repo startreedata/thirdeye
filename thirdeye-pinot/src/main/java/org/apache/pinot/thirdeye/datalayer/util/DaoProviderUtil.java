24cba99c3d232ae70427a36b6f47add78a2e6458
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2020-10-07 12:16:08 -0700

    DaoProviderUtil refactor. Extracted DataSourceBuilder class.
    This will be used by the coordinator module.

--- thirdeye-pinot/src/main/java/org/apache/pinot/thirdeye/datalayer/util/DaoProviderUtil.java
+++ thirdeye-pinot/src/main/java/org/apache/pinot/thirdeye/datalayer/util/DaoProviderUtil.java
@@ -24,86 +24,31 @@ import com.google.inject.Injector;
 import io.dropwizard.configuration.YamlConfigurationFactory;
 import io.dropwizard.jackson.Jackson;
 import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.sql.Connection;
 import javax.validation.Validation;
-import org.apache.pinot.thirdeye.datalayer.ScriptRunner;
+import org.apache.pinot.thirdeye.datalayer.DataSourceBuilder;
 import org.apache.pinot.thirdeye.datalayer.ThirdEyePersistenceModule;
 import org.apache.pinot.thirdeye.datalayer.bao.jdbc.AbstractManagerImpl;
 import org.apache.pinot.thirdeye.datalayer.dto.AbstractDTO;
 import org.apache.pinot.thirdeye.datalayer.util.PersistenceConfig.DatabaseConfiguration;
 import org.apache.tomcat.jdbc.pool.DataSource;
-import org.h2.store.fs.FileUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public abstract class DaoProviderUtil {
-
-  private static final Logger LOG = LoggerFactory.getLogger(DaoProviderUtil.class);
-
-  private static final String DEFAULT_DATABASE_PATH = "jdbc:h2:./config/h2db";
-  private static final String DEFAULT_DATABASE_FILE = "./config/h2db.mv.db";
 
   private static Injector injector;
 
   public static void init(File localConfigFile) {
     final PersistenceConfig configuration = readPersistenceConfig(localConfigFile);
-    final DataSource dataSource = createDataSource(configuration);
+    final DatabaseConfiguration dbConfig = configuration.getDatabaseConfiguration();
 
-    // create schema for default database
-    createSchemaIfReqd(dataSource, configuration.getDatabaseConfiguration());
-
-    init(dataSource);
-  }
-
-  private static void createSchemaIfReqd(
-      final DataSource dataSource,
-      final DatabaseConfiguration dbConfig) {
-    if (dbConfig.getUrl().equals(DEFAULT_DATABASE_PATH)
-        && !FileUtils.exists(DEFAULT_DATABASE_FILE)) {
-      try {
-        LOG.info("Creating database schema for default URL '{}'", DEFAULT_DATABASE_PATH);
-        Connection conn = dataSource.getConnection();
-        final ScriptRunner scriptRunner = new ScriptRunner(conn, false);
-        scriptRunner.setDelimiter(";");
-
-        InputStream createSchema = DaoProviderUtil.class
-            .getResourceAsStream("/schema/create-schema.sql");
-        scriptRunner.runScript(new InputStreamReader(createSchema));
-      } catch (Exception e) {
-        LOG.error("Could not create database schema. Attempting to use existing.", e);
-      }
-    } else {
-      LOG.info("Using existing database at '{}'", dbConfig.getUrl());
-    }
-  }
-
-  private static DataSource createDataSource(final PersistenceConfig configuration) {
-    final DataSource dataSource = new DataSource();
-    dataSource.setInitialSize(10);
-    dataSource.setDefaultAutoCommit(false);
-    dataSource.setMaxActive(100);
-    dataSource.setUsername(configuration.getDatabaseConfiguration().getUser());
-    dataSource.setPassword(configuration.getDatabaseConfiguration().getPassword());
-    dataSource.setUrl(configuration.getDatabaseConfiguration().getUrl());
-    dataSource.setDriverClassName(configuration.getDatabaseConfiguration().getDriver());
-
-    dataSource.setValidationQuery("select 1");
-    dataSource.setTestWhileIdle(true);
-    dataSource.setTestOnBorrow(true);
-    // when returning connection to pool
-    dataSource.setTestOnReturn(true);
-    dataSource.setRollbackOnReturn(true);
-
-    // Timeout before an abandoned(in use) connection can be removed.
-    dataSource.setRemoveAbandonedTimeout(600_000);
-    dataSource.setRemoveAbandoned(true);
-    return dataSource;
+    init(new DataSourceBuilder().build(dbConfig));
   }
 
   public static void init(DataSource dataSource) {
     injector = Guice.createInjector(new ThirdEyePersistenceModule(dataSource));
+  }
+
+  public static <T extends AbstractManagerImpl<? extends AbstractDTO>> T getInstance(Class<T> c) {
+    return injector.getInstance(c);
   }
 
   public static PersistenceConfig readPersistenceConfig(File configFile) {
@@ -117,9 +62,5 @@ public abstract class DaoProviderUtil {
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
-  }
-
-  public static <T extends AbstractManagerImpl<? extends AbstractDTO>> T getInstance(Class<T> c) {
-    return injector.getInstance(c);
   }
 }

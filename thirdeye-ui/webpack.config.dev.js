9592d95652dd9b5518f8776f84b3a8ab44d5044f
Gaurav Joshi <68965367+joshigaurava@users.noreply.github.com>
2020-10-28 15:03:47 -0700

    Webpack refactoring (#2)

--- thirdeye-ui/webpack.config.dev.js
+++ thirdeye-ui/webpack.config.dev.js
@@ -1,34 +1,152 @@
 const path = require("path");
-const webpackConfigBase = require("./webpack.config.base");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
-// Get basic webpack configuration object
-const webpackConfig = webpackConfigBase.getWebpackConfigObject(
-    path.join(__dirname, "src/app/index.tsx"),
-    path.join(__dirname, "dist"),
-    "thirdeye-ui",
-    path.join(__dirname, "node_modules"),
-    true
-);
+module.exports = {
+    // Input configuration
+    entry: path.join(__dirname, "src/app/index.tsx"),
 
-// Set up a distributable index.html output along with other relevant assets (favicon, logo and
-// manifest)
-webpackConfigBase.setupDistributableIndexHtml(
-    webpackConfig,
-    path.join(__dirname, "src/public/index.html"),
-    path.join(__dirname, "src/public/favicon.ico"),
-    path.join(__dirname, "src/public/cortex-data-512x512.png"),
-    path.join(__dirname, "src/public/manifest.json")
-);
+    // Output configuration
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "thirdeye-ui.js",
+        publicPath: "/", // Ensures bundle is served from absolute path as opposed to relative
+        // Development mode optimizations
+        pathinfo: false,
+    },
 
-// Set up webpack-dev-server proxy
-webpackConfigBase.setupWebpackDevServerProxy(
-    webpackConfig,
-    "/api",
-    "http://localhost:8080"
-);
+    // Loaders
+    module: {
+        rules: [
+            {
+                // .ts and .tsx files to be handled by ts-loader
+                test: /\.(ts|tsx)$/,
+                loader: "ts-loader",
+                options: {
+                    // Speed up compilation in development mode
+                    transpileOnly: true,
+                },
+                exclude: /node_modules/, // Just the source code
+            },
+            {
+                // .css and .scss files to be handled by sass-loader
+                test: /\.(css|scss)$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+                // Missing exclude, may need to handle files outside the source code
+                // (from node_modules)
+            },
+            {
+                // .svg files to be handled by @svgr/webpack
+                test: /\.svg$/,
+                use: ["@svgr/webpack", "url-loader"],
+                exclude: /node_modules/, // Just the source code
+            },
+            {
+                // font files to be handled by file-loader
+                test: /\.(woff|woff2|ttf)([\?]?.*)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "fonts/",
+                        },
+                    },
+                ],
+                exclude: /node_modules/, // Just the source code
+            },
+        ],
+    },
 
-// Get webpack configuration function to configure webpack in development mode
-module.exports = webpackConfigBase.getWebpackConfigDevFunction(
-    webpackConfig,
-    7004
-);
+    // Configure how modules are resolved
+    resolve: {
+        // File types to be handled
+        extensions: [
+            ".ts",
+            ".tsx",
+            ".js",
+            ".css",
+            ".scss",
+            ".svg",
+            ".woff",
+            ".woff2",
+            ".ttf",
+        ],
+    },
+
+    // Plugins
+    plugins: [
+        // Clean webpack output directory
+        new CleanWebpackPlugin({
+            verbose: true,
+        }),
+        // In development mode, fork TypeScript checking to run in another thread and not block
+        // main transpilation
+        new ForkTsCheckerWebpackPlugin({
+            eslint: {
+                files: "./src/**/*.{ts,tsx}", // NOTE: this was "files: "./src/**/*.{ts,tsx,js,jsx}","
+            },
+        }),
+        // Generate index.html from template
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src/public/index.html"),
+            scriptLoading: "defer",
+        }),
+        // Copy favicon, logo and manifest for index.html
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.join(__dirname, "src/public/favicon.ico"),
+                    to: path.join(__dirname, "dist"),
+                },
+                {
+                    from: path.join(
+                        __dirname,
+                        "src/public/cortex-data-512x512.png"
+                    ),
+                    to: path.join(__dirname, "dist"),
+                },
+                {
+                    from: path.join(__dirname, "src/public/manifest.json"),
+                    to: path.join(__dirname, "dist"),
+                },
+            ],
+        }),
+    ],
+
+    // Setup webpack-dev-server
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 7004,
+        // Required to route all requests to index.html so that React router gets to handle all
+        // copy pasted deep links
+        historyApiFallback: { disableDotRule: true },
+
+        // Setup webpack-dev-server proxy context and target
+        proxy: [
+            {
+                context: "/api",
+                target: "http://localhost:8080",
+                changeOrigin: true,
+            },
+        ],
+    },
+
+    // Development mode
+    mode: "development",
+
+    // Use eval-cheap-module-source-map for faster rebuilds
+    devtool: "eval-cheap-module-source-map",
+
+    // Development mode optimizations
+
+    optimization: {
+        runtimeChunk: true,
+        removeAvailableModules: false,
+        removeEmptyChunks: false,
+        splitChunks: false,
+    },
+};
--- thirdeye-ui/webpack.config.prod.js
+++ thirdeye-ui/webpack.config.prod.js
@@ -1,24 +1,111 @@
 const path = require("path");
-const webpackConfigBase = require("./webpack.config.base");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
-// Get basic webpack configuration object
-const webpackConfig = webpackConfigBase.getWebpackConfigObject(
-    path.join(__dirname, "src/app/index.tsx"),
-    path.join(__dirname, "dist"),
-    "thirdeye-ui",
-    path.join(__dirname, "node_modules"),
-    false
-);
+module.exports = {
+    // Input configuration
+    entry: path.join(__dirname, "src/app/index.tsx"),
 
-// Set up a distributable index.html output along with other relevant assets (favicon, logo and
-// manifest)
-webpackConfigBase.setupDistributableIndexHtml(
-    webpackConfig,
-    path.join(__dirname, "src/public/index.html"),
-    path.join(__dirname, "src/public/favicon.ico"),
-    path.join(__dirname, "src/public/cortex-data-512x512.png"),
-    path.join(__dirname, "src/public/manifest.json")
-);
+    // Output configuration
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "thirdeye-ui.js",
+        publicPath: "/", // Ensures bundle is served from absolute path as opposed to relative
+    },
 
-// Get webpack configuration function to configure webpack in production mode
-module.exports = webpackConfigBase.getWebpackConfigProdFunction(webpackConfig);
+    // Loaders
+    module: {
+        rules: [
+            {
+                // .ts and .tsx files to be handled by ts-loader
+                test: /\.(ts|tsx)$/,
+                loader: "ts-loader",
+                exclude: /node_modules/, // Just the source code
+            },
+            {
+                // .css and .scss files to be handled by sass-loader
+                test: /\.(css|scss)$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+                // Missing exclude, may need to handle files outside the source code
+                // (from node_modules)
+            },
+            {
+                // .svg files to be handled by @svgr/webpack
+                test: /\.svg$/,
+                use: ["@svgr/webpack", "url-loader"],
+                exclude: /node_modules/, // Just the source code
+            },
+            {
+                // font files to be handled by file-loader
+                test: /\.(woff|woff2|ttf)([\?]?.*)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "fonts/",
+                        },
+                    },
+                ],
+                exclude: /node_modules/, // Just the source code
+            },
+        ],
+    },
+
+    // Configure how modules are resolved
+    resolve: {
+        // File types to be handled
+        extensions: [
+            ".ts",
+            ".tsx",
+            ".js",
+            ".css",
+            ".scss",
+            ".svg",
+            ".woff",
+            ".woff2",
+            ".ttf",
+        ],
+    },
+
+    // Plugins
+    plugins: [
+        // Clean webpack output directory
+        new CleanWebpackPlugin({
+            verbose: true,
+        }),
+        // Generate index.html from template
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src/public/index.html"),
+            scriptLoading: "defer",
+        }),
+        // Copy favicon, logo and manifest for index.html
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.join(__dirname, "src/public/favicon.ico"),
+                    to: path.join(__dirname, "dist"),
+                },
+                {
+                    from: path.join(
+                        __dirname,
+                        "src/public/cortex-data-512x512.png"
+                    ),
+                    to: path.join(__dirname, "dist"),
+                },
+                {
+                    from: path.join(__dirname, "src/public/manifest.json"),
+                    to: path.join(__dirname, "dist"),
+                },
+            ],
+        }),
+    ],
+
+    // Production mode
+    mode: "production",
+
+    // Include cheap source maps for potential debugging
+    devtool: "cheap-source-map",
+};

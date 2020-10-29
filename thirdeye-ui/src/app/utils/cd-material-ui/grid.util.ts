d5124a6474faed8630ee79b27854aae14b7b5a4e
Gaurav Joshi <68965367+joshigaurava@users.noreply.github.com>
2020-10-29 14:43:42 -0700

    Material UI setup (#8)

---
+++ thirdeye-ui/src/app/utils/cd-material-ui/grid.util.ts
@@ -0,0 +1,6 @@
+import { GridProps } from "@material-ui/core";
+
+// CortexData Material-UI theme property overrides for Grid
+export const gridProps = <Partial<GridProps>>{
+    spacing: 2,
+};
---
+++ thirdeye-ui/src/app/utils/cd-material-ui/shadow.util.ts
@@ -0,0 +1,5 @@
+// CortexData Material-UI theme boxShadows
+export const Shadow = {
+    BOX_SHADOW_APP_BAR:
+        "0px 8px 8px rgba(0, 0, 0, 0.04), 0px 16px 22px rgba(0, 0, 0, 0.04), 0px 8px 26px rgba(41, 148, 169, 0.1), 0px 0px 2px rgba(110, 196, 209, 0.12)",
+} as const;

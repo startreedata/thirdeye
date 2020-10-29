650f4f033bb94ef47b594df8f104501e8f0a95cb
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2020-10-29 15:28:04 -0700

    [core] Added utils to report statuses to client

--- thirdeye-core/src/main/java/org/apache/pinot/thirdeye/resources/ResourceUtils.java
+++ thirdeye-core/src/main/java/org/apache/pinot/thirdeye/resources/ResourceUtils.java
@@ -1,7 +1,13 @@
 package org.apache.pinot.thirdeye.resources;
 
+import com.google.common.collect.ImmutableList;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import org.apache.pinot.thirdeye.ThirdEyeStatus;
+import org.apache.pinot.thirdeye.api.StatusApi;
+import org.apache.pinot.thirdeye.api.StatusListApi;
 
 public class ResourceUtils {
 
@@ -21,6 +27,12 @@ public class ResourceUtils {
     }
   }
 
+  public static void ensure(boolean condition, ThirdEyeStatus status) {
+    if (!condition) {
+      throw badRequest(status);
+    }
+  }
+
   public static <T> T ensureExists(T o, String message) {
     ensure(o != null, message);
     return o;
@@ -28,5 +40,17 @@ public class ResourceUtils {
 
   public static void ensureNull(Object o, String message) {
     ensure(o == null, message);
+  }
+
+  public static StatusListApi statusResponse(ThirdEyeStatus status) {
+    return new StatusListApi().setList(ImmutableList.of(new StatusApi(status)));
+  }
+
+  public static BadRequestException badRequest(ThirdEyeStatus status) {
+    return new BadRequestException(Response
+        .status(Status.BAD_REQUEST)
+        .entity(statusResponse(status))
+        .build()
+    );
   }
 }

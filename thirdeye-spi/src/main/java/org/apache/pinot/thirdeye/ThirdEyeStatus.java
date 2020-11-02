0270395cac650a967e6da5cd87c57dcfe4bf7a07
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2020-11-02 15:49:33 -0800

    [spi] Updated ThirdEyeStatus with more entries

--- thirdeye-spi/src/main/java/org/apache/pinot/thirdeye/ThirdEyeStatus.java
+++ thirdeye-spi/src/main/java/org/apache/pinot/thirdeye/ThirdEyeStatus.java
@@ -2,7 +2,9 @@ package org.apache.pinot.thirdeye;
 
 public enum ThirdEyeStatus {
 
-  ERR_DUPLICATE_NAME("Name must be unique!");
+  ERR_DUPLICATE_NAME("Name must be unique!"),
+  ERR_OBJECT_DOES_NOT_EXIST("Object does not exist!"),
+  ERR_UNKNOWN("Unknown Error!");
 
   final String message;
 

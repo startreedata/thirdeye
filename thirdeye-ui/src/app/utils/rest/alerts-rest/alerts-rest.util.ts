b3f6587ff645bb3d579c4159b68edb484e7beafd
Gaurav Joshi <68965367+joshigaurava@users.noreply.github.com>
2020-11-04 18:26:57 -0800

    Create Alert API

--- thirdeye-ui/src/app/utils/rest/alerts-rest/alerts-rest.util.ts
+++ thirdeye-ui/src/app/utils/rest/alerts-rest/alerts-rest.util.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+import yaml from "js-yaml";
 import useSWR, { responseInterface } from "swr";
 import { Alert } from "./alerts-rest.interfaces";
 
@@ -9,4 +11,8 @@ export const useAllAlerts = (): responseInterface<Alert[], Error> => {
 
 export const useAlert = (id: number): responseInterface<Alert, Error> => {
     return useSWR(`${BASE_URL_ALERTS}/${id}`);
+};
+
+export const createAlert = async (alertYaml: string): Promise<void> => {
+    await axios.post(BASE_URL_ALERTS, yaml.safeLoad(alertYaml));
 };

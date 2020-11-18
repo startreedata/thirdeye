612fbf9a0a6efce2a881fc3a00b6d576611b5c26
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2020-11-17 20:34:28 -0800

    [spi] ThirdEyeStatus. Added ERR_ID_UNEXPECTED_AT_CREATION

--- thirdeye-spi/src/main/java/org/apache/pinot/thirdeye/ThirdEyeStatus.java
+++ thirdeye-spi/src/main/java/org/apache/pinot/thirdeye/ThirdEyeStatus.java
@@ -4,6 +4,7 @@ public enum ThirdEyeStatus {
 
   ERR_DUPLICATE_NAME("Name must be unique!"),
   ERR_MISSING_ID("ID is null!"),
+  ERR_ID_UNEXPECTED_AT_CREATION("ID should be null at creation time."),
   ERR_OBJECT_UNEXPECTED("Object should be null/empty! %s"),
   ERR_OBJECT_DOES_NOT_EXIST("Object does not exist! %s"),
   ERR_UNKNOWN("Untagged Error! %s");

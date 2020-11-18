d3ad6ff734b61bce4c5dc7cae5fa9ef34085bf92
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2020-11-17 20:07:28 -0800

    [spi] Cosmetic. Updated Error msg for ERR_MISSING_ID

--- thirdeye-spi/src/main/java/org/apache/pinot/thirdeye/ThirdEyeStatus.java
+++ thirdeye-spi/src/main/java/org/apache/pinot/thirdeye/ThirdEyeStatus.java
@@ -3,7 +3,7 @@ package org.apache.pinot.thirdeye;
 public enum ThirdEyeStatus {
 
   ERR_DUPLICATE_NAME("Name must be unique!"),
-  ERR_MISSING_ID("ID is missing from the entity!"),
+  ERR_MISSING_ID("ID is null!"),
   ERR_OBJECT_UNEXPECTED("Object should be null/empty! %s"),
   ERR_OBJECT_DOES_NOT_EXIST("Object does not exist! %s"),
   ERR_UNKNOWN("Untagged Error! %s");

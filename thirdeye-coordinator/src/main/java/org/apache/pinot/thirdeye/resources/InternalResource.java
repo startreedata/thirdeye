064c04e437eec6936ae31abc5a38d812a8787dab
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2020-11-19 14:27:35 -0800

    [coordinator] Cosmetic. Removed Internal Resource dummy /GET. updated swagger tag

--- thirdeye-coordinator/src/main/java/org/apache/pinot/thirdeye/resources/InternalResource.java
+++ thirdeye-coordinator/src/main/java/org/apache/pinot/thirdeye/resources/InternalResource.java
@@ -2,15 +2,13 @@ package org.apache.pinot.thirdeye.resources;
 
 import io.swagger.annotations.Api;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import org.apache.pinot.thirdeye.datasource.pinot.resources.PinotDataSourceResource;
 
 @Produces(MediaType.APPLICATION_JSON)
-@Api(tags = "Internal")
+@Api(tags = "zzz Internal zzz")
 public class InternalResource {
 
   private final PinotDataSourceResource pinotDataSourceResource;
@@ -18,13 +16,6 @@ public class InternalResource {
   @Inject
   public InternalResource(final PinotDataSourceResource pinotDataSourceResource) {
     this.pinotDataSourceResource = pinotDataSourceResource;
-  }
-
-  @GET
-  public Response home() {
-    return Response
-        .ok("ThirdEye Coordinator is up and running.")
-        .build();
   }
 
   @Path("pinot-data-source")

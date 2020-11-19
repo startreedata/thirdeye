c6618b61ebeeaa5b8c731c7260c418acf2b3f3fd
Gaurav Joshi <68965367+joshigaurava@users.noreply.github.com>
2020-11-19 15:01:41 -0800

    [UI] Revert delete from previous PR (#49) (#50)

---
+++ thirdeye-ui/src/app/components/breadcrumbs/breadcrumbs.styles.ts
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core";
+
+export const breadcrumbsStyles = makeStyles(() => {
+    return {
+        appBar: {
+            background: "#fff",
+            borderBottom: "1px solid #BDBDBD",
+            boxShadow: "none",
+        },
+    };
+});

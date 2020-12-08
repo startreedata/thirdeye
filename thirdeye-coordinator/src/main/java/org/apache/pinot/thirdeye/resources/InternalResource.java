c016a9c3dd2f08908b87ff6fe38fd5ed9bfb66b3
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2020-12-08 12:14:50 -0800

    [coordinator] Add APIs to get version and package information.

--- thirdeye-coordinator/src/main/java/org/apache/pinot/thirdeye/resources/InternalResource.java
+++ thirdeye-coordinator/src/main/java/org/apache/pinot/thirdeye/resources/InternalResource.java
@@ -1,15 +1,21 @@
 package org.apache.pinot.thirdeye.resources;
 
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.jaxrs.annotation.JacksonFeatures;
 import io.swagger.annotations.Api;
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import org.apache.pinot.thirdeye.datasource.pinot.resources.PinotDataSourceResource;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Api(tags = "zzz Internal zzz")
 public class InternalResource {
+
+  private static final Package PACKAGE = InternalResource.class.getPackage();
 
   private final PinotDataSourceResource pinotDataSourceResource;
 
@@ -21,5 +27,19 @@ public class InternalResource {
   @Path("pinot-data-source")
   public PinotDataSourceResource getPinotDataSourceResource() {
     return pinotDataSourceResource;
+  }
+
+  @GET
+  @Path("version")
+  public Response getVersion() {
+    return Response.ok(InternalResource.class.getPackage()
+        .getImplementationVersion()).build();
+  }
+
+  @GET
+  @Path("package-info")
+  @JacksonFeatures(serializationEnable = {SerializationFeature.INDENT_OUTPUT})
+  public Response getPackageInfo() {
+    return Response.ok(PACKAGE).build();
   }
 }

c92966c71638c5f0d900db18ba4c5786be3b47b5
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2020-12-10 15:46:24 -0800

    [coordinator] Added POST /anomaly/{id}/feedback API updating feedback
    The same method can be used to replace existing feedback.

--- thirdeye-coordinator/src/main/java/org/apache/pinot/thirdeye/resources/AnomalyResource.java
+++ thirdeye-coordinator/src/main/java/org/apache/pinot/thirdeye/resources/AnomalyResource.java
@@ -1,18 +1,28 @@
 package org.apache.pinot.thirdeye.resources;
 
 import static org.apache.pinot.thirdeye.ThirdEyeStatus.ERR_OPERATION_UNSUPPORTED;
+import static org.apache.pinot.thirdeye.datalayer.util.ThirdEyeSpiUtils.optional;
 import static org.apache.pinot.thirdeye.resources.ResourceUtils.badRequest;
 
+import com.codahale.metrics.annotation.Timed;
 import com.google.common.collect.ImmutableMap;
 import io.swagger.annotations.Api;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import org.apache.pinot.thirdeye.api.AnomalyApi;
+import org.apache.pinot.thirdeye.api.AnomalyFeedbackApi;
 import org.apache.pinot.thirdeye.auth.AuthService;
 import org.apache.pinot.thirdeye.auth.ThirdEyePrincipal;
 import org.apache.pinot.thirdeye.datalayer.bao.MergedAnomalyResultManager;
+import org.apache.pinot.thirdeye.datalayer.dto.AnomalyFeedbackDTO;
 import org.apache.pinot.thirdeye.datalayer.dto.MergedAnomalyResultDTO;
 import org.apache.pinot.thirdeye.util.ApiBeanMapper;
 
@@ -24,12 +34,22 @@ public class AnomalyResource extends CrudResource<AnomalyApi, MergedAnomalyResul
   public static final ImmutableMap<String, String> API_TO_BEAN_MAP = ImmutableMap.<String, String>builder()
       .put("alert.id", "detectionConfigId")
       .build();
+  private final MergedAnomalyResultManager mergedAnomalyResultManager;
 
   @Inject
   public AnomalyResource(
       final AuthService authService,
       final MergedAnomalyResultManager mergedAnomalyResultManager) {
     super(authService, mergedAnomalyResultManager, API_TO_BEAN_MAP);
+    this.mergedAnomalyResultManager = mergedAnomalyResultManager;
+  }
+
+  private static AnomalyFeedbackDTO toAnomalyFeedbackDTO(AnomalyFeedbackApi api) {
+    final AnomalyFeedbackDTO dto = new AnomalyFeedbackDTO();
+    dto.setComment(api.getComment());
+    dto.setFeedbackType(api.getType());
+
+    return dto;
   }
 
   @Override
@@ -47,5 +67,32 @@ public class AnomalyResource extends CrudResource<AnomalyApi, MergedAnomalyResul
   @Override
   protected AnomalyApi toApi(final MergedAnomalyResultDTO dto) {
     return ApiBeanMapper.toApi(dto);
+  }
+
+  @Path("{id}/feedback")
+  @POST
+  @Timed
+  public Response setFeedback(
+      @HeaderParam(HttpHeaders.AUTHORIZATION) String authHeader,
+      @PathParam("id") Long id,
+      AnomalyFeedbackApi api) {
+    final ThirdEyePrincipal principal = authService.authenticate(authHeader);
+    final MergedAnomalyResultDTO dto = get(id);
+
+    final AnomalyFeedbackDTO feedbackDTO = toAnomalyFeedbackDTO(api);
+    dto.setFeedback(feedbackDTO);
+    mergedAnomalyResultManager.updateAnomalyFeedback(dto);
+
+    if (dto.isChild()) {
+      optional(mergedAnomalyResultManager.findParent(dto))
+          .ifPresent(p -> {
+            p.setFeedback(feedbackDTO);
+            mergedAnomalyResultManager.updateAnomalyFeedback(p);
+          });
+    }
+
+    return Response
+        .ok()
+        .build();
   }
 }

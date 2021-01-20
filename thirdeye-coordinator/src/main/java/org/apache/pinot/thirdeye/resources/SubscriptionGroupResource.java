c9d5a7938a957763034eb9faed55fd25fdcf5913
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2021-01-19 21:03:23 -0800

    [coordinator] Handle duplicate name for subscription group

--- thirdeye-coordinator/src/main/java/org/apache/pinot/thirdeye/resources/SubscriptionGroupResource.java
+++ thirdeye-coordinator/src/main/java/org/apache/pinot/thirdeye/resources/SubscriptionGroupResource.java
@@ -1,7 +1,9 @@
 package org.apache.pinot.thirdeye.resources;
 
+import static org.apache.pinot.thirdeye.ThirdEyeStatus.ERR_DUPLICATE_NAME;
 import static org.apache.pinot.thirdeye.ThirdEyeStatus.ERR_ID_UNEXPECTED_AT_CREATION;
 import static org.apache.pinot.thirdeye.datalayer.util.ThirdEyeSpiUtils.optional;
+import static org.apache.pinot.thirdeye.resources.ResourceUtils.ensure;
 import static org.apache.pinot.thirdeye.resources.ResourceUtils.ensureNull;
 import static org.apache.pinot.thirdeye.util.ApiBeanMapper.toSubscriptionGroupDTO;
 
@@ -18,6 +20,7 @@ import org.apache.pinot.thirdeye.auth.AuthService;
 import org.apache.pinot.thirdeye.auth.ThirdEyePrincipal;
 import org.apache.pinot.thirdeye.datalayer.bao.SubscriptionGroupManager;
 import org.apache.pinot.thirdeye.datalayer.dto.SubscriptionGroupDTO;
+import org.apache.pinot.thirdeye.datalayer.util.Predicate;
 import org.apache.pinot.thirdeye.util.ApiBeanMapper;
 
 @Api(tags = "Subscription Group")
@@ -26,17 +29,23 @@ import org.apache.pinot.thirdeye.util.ApiBeanMapper;
 public class SubscriptionGroupResource extends
     CrudResource<SubscriptionGroupApi, SubscriptionGroupDTO> {
 
+  private final SubscriptionGroupManager subscriptionGroupManager;
+
   @Inject
   public SubscriptionGroupResource(
       final SubscriptionGroupManager subscriptionGroupManager,
       final AuthService authService) {
     super(authService, subscriptionGroupManager, ImmutableMap.of());
+    this.subscriptionGroupManager = subscriptionGroupManager;
   }
 
   @Override
   protected SubscriptionGroupDTO createDto(final ThirdEyePrincipal principal,
       final SubscriptionGroupApi api) {
     ensureNull(api.getId(), ERR_ID_UNEXPECTED_AT_CREATION);
+    ensure(
+        subscriptionGroupManager.findByPredicate(Predicate.EQ("name", api.getName())).size() == 0,
+        ERR_DUPLICATE_NAME);
     return toSubscriptionGroupDTO(api);
   }
 

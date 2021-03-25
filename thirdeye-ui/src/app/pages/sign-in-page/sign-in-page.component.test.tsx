02e7adde6e4910a69e969fccea14c9ccb450c1d0
Chirag Madlani <chiragmadlani@ymail.com>
2021-03-25 21:07:12 +0530

    [UI] Sign In page tests (#284)
    * [UI] Sign In page tests
    
    * Updated tests
    
    Co-authored-by: Gaurav Joshi <68965367+joshigaurava@users.noreply.github.com>

---
+++ thirdeye-ui/src/app/pages/sign-in-page/sign-in-page.component.test.tsx
@@ -0,0 +1,73 @@
+import { act, fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import { SignInPage } from "./sign-in-page.component";
+
+jest.mock("../../components/auth-provider/auth-provider.component", () => ({
+    useAuth: jest.fn().mockImplementation(() => ({
+        signIn: mockSignIn,
+    })),
+}));
+
+jest.mock(
+    "../../components/app-breadcrumbs/app-breadcrumbs-provider/app-breadcrumbs-provider.component",
+    () => ({
+        useAppBreadcrumbs: jest.fn().mockImplementation(() => ({
+            setPageBreadcrumbs: mockSetPageBreadcrumbs,
+        })),
+    })
+);
+
+jest.mock("react-i18next", () => ({
+    useTranslation: jest.fn().mockReturnValue({
+        t: (key: string) => key,
+    }),
+}));
+
+jest.mock("../../components/page-contents/page-contents.component", () => ({
+    PageContents: jest.fn().mockImplementation((props) => (
+        <>
+            <div>title:{props.title}</div>
+            <div>{props.children}</div>
+        </>
+    )),
+}));
+
+describe("Sign In Page", () => {
+    test("should set appropriate page breadcrumbs", async () => {
+        act(() => {
+            render(<SignInPage />);
+        });
+
+        expect(mockSetPageBreadcrumbs).toHaveBeenCalledWith([]);
+    });
+
+    test("should set appropriate page title", async () => {
+        act(() => {
+            render(<SignInPage />);
+        });
+
+        expect(screen.getByText("title:label.sign-in")).toBeInTheDocument();
+    });
+
+    test("should render sign in button", async () => {
+        act(() => {
+            render(<SignInPage />);
+        });
+
+        expect(screen.getByText("label.sign-in")).toBeInTheDocument();
+    });
+
+    test("should sign in on sign in button click", async () => {
+        act(() => {
+            render(<SignInPage />);
+        });
+
+        fireEvent.click(screen.getByText("label.sign-in"));
+
+        expect(mockSignIn).toHaveBeenCalled();
+    });
+});
+
+const mockSetPageBreadcrumbs = jest.fn();
+
+const mockSignIn = jest.fn();

fa054859ac56204079d7efc0f59fccc77ecc9528
Daniel Lavoie <dlavoie@live.ca>
2021-05-12 13:33:41 -0400

    Migrated helm chart to thirdeye repository

---
+++ .concourse/tasks/publish-helm-chart/publish-helm-chart.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -z "$CHART_PREFIX" ]; then
+  echo "CHART_PREFIX is undefined." 1>&2
+  exit 1
+fi
+
+if [ -z "$ARTIFACTORY_URL" ]; then
+  echo "ARTIFACTORY_URL is undefined." 1>&2
+  exit 1
+fi
+
+
+if [ -z "$ARTIFACTORY_TOKEN" ]; then
+  echo "ARTIFACTORY_TOKEN is undefined." 1>&2
+  exit 1
+fi
+
+curl \
+  -H "X-JFrog-Art-Api:${ARTIFACTORY_TOKEN}" \
+  -T chart-distrib/*.tgz \
+  "${ARTIFACTORY_URL}/$CHART_PREFIX-$(cat ../chart-version/version).tgz"

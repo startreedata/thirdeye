57f7884f97fc488d3143c002a821f6e3a82ab33f
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2021-06-16 16:34:49 -0700

    [worker] Moving apis to coordinator. preparing for deletion

--- thirdeye-coordinator/src/main/java/org/apache/pinot/thirdeye/resources/InternalResource.java
+++ thirdeye-coordinator/src/main/java/org/apache/pinot/thirdeye/resources/InternalResource.java
@@ -1,14 +1,33 @@
 package org.apache.pinot.thirdeye.resources;
 
+import static org.apache.pinot.thirdeye.resources.ResourceUtils.ensureExists;
+
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.jaxrs.annotation.JacksonFeatures;
+import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
 import io.swagger.annotations.Api;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.apache.pinot.thirdeye.config.ThirdEyeCoordinatorConfiguration;
+import org.apache.pinot.thirdeye.detection.alert.DetectionAlertFilterNotification;
+import org.apache.pinot.thirdeye.detection.alert.DetectionAlertFilterResult;
+import org.apache.pinot.thirdeye.detection.alert.scheme.DetectionEmailAlerter;
+import org.apache.pinot.thirdeye.spi.datalayer.bao.AlertManager;
+import org.apache.pinot.thirdeye.spi.datalayer.bao.EventManager;
+import org.apache.pinot.thirdeye.spi.datalayer.bao.MergedAnomalyResultManager;
+import org.apache.pinot.thirdeye.spi.datalayer.bao.MetricConfigManager;
+import org.apache.pinot.thirdeye.spi.datalayer.bao.SubscriptionGroupManager;
+import org.apache.pinot.thirdeye.spi.datalayer.dto.MergedAnomalyResultDTO;
+import org.apache.pinot.thirdeye.spi.datalayer.dto.SubscriptionGroupDTO;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Api(tags = "zzz Internal zzz")
@@ -16,11 +35,29 @@ public class InternalResource {
 
   private static final Package PACKAGE = InternalResource.class.getPackage();
 
+  private final MergedAnomalyResultManager mergedAnomalyResultManager;
+  private final EventManager eventManager;
+  private final AlertManager detectionConfigManager;
+  private final MetricConfigManager metricConfigManager;
+  private final SubscriptionGroupManager subscriptionGroupManager;
+  private final ThirdEyeCoordinatorConfiguration thirdEyeCoordinatorConfiguration;
   private final DatabaseAdminResource databaseAdminResource;
 
   @Inject
   public InternalResource(
+      final MergedAnomalyResultManager mergedAnomalyResultManager,
+      final EventManager eventManager,
+      final AlertManager detectionConfigManager,
+      final MetricConfigManager metricConfigManager,
+      final SubscriptionGroupManager subscriptionGroupManager,
+      final ThirdEyeCoordinatorConfiguration thirdEyeCoordinatorConfiguration,
       final DatabaseAdminResource databaseAdminResource) {
+    this.mergedAnomalyResultManager = mergedAnomalyResultManager;
+    this.eventManager = eventManager;
+    this.detectionConfigManager = detectionConfigManager;
+    this.metricConfigManager = metricConfigManager;
+    this.subscriptionGroupManager = subscriptionGroupManager;
+    this.thirdEyeCoordinatorConfiguration = thirdEyeCoordinatorConfiguration;
     this.databaseAdminResource = databaseAdminResource;
   }
 
@@ -39,6 +76,31 @@ public class InternalResource {
   @Path("version")
   public Response getVersion() {
     return Response.ok(InternalResource.class.getPackage().getImplementationVersion()).build();
+  }
+
+  @POST
+  @Path("email")
+  public Response sendEmail(
+      @FormParam("subscriptionGroupId") Long subscriptionGroupId
+  ) throws Exception {
+
+    final SubscriptionGroupDTO sg = ensureExists(subscriptionGroupManager.findById(
+        subscriptionGroupId));
+    final Set<MergedAnomalyResultDTO> all = new HashSet<>(mergedAnomalyResultManager.findAll());
+    final DetectionAlertFilterResult result = new DetectionAlertFilterResult(
+        ImmutableMap.of(new DetectionAlertFilterNotification(sg), all)
+    );
+    DetectionEmailAlerter instance = new DetectionEmailAlerter(
+        sg,
+        thirdEyeCoordinatorConfiguration,
+        result,
+        metricConfigManager,
+        detectionConfigManager,
+        eventManager,
+        mergedAnomalyResultManager);
+
+    instance.buildAndSendEmail(sg, new ArrayList<>(all));
+    return Response.ok().build();
   }
 
   @GET

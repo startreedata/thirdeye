b6a31e983eb36e4521b3cbe612615680fbd02fad
Xiang Fu <xiangfu.1024@gmail.com>
2021-06-27 22:56:09 -0700

    fix start-pinot script

--- scripts/start-pinot.sh
+++ scripts/start-pinot.sh
@@ -1,7 +1,7 @@
 if [ -z "${PINOT_VERSION}" ]; then 
     PINOT_VERSION=0.7.1
 fi
-if [[ ! -f "/tmp/pinot-bin/" ]]; then
+if [[ ! -d "/tmp/pinot-bin/" ]]; then
   wget -O /tmp/pinot-bin.tar.gz https://downloads.apache.org/incubator/pinot/apache-pinot-incubating-${PINOT_VERSION}/apache-pinot-incubating-${PINOT_VERSION}-bin.tar.gz
   mkdir /tmp/pinot-bin/
   tar -vxf /tmp/pinot-bin.tar.gz -C /tmp/pinot-bin/

95db189edcedd7c68d6b2f8e4a71c7f5127b74a0
Gaurav Joshi <68965367+joshigaurava@users.noreply.github.com>
2021-08-03 15:12:06 -0700

    Updated Pinot URL post ASF graduation

--- scripts/start-pinot.sh
+++ scripts/start-pinot.sh
@@ -2,7 +2,7 @@ if [ -z "${PINOT_VERSION}" ]; then
     PINOT_VERSION=0.7.1
 fi
 if [[ ! -d "/tmp/pinot-bin/" ]]; then
-  wget -O /tmp/pinot-bin.tar.gz https://downloads.apache.org/incubator/pinot/apache-pinot-incubating-${PINOT_VERSION}/apache-pinot-incubating-${PINOT_VERSION}-bin.tar.gz
+  wget -O /tmp/pinot-bin.tar.gz https://downloads.apache.org/pinot/apache-pinot-incubating-${PINOT_VERSION}/apache-pinot-incubating-${PINOT_VERSION}-bin.tar.gz
   mkdir /tmp/pinot-bin/
   tar -vxf /tmp/pinot-bin.tar.gz -C /tmp/pinot-bin/
 fi

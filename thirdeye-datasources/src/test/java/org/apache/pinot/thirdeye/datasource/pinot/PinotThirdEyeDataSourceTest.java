efeb3ac6c319671940a6d9649fc0a73a9411969c
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2021-08-11 15:01:59 -0700

    [coordinator] Updated data source creation payload.

--- thirdeye-datasources/src/test/java/org/apache/pinot/thirdeye/datasource/pinot/PinotThirdEyeDataSourceTest.java
+++ thirdeye-datasources/src/test/java/org/apache/pinot/thirdeye/datasource/pinot/PinotThirdEyeDataSourceTest.java
@@ -16,37 +16,12 @@
 
 package org.apache.pinot.thirdeye.datasource.pinot;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import org.apache.pinot.thirdeye.datasource.pinot.resultset.ThirdEyeResultSetUtils;
 import org.apache.pinot.thirdeye.spi.detection.MetricAggFunction;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PinotThirdEyeDataSourceTest {
-
-  @Test
-  public void testInitializeCacheLoaderFromGivenClass() throws Exception {
-    Map<String, Object> properties = new HashMap<>();
-    properties.put(PinotThirdEyeDataSource.CACHE_LOADER_CLASS_NAME_STRING,
-        PinotControllerResponseCacheLoader.class.getName());
-
-    PinotResponseCacheLoader cacheLoaderInstance = PinotThirdEyeDataSource
-        .getCacheLoaderInstance(properties);
-    Assert.assertTrue(
-        PinotControllerResponseCacheLoader.class.equals(cacheLoaderInstance.getClass()));
-  }
-
-  @Test
-  public void testInitializeCacheLoaderFromEmptyClass() throws Exception {
-    Map<String, Object> properties = Collections.emptyMap();
-
-    PinotResponseCacheLoader cacheLoaderInstance = PinotThirdEyeDataSource
-        .getCacheLoaderInstance(properties);
-    Assert.assertTrue(
-        PinotControllerResponseCacheLoader.class.equals(cacheLoaderInstance.getClass()));
-  }
 
   @Test
   public void testReduceSum() {

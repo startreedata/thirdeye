f7876adbd6999d5831691e145833d59a6982f91e
Shounak kulkarni <shounak@startree.ai>
2021-08-18 13:08:26 +0530

    Removed unused property

--- thirdeye-core/src/main/java/org/apache/pinot/thirdeye/notification/formatter/channels/WebhookContentFormatter.java
+++ thirdeye-core/src/main/java/org/apache/pinot/thirdeye/notification/formatter/channels/WebhookContentFormatter.java
@@ -1,21 +1,14 @@
 package org.apache.pinot.thirdeye.notification.formatter.channels;
 
-import com.google.inject.Inject;
+import com.google.inject.Singleton;
 import java.util.List;
-import org.apache.pinot.thirdeye.config.ThirdEyeCoordinatorConfiguration;
 import org.apache.pinot.thirdeye.mapper.ApiBeanMapper;
 import org.apache.pinot.thirdeye.spi.api.WebhookApi;
 import org.apache.pinot.thirdeye.spi.datalayer.dto.MergedAnomalyResultDTO;
 import org.apache.pinot.thirdeye.spi.datalayer.dto.SubscriptionGroupDTO;
 
+@Singleton
 public class WebhookContentFormatter {
-
-  private final ThirdEyeCoordinatorConfiguration teConfig;
-  @Inject
-  public WebhookContentFormatter(
-      final ThirdEyeCoordinatorConfiguration teConfig) {
-    this.teConfig = teConfig;
-  }
 
   public WebhookApi getWebhookApi(final List<MergedAnomalyResultDTO> anomalies, SubscriptionGroupDTO subsConfig){
     return ApiBeanMapper.toWebhookApi(anomalies, subsConfig);

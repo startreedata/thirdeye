0c80361a925e57378c612bc28a65a3d2618a12d7
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2021-10-19 17:34:17 -0700

    [core] Refactor. Trimming unused code paths. Removing config dependencies.
    Goal: Modularize the notification piece to be moved out to another module

--- thirdeye-core/src/main/java/org/apache/pinot/thirdeye/detection/anomaly/alert/util/AlertScreenshotHelper.java
+++ thirdeye-core/src/main/java/org/apache/pinot/thirdeye/detection/anomaly/alert/util/AlertScreenshotHelper.java
@@ -24,7 +24,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import org.apache.pinot.thirdeye.config.ThirdEyeServerConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,47 +31,53 @@ public class AlertScreenshotHelper {
 
   private static final Logger LOG = LoggerFactory.getLogger(AlertScreenshotHelper.class);
 
+  private static final String ROOT_DIR = "";
+  private static final String PHANTOM_JS_PATH = "";
   private static final String TEMP_PATH = "/tmp/graph";
   private static final String SCREENSHOT_FILE_SUFFIX = ".png";
   private static final String GRAPH_SCREENSHOT_GENERATOR_SCRIPT = "/getGraphPnj.js";
   private static final ExecutorService executorService = Executors.newCachedThreadPool();
 
-  public static String takeGraphScreenShot(final String anomalyId,
-      final ThirdEyeServerConfiguration configuration) {
-    return takeGraphScreenShot(anomalyId, configuration.getUiConfiguration().getExternalUrl(),
-        configuration.getRootDir(),
-        configuration.getPhantomJsPath());
+  public static String takeGraphScreenShot(final String anomalyId, final String uiPublicUrl) {
+    return takeGraphScreenShot(anomalyId, uiPublicUrl,
+        ROOT_DIR,
+        PHANTOM_JS_PATH);
   }
 
   public static String takeGraphScreenShot(final String anomalyId, final String dashboardHost,
       final String rootDir,
       final String phantomJsPath) {
-    Callable<String> callable = () -> takeScreenshot(anomalyId, dashboardHost, rootDir,
+    final Callable<String> callable = () -> takeScreenshot(anomalyId, dashboardHost, rootDir,
         phantomJsPath);
-    Future<String> task = executorService.submit(callable);
+    final Future<String> task = executorService.submit(callable);
     String result = null;
     try {
       result = task.get(3, TimeUnit.MINUTES);
       LOG.info("Finished with result: {}", result);
-    } catch (Exception e) {
+    } catch (final Exception e) {
       LOG.error("Exception in fetching screenshot for anomaly id {}", anomalyId, e);
     }
     return result;
   }
 
-  private static String takeScreenshot(String anomalyId, String dashboardHost, String rootDir,
-      String phantomJsPath) throws Exception {
-    String imgRoute = dashboardHost + "/app/#/screenshot/" + anomalyId;
+  private static String takeScreenshot(
+      final String anomalyId, final String dashboardHost, final String rootDir,
+      final String phantomJsPath) throws Exception {
+    final String imgRoute = dashboardHost + "/app/#/screenshot/" + anomalyId;
     LOG.info("imgRoute {}", imgRoute);
-    String phantomScript = rootDir + GRAPH_SCREENSHOT_GENERATOR_SCRIPT;
+
+    final String phantomScript = rootDir + GRAPH_SCREENSHOT_GENERATOR_SCRIPT;
     LOG.info("Phantom JS script {}", phantomScript);
-    String imgPath = TEMP_PATH + anomalyId + SCREENSHOT_FILE_SUFFIX;
+
+    final String imgPath = TEMP_PATH + anomalyId + SCREENSHOT_FILE_SUFFIX;
     LOG.info("imgPath {}", imgPath);
-    Process proc = Runtime.getRuntime().exec(
+
+    final Process proc = Runtime.getRuntime().exec(
         new String[]{phantomJsPath, "phantomjs", "--ssl-protocol=any", "--ignore-ssl-errors=true",
             phantomScript, imgRoute, imgPath});
     LOG.info("Waiting for phantomjs...");
-    boolean isComplete = proc.waitFor(2, TimeUnit.MINUTES);
+
+    final boolean isComplete = proc.waitFor(2, TimeUnit.MINUTES);
     LOG.info("phantomjs complete status after waiting: {}", isComplete);
     if (!isComplete) {
       proc.destroyForcibly();

7cd7bf9d80c3cb0a105585fa33ea6cfcefd34c4f
Shounak kulkarni <shounak@startree.ai>
2021-11-02 17:29:24 +0530

    priority fix for AuthDisabledRequestFilter

--- thirdeye-core/src/main/java/org/apache/pinot/thirdeye/auth/AuthDisabledRequestFilter.java
+++ thirdeye-core/src/main/java/org/apache/pinot/thirdeye/auth/AuthDisabledRequestFilter.java
@@ -4,10 +4,13 @@ import static org.apache.pinot.thirdeye.spi.Constants.AUTH_BEARER;
 import static org.apache.pinot.thirdeye.spi.Constants.NO_AUTH_USER;
 
 import java.io.IOException;
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.HttpHeaders;
 
+@Priority(Priorities.AUTHENTICATION-1)
 public class AuthDisabledRequestFilter implements ContainerRequestFilter {
 
   @Override

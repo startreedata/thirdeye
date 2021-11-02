6a19d529ab8b2fa2152ab6ed4ad00e0419d71a25
aashitk <35622316+aashitk@users.noreply.github.com>
2021-11-02 12:20:05 +0530

    refactor(thirdeye-ui): [UI]redesign alerts list page (#99)
    * refactor(thirdeye-ui): redesign alerts list page
    
    * Added time range selector for Alert-list
    
    * [UI]: replace platform-ui json editor, update platform-ui
    
    * Resolved post merge errors
    
    * Add expand row, layout fixes
    
    * Revert alert-list changes, create new alert-list-v1
    
    Co-authored-by: Chirag Madlani <chirag.m@deuexsolutions.com>

--- thirdeye-ui/src/app/components/configuration-page-header/configuration-page-header.component.tsx
+++ thirdeye-ui/src/app/components/configuration-page-header/configuration-page-header.component.tsx
@@ -1,7 +1,4 @@
 import {
-    DropdownButtonTypeV1,
-    DropdownButtonV1,
-    PageHeaderActionsV1,
     PageHeaderTabsV1,
     PageHeaderTabV1,
     PageHeaderTextV1,
@@ -9,120 +6,19 @@ import {
 } from "@startree-ui/platform-ui";
 import React, { FunctionComponent } from "react";
 import { useTranslation } from "react-i18next";
-import { useHistory } from "react-router";
-import {
-    AppRoute,
-    getAlertsCreatePath,
-    getDatasetsOnboardPath,
-    getDatasourcesCreatePath,
-    getMetricsCreatePath,
-    getSubscriptionGroupsCreatePath,
-} from "../../utils/routes/routes.util";
+import { AppRoute } from "../../utils/routes/routes.util";
+import { CreateMenuButton } from "../create-menu-button.component/create-menu-button.component";
 import { ConfigurationPageHeaderProps } from "./configuration-page-header.interfaces";
 
 export const ConfigurationPageHeader: FunctionComponent<ConfigurationPageHeaderProps> = (
     props: ConfigurationPageHeaderProps
 ) => {
     const { t } = useTranslation();
-    const history = useHistory();
-
-    const handleCreateAlert = (): void => {
-        history.push(getAlertsCreatePath());
-    };
-
-    const handleCreateSubscriptionGroup = (): void => {
-        history.push(getSubscriptionGroupsCreatePath());
-    };
-
-    const handleCreateMetric = (): void => {
-        history.push(getMetricsCreatePath());
-    };
-
-    const handleOnBoardDataset = (): void => {
-        history.push(getDatasetsOnboardPath());
-    };
-
-    const handleCreateDatasource = (): void => {
-        history.push(getDatasourcesCreatePath());
-    };
-
-    const shortcutCreateMenuItems = [
-        {
-            id: "createAlert",
-            text: t("label.create-entity", {
-                entity: t("label.alert"),
-            }),
-        },
-        {
-            id: "createSubscriptionGroup",
-            text: t("label.create-entity", {
-                entity: t("label.subscription-group"),
-            }),
-        },
-        {
-            id: "createMetric",
-            text: t("label.create-entity", {
-                entity: t("label.metric"),
-            }),
-        },
-        {
-            id: "onboardDataset",
-            text: t("label.onboard-entity", {
-                entity: t("label.dataset"),
-            }),
-        },
-        {
-            id: "createDatasource",
-            text: t("label.create-entity", {
-                entity: t("label.datasource"),
-            }),
-        },
-    ];
-
-    const handleShortcutCreateOnclick = (
-        id: number | string,
-        _: string
-    ): void => {
-        switch (id) {
-            case "createAlert":
-                handleCreateAlert();
-
-                break;
-            case "createSubscriptionGroup":
-                handleCreateSubscriptionGroup();
-
-                break;
-            case "createMetric":
-                handleCreateMetric();
-
-                break;
-            case "onboardDataset":
-                handleOnBoardDataset();
-
-                break;
-            case "createDatasource":
-                handleCreateDatasource();
-
-                break;
-            default:
-                break;
-        }
-    };
 
     return (
         <PageHeaderV1>
             <PageHeaderTextV1>{t("label.configuration")}</PageHeaderTextV1>
-            <PageHeaderActionsV1>
-                <DropdownButtonV1
-                    color="primary"
-                    dropdownMenuItems={shortcutCreateMenuItems}
-                    type={DropdownButtonTypeV1.Regular}
-                    onClick={handleShortcutCreateOnclick}
-                >
-                    {t("label.create")}
-                </DropdownButtonV1>
-            </PageHeaderActionsV1>
-
+            <CreateMenuButton />
             <PageHeaderTabsV1 selectedIndex={props.selectedIndex}>
                 <PageHeaderTabV1 href={AppRoute.SUBSCRIPTION_GROUPS}>
                     {t("label.subscription-groups")}

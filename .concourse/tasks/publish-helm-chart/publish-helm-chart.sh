28cae9a72c7c42d989ca90ea6053867d3a31a0a8
Shounak kulkarni <shounak@startree.ai>
2021-11-23 20:07:19 +0530

    Revert "script fixes"
    This reverts commit 7a057d5b1fb2f9cadd6d7e86fb9ce30d5732b8ea.

--- .concourse/tasks/publish-helm-chart/publish-helm-chart.sh
+++ .concourse/tasks/publish-helm-chart/publish-helm-chart.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-if [ -z "$CHART_PATH" ]; then
-  echo "CHART_PATH is undefined." 1>&2
-  exit 1
-fi
-
 if [ -z "$CHART_PREFIX" ]; then
   echo "CHART_PREFIX is undefined." 1>&2
   exit 1
 fi
 
-set -x
+if [ -z "$ARTIFACTORY_URL" ]; then
+  echo "ARTIFACTORY_URL is undefined." 1>&2
+  exit 1
+fi
 
-pushd build && \
-  CHART_PATH=${CHART_PREFIX}-$(cat ../chart-version/version).tgz && \
-  curl -H "X-JFrog-Art-Api:${HELM_REPOSITORY_TOKEN}" -T ${CHART_PATH} "${HELM_REPOSITORY_URL}/${CHART_PATH}"
+
+if [ -z "$ARTIFACTORY_TOKEN" ]; then
+  echo "ARTIFACTORY_TOKEN is undefined." 1>&2
+  exit 1
+fi
+
+curl \
+  -H "X-JFrog-Art-Api:${ARTIFACTORY_TOKEN}" \
+  -T chart-distrib/*.tgz \
+  "${ARTIFACTORY_URL}/$CHART_PREFIX-$(cat chart-version/version).tgz"

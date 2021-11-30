50939b12becc8dcf8ac7129cbed5c4e7fbaeced6
Cyril de Catheu <cyril@startree.ai>
2021-11-30 19:14:44 +0100

    [core] add Calcite engine to sqlOperator

--- thirdeye-core/src/main/java/org/apache/pinot/thirdeye/detection/v2/plan/SqlExecutionPlanNode.java
+++ thirdeye-core/src/main/java/org/apache/pinot/thirdeye/detection/v2/plan/SqlExecutionPlanNode.java
@@ -4,17 +4,11 @@ import java.util.Map;
 import org.apache.pinot.thirdeye.detection.v2.operator.SqlExecutionOperator;
 import org.apache.pinot.thirdeye.spi.detection.v2.Operator;
 import org.apache.pinot.thirdeye.spi.detection.v2.OperatorContext;
-import org.apache.pinot.thirdeye.spi.detection.v2.PlanNodeContext;
 
 public class SqlExecutionPlanNode extends DetectionPipelinePlanNode {
 
   public SqlExecutionPlanNode() {
     super();
-  }
-
-  @Override
-  public void init(final PlanNodeContext planNodeContext) {
-    super.init(planNodeContext);
   }
 
   @Override
@@ -24,16 +18,6 @@ public class SqlExecutionPlanNode extends DetectionPipelinePlanNode {
   @Override
   public String getType() {
     return "SqlExecution";
-  }
-
-  @Override
-  public String getName() {
-    return name;
-  }
-
-  @Override
-  public Map<String, Object> getParams() {
-    return planNodeBean.getParams();
   }
 
   @Override

a826126a02b96b426e6f9552be21c6a42615c2a0
Shounak kulkarni <shounak@startree.ai>
2021-12-09 19:45:15 +0530

    infoURL response overrides the auth.enable flag (#149)

--- thirdeye-core/src/main/java/org/apache/pinot/thirdeye/auth/AuthConfiguration.java
+++ thirdeye-core/src/main/java/org/apache/pinot/thirdeye/auth/AuthConfiguration.java
@@ -4,6 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class AuthConfiguration {
 
+  public static final String ISSUER_KEY = "issuer";
+  public static final String ISSUER_URL_KEY = "oidcIssuerUrl";
+  public static final String OIDC_CONFIG_KEY = "openidConfiguration";
+  public static final String JWKS_KEY = "jwks_uri";
+
   private boolean enabled = true;
 
   @JsonProperty("oauth")

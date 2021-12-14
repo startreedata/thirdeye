08f3d0781244b7a155b09ece5edae139c69b16e6
Shounak kulkarni <shounak@startree.ai>
2021-12-14 19:39:43 +0530

    thirdeye install job

---
+++ .concourse/tasks/install-thirdeye/install-thirdeye.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+wait_service() {
+  attempt_counter=0
+  max_attempts=600
+  sleep 10
+}
+
+set -x && \
+  HELM_CHART_VERSION=`cat chart-version/version` && \
+  ENVIRONMENT_NAME=`cat environment/metadata | jq '.name' -r` && \
+  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && \
+  chmod +x ./kubectl && \
+  curl https://get.helm.sh/helm-v3.7.1-linux-amd64.tar.gz -O && tar -zxvf helm-*.tar.gz --strip-components=1 && \
+  chmod +x helm && \
+  set +x && \
+  az login \
+    --service-principal \
+    --username ${AZURE_CLIENT_ID} \
+    --password ${AZURE_CLIENT_SECRET} \
+    --tenant ${AZURE_TENANT_ID} && \
+  az aks get-credentials \
+    --name $ENVIRONMENT_NAME \
+    --resource-group $ENVIRONMENT_NAME && \
+  set -x && \
+  ./kubectl delete ns te-helm-test && \
+  echo "pausing for 10s after cleanup" && \
+  sleep 10s && \
+  ./helm repo add internal ${STARTREE_HELM_REPOSITORY_URL} \
+    --username ${STARTREE_HELM_USERNAME} \
+    --password ${STARTREE_HELM_PASSWORD} && \
+  ./helm repo update && \
+  ./kubectl create ns te-helm-test && \
+  ./helm install thirdeye internal/startree-thirdeye -n te-helm-test && \
+  echo "Waiting for Services availability" && \
+  wait_service
---
+++ .concourse/thirdeye-helm-integration-testing.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "$CONCOURSE_TEAM" ]; then
+  echo "CONCOURSE_TEAM is undefined. Using \`thirdeye\`"
+  export CONCOURSE_TEAM="thirdeye"
+fi
+
+export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+set -x
+
+fly \
+  -t $CONCOURSE_TEAM \
+  set-pipeline \
+  -p thirdeye-helm-integation-testing \
+  -c $DIR/thirdeye.yml

89975bf234889dcd1b438265a9f9e1d84f8f071c
Shounak kulkarni <shounak@startree.ai>
2021-12-15 20:07:27 +0530

    Update install-thirdeye.sh

--- .concourse/tasks/install-thirdeye/install-thirdeye.sh
+++ .concourse/tasks/install-thirdeye/install-thirdeye.sh
@@ -8,6 +8,7 @@ wait_service() {
 
 set -x && \
   ENVIRONMENT_NAME=`cat environment/metadata | jq '.name' -r` && \
+  ls helm-version && \
   HELM_VERSION=`cat helm-version` && \
   curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && \
   chmod +x ./kubectl && \

6991031eb10fd77054ad1f1c1b1b84e3d66a0a50
Shounak kulkarni <shounak@startree.ai>
2021-12-15 18:57:25 +0530

    Revert "delete te specific resources"
    This reverts commit dc81031cdef428b971650f82b284a2fb0007a537.

--- .concourse/tasks/install-thirdeye/install-thirdeye.sh
+++ .concourse/tasks/install-thirdeye/install-thirdeye.sh
@@ -7,30 +7,30 @@ wait_service() {
 }
 
 set -x && \
-ENVIRONMENT_NAME=`cat environment/metadata | jq '.name' -r` && \
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && \
-chmod +x ./kubectl && \
-curl https://get.helm.sh/helm-v3.7.1-linux-amd64.tar.gz -O && tar -zxvf helm-*.tar.gz --strip-components=1 && \
-chmod +x helm && \
-set +x && \
-az login \
-  --service-principal \
-  --username ${AZURE_CLIENT_ID} \
-  --password ${AZURE_CLIENT_SECRET} \
-  --tenant ${AZURE_TENANT_ID} && \
-az aks get-credentials \
-  --name $ENVIRONMENT_NAME \
-  --resource-group $ENVIRONMENT_NAME && \
-set -x && \
-./kubectl delete all --all -n te-helm-test && \
-echo "pausing for 10s after cleanup" && \
-sleep 10s && \
-./helm repo add internal ${STARTREE_HELM_REPOSITORY_URL} \
-  --username ${STARTREE_HELM_USERNAME} \
-  --password ${STARTREE_HELM_PASSWORD} && \
-./helm repo update && \
-./kubectl create ns te-helm-test && \
-./helm install thirdeye internal/startree-thirdeye --version 0.0.0-build.6 -n te-helm-test --devel && \
-echo "Waiting for Services availability" && \
-wait_service && \
-./helm test thirdeye -n te-helm-test --filter "!name=thirdeye-mysql-test"
+  ENVIRONMENT_NAME=`cat environment/metadata | jq '.name' -r` && \
+  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && \
+  chmod +x ./kubectl && \
+  curl https://get.helm.sh/helm-v3.7.1-linux-amd64.tar.gz -O && tar -zxvf helm-*.tar.gz --strip-components=1 && \
+  chmod +x helm && \
+  set +x && \
+  az login \
+    --service-principal \
+    --username ${AZURE_CLIENT_ID} \
+    --password ${AZURE_CLIENT_SECRET} \
+    --tenant ${AZURE_TENANT_ID} && \
+  az aks get-credentials \
+    --name $ENVIRONMENT_NAME \
+    --resource-group $ENVIRONMENT_NAME && \
+  set -x && \
+  ./kubectl delete ns te-helm-test && \
+  echo "pausing for 10s after cleanup" && \
+  sleep 10s && \
+  ./helm repo add internal ${STARTREE_HELM_REPOSITORY_URL} \
+    --username ${STARTREE_HELM_USERNAME} \
+    --password ${STARTREE_HELM_PASSWORD} && \
+  ./helm repo update && \
+  ./kubectl create ns te-helm-test && \
+  ./helm install thirdeye internal/startree-thirdeye --version 0.0.0-build.6 -n te-helm-test --devel && \
+  echo "Waiting for Services availability" && \
+  wait_service && \
+  ./helm test thirdeye -n te-helm-test --filter "!name=thirdeye-mysql-test"

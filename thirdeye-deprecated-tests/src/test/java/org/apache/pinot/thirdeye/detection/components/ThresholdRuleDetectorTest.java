4bc944153b9ebdca63d8f7be0c9b6aac3d99c7a7
Cyril de Catheu <cyril@startree.ai>
2021-12-16 17:05:45 +0100

    remove comment

--- thirdeye-deprecated-tests/src/test/java/org/apache/pinot/thirdeye/detection/components/ThresholdRuleDetectorTest.java
+++ thirdeye-deprecated-tests/src/test/java/org/apache/pinot/thirdeye/detection/components/ThresholdRuleDetectorTest.java
@@ -80,7 +80,7 @@ public class ThresholdRuleDetectorTest {
     timeSeriesMap.put(AnomalyDetectorV2.KEY_CURRENT, SimpleDataTable.fromDataFrame(currentDf));
 
     ThresholdRuleDetectorSpec spec = new ThresholdRuleDetectorSpec();
-    spec.setMonitoringGranularity("1_DAYS"); //todo cyril factorize this
+    spec.setMonitoringGranularity("1_DAYS");
     double minValue = 50.;
     double maxValue = 600.;
     spec.setMin(minValue);
@@ -141,7 +141,7 @@ public class ThresholdRuleDetectorTest {
     timeSeriesMap.put(AnomalyDetectorV2.KEY_CURRENT, SimpleDataTable.fromDataFrame(currentDf));
 
     ThresholdRuleDetectorSpec spec = new ThresholdRuleDetectorSpec();
-    spec.setMonitoringGranularity("1_DAYS"); //todo cyril factorize this
+    spec.setMonitoringGranularity("1_DAYS");
     // min max not set - not relevant for test
     ThresholdRuleDetector detector = new ThresholdRuleDetector();
     detector.init(spec);

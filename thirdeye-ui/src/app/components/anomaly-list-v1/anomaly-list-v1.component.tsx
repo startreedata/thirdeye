393da390222fc7568bfe2bd2a1efaa2a57b2db02
Chirag Madlani <chirag.m@deuexsolutions.com>
2021-12-30 11:07:27 +0530

    [UI] Anomalies all page UI update

---
+++ thirdeye-ui/src/app/components/anomaly-list-v1/anomaly-list-v1.component.tsx
@@ -0,0 +1,163 @@
+import { Button, Typography } from "@material-ui/core";
+import {
+    DataGridSelectionModelV1,
+    DataGridV1,
+    linkRendererV1,
+} from "@startree-ui/platform-ui";
+import React, { FunctionComponent, ReactNode, useMemo, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { UiAnomaly } from "../../rest/dto/ui-anomaly.interfaces";
+import {
+    getAlertsViewPath,
+    getAnomaliesViewPath,
+} from "../../utils/routes/routes.util";
+import { AnomalyListV1Props } from "./anomaly-list-v1.interfaces";
+
+export const AnomalyListV1: FunctionComponent<AnomalyListV1Props> = (
+    props: AnomalyListV1Props
+) => {
+    const [
+        selectedAnomaly,
+        setSelectedAnomaly,
+    ] = useState<DataGridSelectionModelV1>();
+    const { t } = useTranslation();
+
+    const anomalyNameRenderer = (
+        cellValue: Record<string, unknown>,
+        data: Record<string, unknown>
+    ): ReactNode => {
+        return linkRendererV1(
+            cellValue,
+            getAnomaliesViewPath(data.id as number)
+        );
+    };
+
+    const alertNameRenderer = (
+        cellValue: Record<string, unknown>,
+        data: Record<string, unknown>
+    ): ReactNode => {
+        return linkRendererV1(
+            cellValue,
+            getAlertsViewPath(data.alertId as number)
+        );
+    };
+
+    const deviationRenderer = (
+        cellValue: Record<string, unknown>,
+        data: Record<string, unknown>
+    ): ReactNode => {
+        return (
+            <Typography
+                color={data.negativeDeviation ? "error" : undefined}
+                variant="body2"
+            >
+                {cellValue}
+            </Typography>
+        );
+    };
+
+    const isActionButtonDisable = !(
+        selectedAnomaly && selectedAnomaly.rowKeyValues.length === 1
+    );
+
+    const handleAnomalyDelete = (): void => {
+        if (!selectedAnomaly) {
+            return;
+        }
+
+        const anomalyId = selectedAnomaly.rowKeyValues[0];
+        const anomaly = (selectedAnomaly.rowKeyValueMap?.get(
+            anomalyId
+        ) as unknown) as UiAnomaly;
+
+        props.onDelete && props.onDelete(anomaly);
+    };
+
+    const anomalyListColumns = useMemo(
+        () => [
+            {
+                key: "name",
+                dataKey: "name",
+                header: t("label.name"),
+                sortable: true,
+                minWidth: 200,
+                customCellRenderer: anomalyNameRenderer,
+            },
+            {
+                key: "alertName",
+                dataKey: "alertName",
+                header: t("label.alert"),
+                sortable: true,
+                minWidth: 300,
+                // flex: 1,
+                customCellRenderer: alertNameRenderer,
+            },
+            {
+                key: "duration",
+                dataKey: "duration",
+                header: t("label.duration"),
+                sortable: true,
+                minWidth: 150,
+            },
+            {
+                key: "startTime",
+                dataKey: "startTime",
+                header: t("label.start"),
+                sortable: true,
+                minWidth: 200,
+            },
+            {
+                key: "endTime",
+                dataKey: "endTime",
+                header: t("label.end"),
+                sortable: true,
+                minWidth: 200,
+            },
+            {
+                key: "current",
+                dataKey: "current",
+                header: t("label.current"),
+                sortable: true,
+                minWidth: 150,
+            },
+            {
+                key: "predicted",
+                dataKey: "predicted",
+                header: t("label.predicted"),
+                sortable: true,
+                minWidth: 150,
+            },
+            {
+                key: "deviation",
+                dataKey: "deviation",
+                header: t("label.deviation"),
+                sortable: true,
+                minWidth: 150,
+                customCellRenderer: deviationRenderer,
+            },
+        ],
+        [anomalyNameRenderer, alertNameRenderer, deviationRenderer]
+    );
+
+    return (
+        <DataGridV1
+            hideBorder
+            columns={anomalyListColumns}
+            data={(props.anomalies as unknown) as Record<string, unknown>[]}
+            rowKey="id"
+            searchPlaceholder={t("label.search-entity", {
+                entity: t("label.anomalies"),
+            })}
+            toolbarComponent={
+                <Button
+                    disabled={isActionButtonDisable}
+                    variant="contained"
+                    onClick={handleAnomalyDelete}
+                >
+                    {t("label.delete")}
+                </Button>
+            }
+            onSelectionChange={setSelectedAnomaly}
+        />
+    );
+};

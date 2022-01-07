5d78de5d12c419663cb45fd9354ad131ca97c2a1
Cyril de Catheu <cyril@startree.ai>
2022-01-07 01:00:11 +0100

    upate pinot version to 0.9.3

--- scripts/start-pinot.sh
+++ scripts/start-pinot.sh
@@ -3,11 +3,11 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 REPO_DIR="${SCRIPT_DIR}/.."
 
 if [ -z "${PINOT_VERSION}" ]; then
-    PINOT_VERSION=0.7.1
+    PINOT_VERSION=0.9.3
 fi
 
 export PINOT_INSTALL_TMP_DIR="${REPO_DIR}/tmp/pinot-bin"
-export PINOT_DIR="${PINOT_INSTALL_TMP_DIR}/apache-pinot-incubating-${PINOT_VERSION}-bin"
+export PINOT_DIR="${PINOT_INSTALL_TMP_DIR}/apache-pinot-${PINOT_VERSION}-bin"
 export PINOT_LAUNCH_SH="${PINOT_DIR}/bin/quick-start-batch.sh"
 
 # If pinot quick-start script cannot be found, then reinstall
@@ -17,7 +17,7 @@ if [[ ! -f "${PINOT_LAUNCH_SH}" ]]; then
   rm -rf "${PINOT_INSTALL_TMP_DIR}"
 
   # Download pinot
-  curl -o /tmp/pinot-bin.tar.gz "https://downloads.apache.org/pinot/apache-pinot-incubating-${PINOT_VERSION}/apache-pinot-incubating-${PINOT_VERSION}-bin.tar.gz"
+  curl -o /tmp/pinot-bin.tar.gz "https://downloads.apache.org/pinot/apache-pinot-${PINOT_VERSION}/apache-pinot-${PINOT_VERSION}-bin.tar.gz"
   mkdir -p "${PINOT_INSTALL_TMP_DIR}"
 
   # extract pinot

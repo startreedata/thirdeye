e06a41d125d921c517301dccdc8eaebe8c0a705b
Steven Pham <steven@startree.ai>
2022-01-26 08:09:05 -0800

    fix(ui): remove date picker from configuration page

--- thirdeye-ui/src/app/components/configuration-page-header/configuration-page-header.component.tsx
+++ thirdeye-ui/src/app/components/configuration-page-header/configuration-page-header.component.tsx
@@ -1,4 +1,3 @@
-import { useMediaQuery, useTheme } from "@material-ui/core";
 import {
     PageHeaderActionsV1,
     PageHeaderTabsV1,
@@ -10,39 +9,17 @@ import React, { FunctionComponent } from "react";
 import { useTranslation } from "react-i18next";
 import { AppRoute } from "../../utils/routes/routes.util";
 import { CreateMenuButton } from "../create-menu-button.component/create-menu-button.component";
-import { useTimeRange } from "../time-range/time-range-provider/time-range-provider.component";
-import { TimeRangeSelector } from "../time-range/time-range-selector/time-range-selector/time-range-selector.component";
 import { ConfigurationPageHeaderProps } from "./configuration-page-header.interfaces";
 
 export const ConfigurationPageHeader: FunctionComponent<ConfigurationPageHeaderProps> = (
     props: ConfigurationPageHeaderProps
 ) => {
     const { t } = useTranslation();
-    const theme = useTheme();
-    const {
-        timeRangeDuration,
-        recentCustomTimeRangeDurations,
-        setTimeRangeDuration,
-        refreshTimeRange,
-    } = useTimeRange();
-
-    const screenWidthSmUp = useMediaQuery(theme.breakpoints.up("sm"));
 
     return (
         <PageHeaderV1>
             <PageHeaderTextV1>{t("label.configuration")}</PageHeaderTextV1>
             <PageHeaderActionsV1>
-                {/* Time range selector */}
-                <TimeRangeSelector
-                    hideTimeRange={!screenWidthSmUp}
-                    recentCustomTimeRangeDurations={
-                        recentCustomTimeRangeDurations
-                    }
-                    timeRangeDuration={timeRangeDuration}
-                    onChange={setTimeRangeDuration}
-                    onRefresh={refreshTimeRange}
-                />
-
                 {/* Create options button */}
                 <CreateMenuButton />
             </PageHeaderActionsV1>

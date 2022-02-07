6e8fc74ffb9c7fbecf147f44a20438233f687ee9
LuciKritZ <krishal.shah@deuexsolutions.com>
2022-02-07 21:23:57 +0530

    acknowledged pr comments

--- thirdeye-ui/src/app/components/no-data-indicator/no-data-indicator.test.tsx
+++ thirdeye-ui/src/app/components/no-data-indicator/no-data-indicator.test.tsx
@@ -1,24 +1,18 @@
-import { act, cleanup, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import React from "react";
 import { NoDataIndicator } from "./no-data-indicator.component";
 import { NoDataIndicatorProps } from "./no-data-indicator.interfaces";
 
 describe("NoDataIndicator", () => {
-    beforeEach(() => cleanup);
-
     it("component should load text prop correctly", async () => {
-        act(() => {
-            render(<NoDataIndicator {...mockDefaultProps} />);
-        });
+        render(<NoDataIndicator {...mockDefaultProps} />);
 
         expect(await screen.findByText("TestText")).toBeInTheDocument();
     });
 
     it("component should not load text if it's not passed", async () => {
         const props = { ...mockDefaultProps, text: "" };
-        act(() => {
-            render(<NoDataIndicator {...props} />);
-        });
+        render(<NoDataIndicator {...props} />);
 
         expect(screen.queryByText("TestText")).not.toBeInTheDocument();
     });

f02830b0581b3718733ffbffc2ff7cb3dc68057c
Steven Pham <2080348+spham92@users.noreply.github.com>
2022-02-08 08:26:42 -0800

    feat(ui): readonly detection configuration in alert view page (#240)
    * feat(ui): readonly detection configuration in alert view page
    
    * test(ui): remove mock server
    
    Co-authored-by: Steven Pham <steven@startree.ai>

--- thirdeye-ui/src/app/components/entity-cards/alert-card/alert-card.interfaces.ts
+++ thirdeye-ui/src/app/components/entity-cards/alert-card/alert-card.interfaces.ts
@@ -3,7 +3,7 @@ import { UiAlert } from "../../../rest/dto/ui-alert.interfaces";
 
 export interface AlertCardProps {
     uiAlert: UiAlert | null;
-    alertEvaluation: AlertEvaluation;
+    alertEvaluation: AlertEvaluation | null;
     searchWords?: string[];
     showViewDetails?: boolean;
     onChange?: (uiAlert: UiAlert) => void;

df0f7b4a14ef43e0a8be9b3a448df96dce79253a
Steven Pham <steven@startree.ai>
2022-02-14 22:34:15 -0800

    fix(ui): add @types from platform-ui

--- thirdeye-ui/src/app/components/platform-ui/components/info-icon-v1/info-icon-v1.styles.ts
+++ thirdeye-ui/src/app/components/platform-ui/components/info-icon-v1/info-icon-v1.styles.ts
@@ -1,7 +1,7 @@
 // Copyright 2022 StarTree Inc.
 // All rights reserved. Confidential and proprietary information of StarTree Inc.
 import { makeStyles } from "@material-ui/core";
-import { DimensionV1 } from "../..";
+import { DimensionV1 } from "../../utils/material-ui/dimension.util";
 
 export const useInfoIconV1Styles = makeStyles((theme) => ({
     infoIconFlex: {

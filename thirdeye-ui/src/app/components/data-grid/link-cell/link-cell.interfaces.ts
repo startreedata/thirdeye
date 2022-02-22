e53d6644dfac33f7f5964747e255b0ba7ac5f752
Chirag Madlani <chirag.m@deuexsolutions.com>
2022-02-22 15:19:57 +0530

    (chore)ui: remove all unused files and packages

--- thirdeye-ui/src/app/components/data-grid/link-cell/link-cell.interfaces.ts
+++
@@ -1,8 +0,0 @@
-import { GridCellParams } from "@material-ui/data-grid";
-
-export interface LinkCellProps<T> {
-    params: GridCellParams;
-    searchWords?: string[];
-    valueTextFn?: (value: T) => string;
-    onClick?: (value: T, rowId: number) => void;
-}

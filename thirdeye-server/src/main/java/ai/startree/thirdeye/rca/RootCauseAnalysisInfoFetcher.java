a1380d3a275b4ee6d5f51ebd2cc0d1ff8c6622ec
Cyril de Catheu <cyril@startree.ai>
2022-02-25 13:18:31 +0100

    implement migration from rca to metadata in alert config

--- thirdeye-server/src/main/java/ai/startree/thirdeye/rca/RootCauseAnalysisInfoFetcher.java
+++ thirdeye-server/src/main/java/ai/startree/thirdeye/rca/RootCauseAnalysisInfoFetcher.java
@@ -13,7 +13,9 @@ import ai.startree.thirdeye.spi.datalayer.bao.DatasetConfigManager;
 import ai.startree.thirdeye.spi.datalayer.bao.MergedAnomalyResultManager;
 import ai.startree.thirdeye.spi.datalayer.bao.MetricConfigManager;
 import ai.startree.thirdeye.spi.datalayer.dto.AlertDTO;
+import ai.startree.thirdeye.spi.datalayer.dto.AlertMetadataDTO;
 import ai.startree.thirdeye.spi.datalayer.dto.AlertTemplateDTO;
+import ai.startree.thirdeye.spi.datalayer.dto.DataSourceDTO;
 import ai.startree.thirdeye.spi.datalayer.dto.DatasetConfigDTO;
 import ai.startree.thirdeye.spi.datalayer.dto.MergedAnomalyResultDTO;
 import ai.startree.thirdeye.spi.datalayer.dto.MetricConfigDTO;
@@ -22,6 +24,7 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import java.io.IOException;
 import java.util.Objects;
+import java.util.Optional;
 
 @Singleton
 public class RootCauseAnalysisInfoFetcher {
@@ -62,26 +65,54 @@ public class RootCauseAnalysisInfoFetcher {
         mergedAnomalyDAO.findById(anomalyId), String.format("Anomaly ID: %d", anomalyId));
     final long detectionConfigId = anomalyDTO.getDetectionConfigId();
     final AlertDTO alertDTO = alertDAO.findById(detectionConfigId);
-    //startTime/endTime not important
+
+    if (alertDTO.getTemplate().getRca() != null) {
+      migrateRcaInfoToAlertMetadata(alertDTO);
+    }
+
+    // render properties - startTime/endTime not important
     final AlertTemplateDTO templateWithProperties = alertTemplateRenderer.renderAlert(
         alertDTO, 0L, 0L);
-    final RcaMetadataDTO rcaMetadataDTO = Objects.requireNonNull(templateWithProperties.getRca(),
-        "rca not found in alert config.");
-    final String metric = Objects.requireNonNull(rcaMetadataDTO.getMetric(),
-        "rca$metric not found in alert config.");
-    final String dataset = Objects.requireNonNull(rcaMetadataDTO.getDataset(),
-        "rca$dataset not found in alert config.");
+    // parse metadata
+    AlertMetadataDTO alertMetadataDto = Objects.requireNonNull(templateWithProperties.getMetadata(),
+        "metadata not found in alert config.");
+    final MetricConfigDTO metadataMetricDTO = Objects.requireNonNull(alertMetadataDto.getMetric(),
+        "metadata$metric not found in alert config.");
+    final String metricName = Objects.requireNonNull(metadataMetricDTO.getName(),
+        "metadata$metric$name not found in alert config.");
+    final DatasetConfigDTO metadataDatasetDTO = Objects.requireNonNull(alertMetadataDto.getDataset(),
+        "metadata$dataset not found in alert config.");
+    final String datasetName = Objects.requireNonNull(metadataDatasetDTO.getDataset(),
+        "metadata$dataset$dataset not found in alert config.");
+
+    // take config from persistence - makes it sure dataset/metric DTO configs are correct for RCA
     final MetricConfigDTO metricConfigDTO = ensureExists(
-        metricDAO.findByMetricAndDataset(metric, dataset),
+        metricDAO.findByMetricAndDataset(metricName, datasetName),
         String.format(
             "Could not find metric %s for dataset %s. Invalid RCA configuration for the alert %s?",
-            metric,
-            dataset,
+            metricName,
+            datasetName,
             anomalyId));
-
     final DatasetConfigDTO datasetConfigDTO = ensureExists(datasetDAO.findByDataset(metricConfigDTO.getDataset()),
         String.format("Dataset name: %s", metricConfigDTO.getDataset()));
 
     return new RootCauseAnalysisInfo(anomalyDTO, metricConfigDTO, datasetConfigDTO);
+  }
+
+  private void migrateRcaInfoToAlertMetadata(final AlertDTO alertDTO) {
+    RcaMetadataDTO rcaMetadataDTO = alertDTO.getTemplate().getRca();
+    DatasetConfigDTO metadataDatasetDTO = new DatasetConfigDTO();
+    metadataDatasetDTO.setDataset(rcaMetadataDTO.getDataset());
+    AlertMetadataDTO alertMetadataDTO = Optional.ofNullable(alertDTO.getTemplate().getMetadata()).orElse(new AlertMetadataDTO());
+    alertDTO.getTemplate().setMetadata(alertMetadataDTO
+        .setDataset(metadataDatasetDTO)
+        .setDatasource(new DataSourceDTO().setName(rcaMetadataDTO.getDatasource()))
+        .setMetric(new MetricConfigDTO().setName(rcaMetadataDTO.getMetric()))
+    );
+    alertDTO.getTemplate().setRca(null);
+    int migrationSuccess = alertDAO.update(alertDTO);
+    if (migrationSuccess != 1) {
+      throw new RuntimeException("Error when migrating rca info to AlertMetadata");
+    }
   }
 }

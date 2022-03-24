96d18644f052bb6b7b259be5f7cd7f9b6f94fcc1
Chirag Madlani <chirag.m@deuexsolutions.com>
2022-03-24 22:42:51 +0530

    feat(ui): support comment for anomaly feedback (#348)
    * feat(ui): support comment for anomaly feedback
    
    * seprate out comment and feedback change dialog

--- thirdeye-ui/src/app/components/anomlay-feedback/anomaly-feedback.component.tsx
+++ thirdeye-ui/src/app/components/anomlay-feedback/anomaly-feedback.component.tsx
@@ -1,7 +1,14 @@
-import { Box, Card, CardContent, MenuItem, TextField } from "@material-ui/core";
+import {
+    Button,
+    Card,
+    CardContent,
+    Grid,
+    MenuItem,
+    TextField,
+} from "@material-ui/core";
 import { AxiosError } from "axios";
 import { isEmpty } from "lodash";
-import React, { FunctionComponent, useState } from "react";
+import React, { FunctionComponent, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import {
     NotificationTypeV1,
@@ -31,9 +38,13 @@ export const AnomalyFeedback: FunctionComponent<AnomalyFeedbackProps> = ({
 }) => {
     const [currentlySelected, setCurrentlySelected] =
         useState<AnomalyFeedbackType>(anomalyFeedback.type);
+    const [feedbackComment, setFeedbackComment] = useState(
+        anomalyFeedback.comment
+    );
     const { showDialog } = useDialog();
     const { notify } = useNotificationProviderV1();
     const { t } = useTranslation();
+    const commentRef = useRef<HTMLInputElement>();
 
     const handleChange = (
         event: React.ChangeEvent<{ value: unknown }>
@@ -51,17 +62,46 @@ export const AnomalyFeedback: FunctionComponent<AnomalyFeedbackProps> = ({
                     value: `"${OPTION_TO_DESCRIPTIONS[newSelectedFeedbackType]}"`,
                 }),
                 okButtonLabel: t("label.change"),
-                onOk: () => handleFeedbackChangeOk(newSelectedFeedbackType),
+                onOk: () =>
+                    handleFeedbackChangeOk(
+                        newSelectedFeedbackType,
+                        commentRef.current?.value || anomalyFeedback.comment
+                    ),
             });
         }
     };
 
+    const handleCommentUpdate = (): void => {
+        showDialog({
+            type: DialogType.CUSTOM,
+            title: "Update comment",
+            children: (
+                <TextField
+                    fullWidth
+                    multiline
+                    defaultValue={feedbackComment}
+                    inputRef={commentRef}
+                    name="comment"
+                    rows={3}
+                />
+            ),
+            okButtonLabel: t("label.change"),
+            onOk: () =>
+                handleFeedbackChangeOk(
+                    currentlySelected,
+                    commentRef.current?.value || feedbackComment
+                ),
+        });
+    };
+
     const handleFeedbackChangeOk = (
-        feedbackType: AnomalyFeedbackType
+        feedbackType: AnomalyFeedbackType,
+        comment: string
     ): void => {
         const updateRequestPayload = {
             ...anomalyFeedback,
             type: feedbackType,
+            comment,
         };
         updateAnomalyFeedback(anomalyId, updateRequestPayload)
             .then(() => {
@@ -72,8 +112,12 @@ export const AnomalyFeedback: FunctionComponent<AnomalyFeedbackProps> = ({
                     })
                 );
                 setCurrentlySelected(feedbackType);
+                setFeedbackComment(comment);
             })
             .catch((error: AxiosError) => {
+                // Reset comment on error
+                setFeedbackComment(anomalyFeedback.comment);
+
                 const errMessages = getErrorMessages(error);
 
                 isEmpty(errMessages)
@@ -93,26 +137,35 @@ export const AnomalyFeedback: FunctionComponent<AnomalyFeedbackProps> = ({
     return (
         <Card className={className} variant="outlined">
             <CardContent>
-                <Box marginBottom="4px">
-                    <label>
-                        <strong>Is this an anomaly?</strong>
-                    </label>
-                </Box>
-                <TextField
-                    fullWidth
-                    select
-                    id="anomaly-feedback-select"
-                    value={currentlySelected}
-                    onChange={handleChange}
-                >
-                    {Object.keys(OPTION_TO_DESCRIPTIONS).map(
-                        (optionKey: string) => (
-                            <MenuItem key={optionKey} value={optionKey}>
-                                {OPTION_TO_DESCRIPTIONS[optionKey]}
-                            </MenuItem>
-                        )
-                    )}
-                </TextField>
+                <Grid container>
+                    <Grid item xs={12}>
+                        <label>
+                            <strong>Is this an anomaly?</strong>
+                        </label>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            fullWidth
+                            select
+                            id="anomaly-feedback-select"
+                            value={currentlySelected}
+                            onChange={handleChange}
+                        >
+                            {Object.keys(OPTION_TO_DESCRIPTIONS).map(
+                                (optionKey: string) => (
+                                    <MenuItem key={optionKey} value={optionKey}>
+                                        {OPTION_TO_DESCRIPTIONS[optionKey]}
+                                    </MenuItem>
+                                )
+                            )}
+                        </TextField>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button onClick={handleCommentUpdate}>
+                            View / Edit comment
+                        </Button>
+                    </Grid>
+                </Grid>
             </CardContent>
         </Card>
     );

26a64bec7bc0cfea06b8f6badc94ebd37af9115b
Steven Pham <steven@startree.ai>
2022-03-30 10:34:53 -0700

    refactor(ui): move a soon-to-be commonly used constant to shareable location

--- thirdeye-ui/src/app/pages/anomalies-view-page/anomalies-view-page.interfaces.ts
+++ thirdeye-ui/src/app/pages/anomalies-view-page/anomalies-view-page.interfaces.ts
@@ -10,9 +10,26 @@ export enum AnomalyBreakdownAPIOffsetValues {
     FOUR_WEEKS_AGO = "P4W",
 }
 
-export enum AnomalyBreakdownAPIOffsetValuesV2 {
-    CURRENT = "current",
-    ONE_WEEK_AGO = "P1W",
-    TWO_WEEKS_AGO = "P2W",
-    THREE_WEEKS_AGO = "P3W",
-}
+export const OFFSET_TO_HUMAN_READABLE = {
+    [AnomalyBreakdownAPIOffsetValues.CURRENT]: "",
+    [AnomalyBreakdownAPIOffsetValues.ONE_WEEK_AGO]: "One Week Ago",
+    [AnomalyBreakdownAPIOffsetValues.TWO_WEEKS_AGO]: "Two Weeks Ago",
+    [AnomalyBreakdownAPIOffsetValues.THREE_WEEKS_AGO]: "Three Weeks Ago",
+    [AnomalyBreakdownAPIOffsetValues.FOUR_WEEKS_AGO]: "Four Weeks Ago",
+};
+
+export const BASELINE_OPTIONS: {
+    key: AnomalyBreakdownAPIOffsetValues;
+    description: string;
+}[] = [];
+
+Object.values(AnomalyBreakdownAPIOffsetValues).forEach(
+    (offsetKey: AnomalyBreakdownAPIOffsetValues) => {
+        if (offsetKey !== AnomalyBreakdownAPIOffsetValues.CURRENT) {
+            BASELINE_OPTIONS.push({
+                key: offsetKey,
+                description: OFFSET_TO_HUMAN_READABLE[offsetKey],
+            });
+        }
+    }
+);

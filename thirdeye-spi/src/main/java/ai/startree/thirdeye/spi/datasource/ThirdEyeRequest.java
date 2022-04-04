b5ebb4b3b17ac322037cc2898fd101f7a99fdf47
Cyril de Catheu <cyril@startree.ai>
2022-04-04 18:13:17 +0200

    fixes

--- thirdeye-spi/src/main/java/ai/startree/thirdeye/spi/datasource/ThirdEyeRequest.java
+++ thirdeye-spi/src/main/java/ai/startree/thirdeye/spi/datasource/ThirdEyeRequest.java
@@ -16,7 +16,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 

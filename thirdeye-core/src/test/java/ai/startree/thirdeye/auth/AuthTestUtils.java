a5bbbfe5b73eb76ca5a161a8f7aec727f5185a2e
Shounak kulkarni <shounak@startree.ai>
2022-04-12 13:11:14 +0530

    [integration-tests] OAuth test suite (#345)
    * Unit test cases for oidc flows
    
    * Unit tests cleanup
    
    * OAuth integration testing
    
    * Config changes fix.
    
    * Internal protected ping endpoint added.
    
    * import fix
    
    * Test case fix
    
    * removed internal/ping-auth to use api/auth/login
    
    * OAuth test case updated based on code changes
    
    * Changed db port as existing port is not released in time by previous test.
    
    * [integration-tests] Auth disabled by default as its shared across test files.
    
    * [integration-tests] Moved AuthTestUtils to utils package
    
    * [core] removed redundant dependency
    
    * [integration-tests] Config input file for auth related tests added.

---
+++ thirdeye-core/src/test/java/ai/startree/thirdeye/auth/AuthTestUtils.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2022 StarTree Inc. All rights reserved.
+ * Confidential and Proprietary Information of StarTree Inc.
+ */
+
+package ai.startree.thirdeye.auth;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSObject;
+import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.gen.RSAKeyGenerator;
+import com.nimbusds.jwt.JWTClaimsSet;
+
+public class AuthTestUtils {
+
+  public static JWK getJWK(String kid) throws JOSEException {
+    return new RSAKeyGenerator(2048)
+        .keyID(kid)
+        .generate();
+  }
+  public static String getToken(JWK key, JWTClaimsSet claims) throws JOSEException {
+    JWSObject jwsObject = new JWSObject(
+        new JWSHeader.Builder(JWSAlgorithm.RS256).keyID(key.getKeyID()).build(),
+        claims.toPayload());
+    jwsObject.sign(new RSASSASigner((RSAKey) key));
+    return jwsObject.serialize();
+  }
+}

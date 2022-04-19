7a243326dc8b4ff6943526514f769944191b11fd
Cyril de Catheu <cyril@startree.ai>
2022-04-19 17:21:08 +0200

    fixes

--- thirdeye-core/src/main/java/ai/startree/thirdeye/mapper/RootCauseSessionMapper.java
+++ thirdeye-core/src/main/java/ai/startree/thirdeye/mapper/RootCauseSessionMapper.java
@@ -3,8 +3,6 @@ package ai.startree.thirdeye.mapper;
 import ai.startree.thirdeye.spi.api.RootCauseSessionApi;
 import ai.startree.thirdeye.spi.datalayer.dto.RootCauseSessionDTO;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -12,10 +10,6 @@ public interface RootCauseSessionMapper {
 
   RootCauseSessionMapper INSTANCE = Mappers.getMapper(RootCauseSessionMapper.class);
 
-  @Mappings({
-      @Mapping(target = "webhook", source = "dto.webhookScheme"),
-      @Mapping(target = "updatedBy", source = "api.owner"),
-  })
   RootCauseSessionDTO toDto(RootCauseSessionApi api);
 
   RootCauseSessionApi toApi(RootCauseSessionDTO dto);

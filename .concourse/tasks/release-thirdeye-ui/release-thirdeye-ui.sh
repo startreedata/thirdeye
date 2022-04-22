cdfdfa471372fdaa9f53fd11a32ca7422551ee1e
Chirag Madlani <chirag.m@deuexsolutions.com>
2022-04-22 17:11:41 +0530

    artifact auth changes

--- .concourse/tasks/release-thirdeye-ui/release-thirdeye-ui.sh
+++ .concourse/tasks/release-thirdeye-ui/release-thirdeye-ui.sh
@@ -5,6 +5,20 @@
 # Confidential and Proprietary Information of StarTree Inc.
 #
 
+cat > ~/.npmrc <<EOF
+; this will be our temp .npmrc credentials for Artifactory
+@startree-ui:registry=https://repo.startreedata.io/artifactory/api/npm/startree-ui/
+//repo.startreedata.io/artifactory/api/npm/startree-ui/:_password=QVA3cHphSlFzYnlMdUhXSmlOcGFtVEtub1ZmMzFVMkd3TDFpZkc=
+//repo.startreedata.io/artifactory/api/npm/startree-ui/:username=chirag.m@deuexsolutions.com
+//repo.startreedata.io/artifactory/api/npm/startree-ui/:email=chirag.m@deuexsolutions.com
+//repo.startreedata.io/artifactory/api/npm/startree-ui/:always-auth=true
+EOF
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+
 cd src/thirdeye-ui && \
   npm install && \
   npm run release

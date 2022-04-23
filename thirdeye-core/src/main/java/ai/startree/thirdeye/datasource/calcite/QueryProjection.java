b026e59483f7628a73fa176fd351a0f9edd78c9a
Cyril de Catheu <cyril@startree.ai>
2022-04-23 14:49:27 +0200

    minor fixes

--- thirdeye-core/src/main/java/ai/startree/thirdeye/datasource/calcite/QueryProjection.java
+++ thirdeye-core/src/main/java/ai/startree/thirdeye/datasource/calcite/QueryProjection.java
@@ -54,7 +54,6 @@ public class QueryProjection {
   }
 
   public SqlNode toSqlNode() {
-    SqlNode node;
     if (operator != null) {
       return applyAlias(new SqlBasicCall(
           new SqlUnresolvedFunction(identifierOf(operator),

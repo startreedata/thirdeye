6e0781dbd75b210c18e7e6303a02e9cbc22d5943
Cyril de Catheu <cyril@startree.ai>
2022-04-28 23:41:04 +0200

    remove deprecated query endpoint

--- thirdeye-server/src/main/java/ai/startree/thirdeye/resources/RcaInvestigationResource.java
+++ thirdeye-server/src/main/java/ai/startree/thirdeye/resources/RcaInvestigationResource.java
@@ -8,32 +8,20 @@ package ai.startree.thirdeye.resources;
 import ai.startree.thirdeye.mapper.ApiBeanMapper;
 import ai.startree.thirdeye.spi.ThirdEyePrincipal;
 import ai.startree.thirdeye.spi.api.RcaInvestigationApi;
-import ai.startree.thirdeye.spi.datalayer.Predicate;
 import ai.startree.thirdeye.spi.datalayer.bao.RcaInvestigationManager;
 import ai.startree.thirdeye.spi.datalayer.dto.RcaInvestigationDTO;
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import io.dropwizard.auth.Auth;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiKeyAuthDefinition;
 import io.swagger.annotations.ApiKeyAuthDefinition.ApiKeyLocation;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.Authorization;
 import io.swagger.annotations.SecurityDefinition;
 import io.swagger.annotations.SwaggerDefinition;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
-import org.apache.commons.lang3.StringUtils;
 
 @Api(authorizations = {@Authorization(value = "oauth")})
 @SwaggerDefinition(securityDefinition = @SecurityDefinition(apiKeyAuthDefinitions = @ApiKeyAuthDefinition(name = HttpHeaders.AUTHORIZATION, in = ApiKeyLocation.HEADER, key = "oauth")))
@@ -66,94 +54,5 @@ public class RcaInvestigationResource extends CrudResource<RcaInvestigationApi, 
   @Override
   protected RcaInvestigationApi toApi(final RcaInvestigationDTO dto) {
     return ApiBeanMapper.toApi(dto);
-  }
-
-  @GET
-  @Path("/query")
-  @ApiOperation(value = "Query")
-  @Deprecated
-  public List<RcaInvestigationDTO> query(
-      @ApiParam(hidden = true) @Auth ThirdEyePrincipal principal,
-      @QueryParam("id") String idsString,
-      @QueryParam("name") String namesString,
-      @QueryParam("owner") String ownersString,
-      @QueryParam("previousId") String previousIdsString,
-      @QueryParam("anomalyId") String anomalyIdsString,
-      @QueryParam("anomalyRangeStart") Long anomalyRangeStart,
-      @QueryParam("anomalyRangeEnd") Long anomalyRangeEnd,
-      @QueryParam("createdRangeStart") Long createdRangeStart,
-      @QueryParam("createdRangeEnd") Long createdRangeEnd,
-      @QueryParam("updatedRangeStart") Long updatedRangeStart,
-      @QueryParam("updatedRangeEnd") Long updatedRangeEnd) {
-
-    List<Predicate> predicates = new ArrayList<>();
-
-    if (!StringUtils.isBlank(idsString)) {
-      predicates.add(Predicate.IN("baseId", split(idsString)));
-    }
-
-    if (!StringUtils.isBlank(namesString)) {
-      predicates.add(Predicate.IN("name", split(namesString)));
-    }
-
-    if (!StringUtils.isBlank(ownersString)) {
-      predicates.add(Predicate.IN("owner", split(ownersString)));
-    }
-
-    if (!StringUtils.isBlank(previousIdsString)) {
-      predicates.add(Predicate.IN("previousId", split(previousIdsString)));
-    }
-
-    if (!StringUtils.isBlank(anomalyIdsString)) {
-      predicates.add(Predicate.IN("anomalyId", split(anomalyIdsString)));
-    }
-
-    if (anomalyRangeStart != null) {
-      predicates.add(Predicate.GT("anomalyRangeEnd", anomalyRangeStart));
-    }
-
-    if (anomalyRangeEnd != null) {
-      predicates.add(Predicate.LT("anomalyRangeStart", anomalyRangeEnd));
-    }
-
-    if (createdRangeStart != null) {
-      predicates.add(Predicate.GE("created", createdRangeStart));
-    }
-
-    if (createdRangeEnd != null) {
-      predicates.add(Predicate.LT("created", createdRangeEnd));
-    }
-
-    if (updatedRangeStart != null) {
-      predicates.add(Predicate.GE("updated", updatedRangeStart));
-    }
-
-    if (updatedRangeEnd != null) {
-      predicates.add(Predicate.LT("updated", updatedRangeEnd));
-    }
-
-    if (predicates.isEmpty()) {
-      throw new IllegalArgumentException("Must provide at least one property");
-    }
-
-    return this.dtoManager.findByPredicate(Predicate.AND(predicates.toArray(new Predicate[predicates
-        .size()])));
-  }
-
-  /**
-   * Returns query param value split by comma and trimmed of empty entries.
-   *
-   * @param str query param value string (comma separated)
-   * @return array of non-empty values
-   */
-  private static String[] split(String str) {
-    List<String> args = new ArrayList<>(Arrays.asList(str.split(",")));
-    Iterator<String> itStr = args.iterator();
-    while (itStr.hasNext()) {
-      if (itStr.next().length() <= 0) {
-        itStr.remove();
-      }
-    }
-    return args.toArray(new String[args.size()]);
   }
 }

2400678a9e4396437ba4ba0e51aa4532f8198a62
Cyril de Catheu <cyril@startree.ai>
2022-05-03 15:02:06 +0200

    add related events and anomalies endpoint

---
+++ thirdeye-core/src/main/java/ai/startree/thirdeye/rootcause/events/EventScoring.java
@@ -0,0 +1,63 @@
+package ai.startree.thirdeye.rootcause.events;
+
+import java.util.concurrent.TimeUnit;
+import org.joda.time.Interval;
+import org.joda.time.Period;
+
+public enum EventScoring {
+  /**
+   * Determines a score between {@code [0.0, 1.0]} based on the entity's start time. The closer
+   * an entity's start time is to the start time of the interval, the higher the score. This
+   * scorer allows for a lookback and lookahead period around the startTime of the anomalyInterval.
+   */
+  TRIANGULAR {
+    public double score(final Interval anomalyInterval, final Interval otherInterval,
+        final Period lookaround) {
+      double diff = Math.abs(otherInterval.getStartMillis() - anomalyInterval.getStartMillis())
+          / (double) lookaround.toStandardDuration().getMillis();
+      // clip time out of lookaround range to 1
+      diff = Math.min(diff, 1.);
+
+      return 1 - diff;
+    }
+  },
+
+  /**
+   * Determines a score between {@code [0.0, 1.0]} based on the entity's start time. Similar to
+   * {@code TRIANGULAR} in function, squaring the result score.
+   *
+   * @see #TRIANGULAR
+   */
+  QUADRATIC {
+    public double score(final Interval anomalyInterval, final Interval eventInterval,
+        final Period lookback) {
+      return Math.pow(TRIANGULAR.score(anomalyInterval, eventInterval, lookback), 2);
+    }
+  },
+
+  /**
+   * Determines a score between {@code [0.0, 1.0]} based on the entity's start time. The score
+   * is proportional to the absolute distance from the anomaly region start and truncated to
+   * {@code 0.0} after the midpoint of this region.
+   *
+   * Note:  this was kept because it was the default scoring function used in legacy TE. New
+   * triangular and quadratic scorer look better and are easier to understand.
+   */
+  HYPERBOLA {
+    public double score(final Interval anomalyInterval, final Interval eventInterval,
+        final Period lookback) {
+      if (eventInterval.getStartMillis()
+          >= (anomalyInterval.getStartMillis() + anomalyInterval.getEndMillis()) / 2) {
+        return 0;
+      }
+      return 1.0d / (
+          COEFFICIENT * Math.abs(eventInterval.getStartMillis() - anomalyInterval.getStartMillis())
+              + 1.0);
+    }
+  };
+
+  private static final double COEFFICIENT = 1.0d / TimeUnit.HOURS.toMillis(1);
+
+  public abstract double score(final Interval anomalyInterval, final Interval eventInterval,
+      final Period lookaround);
+}
--- thirdeye-server/src/main/java/ai/startree/thirdeye/resources/RcaEventsResource.java
+++ thirdeye-server/src/main/java/ai/startree/thirdeye/resources/RcaEventsResource.java
@@ -1,17 +1,44 @@
 package ai.startree.thirdeye.resources;
 
+import ai.startree.thirdeye.mapper.ApiBeanMapper;
+import ai.startree.thirdeye.rca.RootCauseAnalysisInfo;
 import ai.startree.thirdeye.rca.RootCauseAnalysisInfoFetcher;
+import ai.startree.thirdeye.rootcause.events.EventScoring;
+import ai.startree.thirdeye.spi.ThirdEyePrincipal;
+import ai.startree.thirdeye.spi.api.AnomalyApi;
+import ai.startree.thirdeye.spi.api.EventApi;
+import ai.startree.thirdeye.spi.datalayer.bao.EventManager;
+import ai.startree.thirdeye.spi.datalayer.bao.MergedAnomalyResultManager;
+import ai.startree.thirdeye.spi.datalayer.dto.EventDTO;
+import ai.startree.thirdeye.spi.datalayer.dto.MergedAnomalyResultDTO;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import io.dropwizard.auth.Auth;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiKeyAuthDefinition;
 import io.swagger.annotations.ApiKeyAuthDefinition.ApiKeyLocation;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.Authorization;
 import io.swagger.annotations.SecurityDefinition;
 import io.swagger.annotations.SwaggerDefinition;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.annotation.Nullable;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.joda.time.Interval;
+import org.joda.time.Period;
+import org.joda.time.format.ISOPeriodFormat;
 
 @Api(authorizations = {@Authorization(value = "oauth")})
 @SwaggerDefinition(securityDefinition = @SecurityDefinition(apiKeyAuthDefinitions = @ApiKeyAuthDefinition(name = HttpHeaders.AUTHORIZATION, in = ApiKeyLocation.HEADER, key = "oauth")))
@@ -19,13 +46,101 @@ import javax.ws.rs.core.MediaType;
 @Singleton
 public class RcaEventsResource {
 
+  private static final String DEFAULT_LOOKBACK = "P7D";
+  private static final String DEFAULT_LIMIT = "100";
+  private static final String DEFAULT_SCORING = "TRIANGULAR";
   private final RootCauseAnalysisInfoFetcher rootCauseAnalysisInfoFetcher;
+  private final EventManager eventDAO;
+  private final MergedAnomalyResultManager anomalyDAO;
 
   @Inject
   public RcaEventsResource(
-      final RootCauseAnalysisInfoFetcher rootCauseAnalysisInfoFetcher) {
+      final RootCauseAnalysisInfoFetcher rootCauseAnalysisInfoFetcher,
+      final EventManager eventDAO,
+      final MergedAnomalyResultManager anomalyDAO) {
     this.rootCauseAnalysisInfoFetcher = rootCauseAnalysisInfoFetcher;
+    this.eventDAO = eventDAO;
+    this.anomalyDAO = anomalyDAO;
   }
 
-  // todo cyril I am here
+  @GET
+  @Path("/calendar/anomaly/{id}")
+  @ApiOperation(value = "Returns calendar events related to the anomaly.")
+  public Response getCalendarEvents(
+      @ApiParam(hidden = true) @Auth ThirdEyePrincipal principal,
+      @ApiParam(value = "id of the anomaly") @PathParam("id") long anomalyId,
+      @ApiParam(value = "type") @QueryParam("type") @Nullable String type,
+      @ApiParam(value = "scoring") @QueryParam("scoring") @DefaultValue(DEFAULT_SCORING) EventScoring scoring,
+      @ApiParam(value = "limit") @QueryParam("limit") @DefaultValue(DEFAULT_LIMIT) int limit,
+      @ApiParam(value = "lookaround") @QueryParam("lookaround") @DefaultValue(DEFAULT_LOOKBACK) String lookaround)
+      throws IOException, ClassNotFoundException {
+
+    final Period lookaroundPeriod = Period.parse(lookaround, ISOPeriodFormat.standard());
+    final RootCauseAnalysisInfo rootCauseAnalysisInfo = rootCauseAnalysisInfoFetcher.getRootCauseAnalysisInfo(
+        anomalyId);
+    final Interval anomalyInterval = new Interval(
+        rootCauseAnalysisInfo.getMergedAnomalyResultDTO().getStartTime(),
+        rootCauseAnalysisInfo.getMergedAnomalyResultDTO().getEndTime(),
+        rootCauseAnalysisInfo.getTimezone()
+    );
+    final long startWithLookback = anomalyInterval.getStart()
+        .minus(lookaroundPeriod)
+        .getMillis();
+    final long endWithLookahead = Math.max(anomalyInterval.getStart()
+        .plus(lookaroundPeriod)
+        .getMillis(), anomalyInterval.getEnd().getMillis());
+
+    final List<EventDTO> events = type != null ?
+        eventDAO.findEventsBetweenTimeRangeWithType(type, startWithLookback, endWithLookahead) :
+        eventDAO.findEventsBetweenTimeRange(startWithLookback, endWithLookahead);
+
+    events.sort(Comparator.comparingDouble(dto -> scoring.score(anomalyInterval,
+        new Interval(dto.getStartTime(), dto.getEndTime(), anomalyInterval.getChronology()),
+        lookaroundPeriod)));
+
+    final List<EventApi> eventApis = events.stream().limit(limit).map(ApiBeanMapper::toApi).collect(
+        Collectors.toList());
+    return Response.ok(eventApis).build();
+  }
+
+  @GET
+  @Path("/anomalies/anomaly/{id}")
+  @ApiOperation(value = "Returns anomalies related to the anomaly.")
+  public Response getAnomaliesEvents(
+      @ApiParam(hidden = true) @Auth ThirdEyePrincipal principal,
+      @ApiParam(value = "id of the anomaly") @PathParam("id") long anomalyId,
+      @ApiParam(value = "scoring") @QueryParam("scoring") @DefaultValue(DEFAULT_SCORING) EventScoring scoring,
+      @ApiParam(value = "limit") @QueryParam("limit") @DefaultValue(DEFAULT_LIMIT) int limit,
+      @ApiParam(value = "lookaround") @QueryParam("lookaround") @DefaultValue(DEFAULT_LOOKBACK) String lookaround)
+      throws IOException, ClassNotFoundException {
+
+    final Period lookaroundPeriod = Period.parse(lookaround, ISOPeriodFormat.standard());
+    final RootCauseAnalysisInfo rootCauseAnalysisInfo = rootCauseAnalysisInfoFetcher.getRootCauseAnalysisInfo(
+        anomalyId);
+    final Interval anomalyInterval = new Interval(
+        rootCauseAnalysisInfo.getMergedAnomalyResultDTO().getStartTime(),
+        rootCauseAnalysisInfo.getMergedAnomalyResultDTO().getEndTime(),
+        rootCauseAnalysisInfo.getTimezone()
+    );
+    final long startWithLookback = anomalyInterval.getStart()
+        .minus(lookaroundPeriod)
+        .getMillis();
+    final long endWithLookahead = Math.max(anomalyInterval.getStart()
+        .plus(lookaroundPeriod)
+        .getMillis(), anomalyInterval.getEnd().getMillis());
+
+    final List<MergedAnomalyResultDTO> anomalies = anomalyDAO.findByTime(startWithLookback,
+        endWithLookahead);
+
+    anomalies.sort(Comparator.comparingDouble(dto -> scoring.score(anomalyInterval,
+        new Interval(dto.getStartTime(), dto.getEndTime(), anomalyInterval.getChronology()),
+        lookaroundPeriod)));
+
+    final List<AnomalyApi> anomalyApis = anomalies.stream()
+        .limit(limit)
+        .filter(dto -> !dto.getId().equals(anomalyId))
+        .map(ApiBeanMapper::toApi)
+        .collect(Collectors.toList());
+    return Response.ok(anomalyApis).build();
+  }
 }

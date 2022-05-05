7ecb8c9fd65e89ab1c1cbb11fe91cf9277c46b0c
Steven Pham <steven@startree.ai>
2022-05-04 18:33:12 -0700

    chore(ui): more fixes with merge

--- thirdeye-ui/src/app/rest/app-config/app-config.action.ts
+++ thirdeye-ui/src/app/rest/app-config/app-config.action.ts
@@ -4,12 +4,12 @@ import { GetAppConfiguration } from "./app-config.interfaces";
 import { getAppConfiguration as getAppConfigurationRest } from "./app-config.rest";
 
 export const useGetAppConfiguration = (): GetAppConfiguration => {
-    const { data, makeRequest, status, errorMessage } =
+    const { data, makeRequest, status, errorMessages } =
         useHTTPAction<AppConfiguration>(getAppConfigurationRest);
 
     const getAppConfiguration = (): Promise<AppConfiguration | undefined> => {
         return makeRequest();
     };
 
-    return { appConfig: data, getAppConfiguration, status, errorMessage };
+    return { appConfig: data, getAppConfiguration, status, errorMessages };
 };

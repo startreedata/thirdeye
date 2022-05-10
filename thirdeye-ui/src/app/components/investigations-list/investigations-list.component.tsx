e059b7d8129f6ddda08d38e92ace6be99ae52923
Steven Pham <2080348+spham92@users.noreply.github.com>
2022-05-10 10:25:35 -0700

    feat(ui): components relating to modifying investigations (#440)
    * feat(ui): components relating to modifying investigations
    
    * feat(ui): add icon to investigation name in list
    
    * fix(ui): move icon to right of text for link item
    
    * feat(ui): link to anomaly rca page
    
    * chore(ui): replace string with enum
    
    Co-authored-by: Steven Pham <steven@startree.ai>

---
+++ thirdeye-ui/src/app/components/investigations-list/investigations-list.component.tsx
@@ -0,0 +1,118 @@
+import { Grid, Link, Typography } from "@material-ui/core";
+import OpenInNewIcon from "@material-ui/icons/OpenInNew";
+import React, { FunctionComponent, ReactElement } from "react";
+import { useTranslation } from "react-i18next";
+import {
+    DataGridScrollV1,
+    DataGridV1,
+    PageContentsCardV1,
+} from "../../platform/components";
+import { formatDateAndTimeV1 } from "../../platform/utils";
+import { Investigation, SavedStateKeys } from "../../rest/dto/rca.interfaces";
+import { getRootCauseAnalysisForAnomalyInvestigatePath } from "../../utils/routes/routes.util";
+import { useInvestigationListStyles } from "./investigation-list.styles";
+import { InvestigationsListProps } from "./investigations-list.interfaces";
+
+export const InvestigationsList: FunctionComponent<InvestigationsListProps> = ({
+    investigations,
+}) => {
+    const classes = useInvestigationListStyles();
+    const { t } = useTranslation();
+
+    const renderLink = (
+        cellValue: Record<string, unknown>,
+        data: Investigation
+    ): ReactElement => {
+        if (data.anomaly) {
+            const url = `${getRootCauseAnalysisForAnomalyInvestigatePath(
+                data.anomaly.id as number
+            )}?${data.uiMetadata[SavedStateKeys.QUERY_SEARCH_STRING]}`;
+
+            return (
+                <Link href={url} target="blank">
+                    <Grid container alignItems="center">
+                        <Grid item>{cellValue}</Grid>
+                        <Grid item>
+                            <OpenInNewIcon
+                                className={classes.iconAlignVertical}
+                                fontSize="small"
+                            />
+                        </Grid>
+                    </Grid>
+                    :
+                </Link>
+            );
+        }
+
+        /**
+         * In the rare event that anomaly data is missing in the Investigation
+         * object, just render the name
+         */
+        return <span>{cellValue}</span>;
+    };
+
+    const investigationsColumns = [
+        {
+            key: "name",
+            dataKey: "name",
+            header: t("label.name"),
+            minWidth: 0,
+            flex: 1.5,
+            sortable: true,
+            customCellRenderer: renderLink,
+            customCellTooltipRenderer: () => (
+                <span>{t("label.view-investigation")}</span>
+            ),
+        },
+        {
+            key: "createdBy.principal",
+            dataKey: "createdBy.principal",
+            header: t("label.created-by"),
+            cellTooltip: false,
+            minWidth: 0,
+            flex: 1,
+            sortable: true,
+        },
+        {
+            key: "created",
+            dataKey: "created",
+            header: t("label.created"),
+            cellTooltip: false,
+            minWidth: 0,
+            flex: 1,
+            sortable: true,
+            customCellRenderer: (value: Record<string, unknown>) =>
+                formatDateAndTimeV1(Number(value)),
+        },
+        {
+            key: "updated",
+            dataKey: "updated",
+            header: t("label.last-updated"),
+            cellTooltip: false,
+            minWidth: 0,
+            flex: 1,
+            sortable: true,
+            customCellRenderer: (value: Record<string, unknown>) =>
+                formatDateAndTimeV1(Number(value)),
+        },
+    ];
+
+    return (
+        <PageContentsCardV1 disablePadding fullHeight>
+            <DataGridV1<Investigation>
+                disableSearch
+                disableSelection
+                hideBorder
+                columns={investigationsColumns}
+                data={investigations as Investigation[]}
+                rowKey="id"
+                scroll={DataGridScrollV1.Body}
+                toolbarComponent={
+                    <Typography variant="h6">
+                        {t("label.saved-investigations-anomaly")}
+                    </Typography>
+                }
+            />
+        </PageContentsCardV1>
+    );
+};

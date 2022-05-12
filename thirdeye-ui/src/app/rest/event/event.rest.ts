c15ec9e38c6f1affee02f7da82b24063ff30ce22
Chirag Madlani <chirag.m@deuexsolutions.com>
2022-05-12 11:57:49 +0530

    feat(ui): support for events in rca page

---
+++ thirdeye-ui/src/app/rest/event/event.rest.ts
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { Event } from "../dto/event.interfaces";
+
+const BASE_URL_EVENT = "/api/events";
+
+export const getEvent = async (id: number): Promise<Event> => {
+    const response = await axios.get(`${BASE_URL_EVENT}/${id}`);
+
+    return response.data;
+};
+
+export const getAllEvents = async (
+    startTime?: string,
+    endTime?: string,
+    type?: string
+): Promise<Event[]> => {
+    const queryParams = new URLSearchParams();
+
+    if (startTime) {
+        queryParams.set("startTime", `${startTime}`);
+    }
+
+    if (endTime) {
+        queryParams.set("endTime", `${endTime}`);
+    }
+
+    if (type) {
+        queryParams.set("metadata.dataset.name", type);
+    }
+
+    const response = await axios.get(
+        `${BASE_URL_EVENT}?${queryParams.toString()}`
+    );
+
+    return response.data;
+};
+
+export const createEvent = async (event: EditableEvent): Promise<Event> => {
+    const response = await axios.post(BASE_URL_EVENT, [event]);
+
+    return response.data[0];
+};
+
+export const createEvents = async (events: Event[]): Promise<Event[]> => {
+    const response = await axios.post(BASE_URL_EVENT, events);
+
+    return response.data;
+};
+
+export const updateEvent = async (event: Event): Promise<Event> => {
+    const response = await axios.put(BASE_URL_EVENT, [event]);
+
+    return response.data[0];
+};
+
+export const updateEvents = async (events: Event[]): Promise<Event[]> => {
+    const response = await axios.put(BASE_URL_EVENT, events);
+
+    return response.data;
+};
+
+export const deleteEvent = async (id: number): Promise<Event> => {
+    const response = await axios.delete(`${BASE_URL_EVENT}/${id}`);
+
+    return response.data;
+};

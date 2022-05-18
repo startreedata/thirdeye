2a42cc8d324e42b6ed966618942269cd00bb346f
Steven Pham <2080348+spham92@users.noreply.github.com>
2022-05-18 09:11:08 -0700

    feat(ui): semantic versioning for release to artifactory (#456)
    Co-authored-by: Steven Pham <steven@startree.ai>

---
+++ thirdeye-ui/src/app/utils/version/version.util.ts
@@ -0,0 +1,7 @@
+// Returns version number of current release
+// Version number placeholder is replaced by the precise release version number at the time of
+// release
+// Relevant configuration located in semantic-release configuration file
+export const getThirdEyeUiVersion = (): string => {
+    return "0.0.0-development-thirdeye-ui"; // Version number placeholder
+};

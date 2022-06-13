f1ea01e60372590ffde56a1ef40f7a0bde4b5102
Shounak kulkarni <shounak@startree.ai>
2022-06-13 18:22:35 +0530

    [core] Expose metrics for task counts of each task status.

--- thirdeye-persistence/src/main/java/ai/startree/thirdeye/datalayer/bao/TaskManagerImpl.java
+++ thirdeye-persistence/src/main/java/ai/startree/thirdeye/datalayer/bao/TaskManagerImpl.java
@@ -55,15 +55,8 @@ public class TaskManagerImpl extends AbstractManagerImpl<TaskDTO> implements Tas
   private static final Logger LOG = LoggerFactory.getLogger(TaskManagerImpl.class);
 
   @Inject
-  public TaskManagerImpl(final GenericPojoDao genericPojoDao,
-      final MetricRegistry metricRegistry) {
+  public TaskManagerImpl(final GenericPojoDao genericPojoDao) {
     super(TaskDTO.class, genericPojoDao);
-    metricRegistry.register("taskCountTotal", new CachedGauge<Long>(1, TimeUnit.MINUTES) {
-      @Override
-      protected Long loadValue() {
-        return count();
-      }
-    });
   }
 
   @Override

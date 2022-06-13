52d6bde49311926440e1074326245b7cb4fce889
Shounak kulkarni <shounak@startree.ai>
2022-06-13 11:39:19 +0530

    header changes revert

--- thirdeye-spi/src/main/java/ai/startree/thirdeye/spi/datalayer/bao/RootcauseTemplateManager.java
+++ thirdeye-spi/src/main/java/ai/startree/thirdeye/spi/datalayer/bao/RootcauseTemplateManager.java
@@ -1,16 +1,8 @@
 /*
- * Copyright 2022 StarTree Inc
- *
- * Licensed under the StarTree Community License (the "License"); you may not use
- * this file except in compliance with the License. You may obtain a copy of the
- * License at http://www.startree.ai/legal/startree-community-license
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT * WARRANTIES OF ANY KIND,
- * either express or implied.
- * See the License for the specific language governing permissions and limitations under
- * the License.
+ * Copyright (c) 2022 StarTree Inc. All rights reserved.
+ * Confidential and Proprietary Information of StarTree Inc.
  */
+
 package ai.startree.thirdeye.spi.datalayer.bao;
 
 import ai.startree.thirdeye.spi.datalayer.dto.RootcauseTemplateDTO;

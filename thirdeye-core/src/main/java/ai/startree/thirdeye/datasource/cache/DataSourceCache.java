a93fc11a7a03ba539ecf6ab964bc4caf019eb656
Shounak kulkarni <shounak@startree.ai>
2022-06-13 11:36:24 +0530

    Revert "[thirdeye] Added license header to files which were skipped earlier."
    This reverts commit 5ee2fc37af549cf5b5fdfc04ccf52bc48d94fb53.

--- thirdeye-core/src/main/java/ai/startree/thirdeye/datasource/cache/DataSourceCache.java
+++ thirdeye-core/src/main/java/ai/startree/thirdeye/datasource/cache/DataSourceCache.java
@@ -11,6 +11,11 @@
  * See the License for the specific language governing permissions and limitations under
  * the License.
  */
+/*
+ * Copyright (c) 2022 StarTree Inc. All rights reserved.
+ * Confidential and Proprietary Information of StarTree Inc.
+ */
+
 package ai.startree.thirdeye.datasource.cache;
 
 import static com.google.common.base.Preconditions.checkState;

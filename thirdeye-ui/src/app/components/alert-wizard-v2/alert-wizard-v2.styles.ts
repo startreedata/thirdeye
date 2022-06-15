ec827715d0ce5fcd6a47b41b99712345c8fe2ed9
Steven Pham <2080348+spham92@users.noreply.github.com>
2022-06-15 10:27:50 -0700

    feat(ui): cron input component (#521)
    * feat(ui): cron input component
    
    * fix(ui): use FormHelperText
    
    Co-authored-by: Steven Pham <steven@startree.ai>

---
+++ thirdeye-ui/src/app/components/alert-wizard-v2/alert-wizard-v2.styles.ts
@@ -0,0 +1,7 @@
+import { makeStyles } from "@material-ui/core";
+
+export const useAlertWizardV2Styles = makeStyles((theme) => ({
+    label: {
+        color: theme.palette.text.primary,
+    },
+}));

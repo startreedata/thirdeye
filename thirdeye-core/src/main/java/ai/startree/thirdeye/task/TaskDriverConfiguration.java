d1e95fcc1340f8f7c34325ae32d5d6a775662dbb
Shounak kulkarni <shounak@startree.ai>
2022-06-21 13:13:48 +0530

    [core] Changed default heartbeatInterval to 30 seconds

--- thirdeye-core/src/main/java/ai/startree/thirdeye/task/TaskDriverConfiguration.java
+++ thirdeye-core/src/main/java/ai/startree/thirdeye/task/TaskDriverConfiguration.java
@@ -16,7 +16,7 @@ public class TaskDriverConfiguration {
   private Duration taskFailureDelay = Duration.ofSeconds(30);
   private Duration randomDelayCap = Duration.ofSeconds(15);
   private Duration maxTaskRunTime = Duration.ofHours(6);
-  private Duration heartbeatInterval = Duration.ofMinutes(1);
+  private Duration heartbeatInterval = Duration.ofSeconds(30);
 
   private int taskFetchSizeCap = 50;
   private int maxParallelTasks = 5;

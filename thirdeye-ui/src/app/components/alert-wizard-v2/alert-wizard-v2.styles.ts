7ecc9aacf22057d11dcdc32b0260fa0d05ed6c9c
Steven Pham <2080348+spham92@users.noreply.github.com>
2022-06-23 11:08:08 -0700

    chore(ui): add css styles for alert creation ui and new translations (#553)
    Co-authored-by: Steven Pham <steven@startree.ai>

--- thirdeye-ui/src/app/components/alert-wizard-v2/alert-wizard-v2.styles.ts
+++ thirdeye-ui/src/app/components/alert-wizard-v2/alert-wizard-v2.styles.ts
@@ -12,9 +12,21 @@
  * the License.
  */
 import { makeStyles } from "@material-ui/core";
+import { darken, lighten } from "@material-ui/core/styles/colorManipulator";
 
 export const useAlertWizardV2Styles = makeStyles((theme) => ({
     label: {
         color: theme.palette.text.primary,
+    },
+    autoCompleteInput: {
+        paddingRight: "45px",
+    },
+    infoAlert: {
+        backgroundColor: lighten(theme.palette.primary.light, 0.9),
+        color: darken(theme.palette.primary.light, 0.5),
+    },
+    warningAlert: {
+        backgroundColor: lighten(theme.palette.warning.light, 0.9),
+        color: darken(theme.palette.warning.light, 0.5),
     },
 }));

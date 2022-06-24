cc1a53906462dc78d879e1e49dd1a9679498e722
Steven Pham <2080348+spham92@users.noreply.github.com>
2022-06-24 16:05:19 -0700

    feat(ui): alert creation simple and advance page (#558)
    * feat(ui): alert creation simple and advance page
    
    * fix(ui): only style active
    
    * fix(ui): remove btnActive
    
    Co-authored-by: Steven Pham <steven@startree.ai>

---
+++ thirdeye-ui/src/app/pages/alerts-create-page/alerts-create-simple-page/alerts-create-simple-page.component.tsx
@@ -0,0 +1,58 @@
+import { Grid } from "@material-ui/core";
+import React, { FunctionComponent } from "react";
+import { useOutletContext } from "react-router-dom";
+import { AlertDetails } from "../../../components/alert-wizard-v2/alert-details/alert-details.component";
+import { AlertNotifications } from "../../../components/alert-wizard-v2/alert-notifications/alert-notifications.component";
+import { AlertTemplate } from "../../../components/alert-wizard-v2/alert-template/alert-template.component";
+import { AlertTemplate as AlertTemplateType } from "../../../rest/dto/alert-template.interfaces";
+import { EditableAlert } from "../../../rest/dto/alert.interfaces";
+import { SubscriptionGroup } from "../../../rest/dto/subscription-group.interfaces";
+
+export const AlertsCreateSimplePage: FunctionComponent = () => {
+    const [
+        alert,
+        onAlertPropertyChange,
+        ,
+        onSubscriptionGroupsChange,
+        selectedAlertTemplate,
+        setSelectedAlertTemplate,
+        alertTemplateOptions,
+    ] =
+        useOutletContext<
+            [
+                EditableAlert,
+                (contents: Partial<EditableAlert>) => void,
+                SubscriptionGroup[],
+                (groups: SubscriptionGroup[]) => void,
+                AlertTemplateType,
+                (newAlertTemplate: AlertTemplateType) => void,
+                AlertTemplateType[]
+            ]
+        >();
+
+    return (
+        <>
+            <Grid item xs={12}>
+                <AlertDetails
+                    alert={alert}
+                    onAlertPropertyChange={onAlertPropertyChange}
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <AlertTemplate
+                    alert={alert}
+                    alertTemplateOptions={alertTemplateOptions}
+                    selectedAlertTemplate={selectedAlertTemplate}
+                    setSelectedAlertTemplate={setSelectedAlertTemplate}
+                    onAlertPropertyChange={onAlertPropertyChange}
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <AlertNotifications
+                    alert={alert}
+                    onSubscriptionGroupsChange={onSubscriptionGroupsChange}
+                />
+            </Grid>
+        </>
+    );
+};

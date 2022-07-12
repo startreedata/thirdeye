65e7f2f92f5db4ee44894ef4bfd84632dc3c3cc8
Shounak kulkarni <shounak@startree.ai>
2022-07-12 21:29:04 +0530

    [server] Analytics. Uniquely monitored metrics by active alerts. (#574)
    * [core] Created AlertMonitoringService. Ability to fetch unique monitored metrics.
    
    * [server] Exposed count of uniquely monitored metrics under app analytics
    
    * [core] Moved analytics related operations under AppAnalyticsService.
    
    Removed the separate endpoint for monitored-metrics.
    
    * [server] Moved AppAnalyticsService to server under ai.startree.thirdeye.core
    
    * [spi] Renamed uniqueMonitoredMetrics to nMonitoredMetrics in AppAnalyticsApi
    
    * Added missing license headers.
    
    * [server] Moved MonitoredMetricWrapper to ai.startree.thirdeye.core inside thirdeye-server

---
+++ thirdeye-server/src/main/java/ai/startree/thirdeye/core/AppAnalyticsService.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2022 StarTree Inc
+ *
+ * Licensed under the StarTree Community License (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at http://www.startree.ai/legal/startree-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT * WARRANTIES OF ANY KIND,
+ * either express or implied.
+ * See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package ai.startree.thirdeye.core;
+
+import ai.startree.thirdeye.alert.AlertTemplateRenderer;
+import ai.startree.thirdeye.spi.datalayer.bao.AlertManager;
+import ai.startree.thirdeye.spi.datalayer.dto.AlertDTO;
+import ai.startree.thirdeye.spi.datalayer.dto.AlertMetadataDTO;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.joda.time.Interval;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class AppAnalyticsService {
+
+  private static final Logger log = LoggerFactory.getLogger(AppAnalyticsService.class);
+
+  private final AlertManager alertManager;
+  private final AlertTemplateRenderer renderer;
+
+  @Inject
+  public AppAnalyticsService(final AlertManager alertManager,
+      final AlertTemplateRenderer renderer) {
+    this.alertManager = alertManager;
+    this.renderer = renderer;
+  }
+
+  public Integer uniqueMonitoredMetricsCount() {
+    return getUniqueMonitoredMetrics().size();
+  }
+  private Set<MonitoredMetricWrapper> getUniqueMonitoredMetrics() {
+    return alertManager.findAllActive().stream()
+        .map(this::getMetadata)
+        .filter(Objects::nonNull)
+        .map(this::wrapMonitoredMetric)
+        .collect(Collectors.toSet());
+  }
+
+  private AlertMetadataDTO getMetadata(final AlertDTO alertDTO) {
+    try {
+      // Interval does not have significance in this case, just a placeholder.
+      return renderer.renderAlert(alertDTO, new Interval(1L, 2L)).getMetadata();
+    } catch (final IOException | ClassNotFoundException e) {
+      log.warn(String.format("Trouble while rendering alert, %s. id : %d", alertDTO.getName(), alertDTO.getId()), e);
+      return null;
+    }
+  }
+
+  private MonitoredMetricWrapper wrapMonitoredMetric(final AlertMetadataDTO metadata) {
+    return new MonitoredMetricWrapper()
+        .setMetric(metadata.getMetric().getName())
+        .setDataset(metadata.getDataset().getName())
+        .setDatasource(metadata.getDatasource().getName());
+  }
+}

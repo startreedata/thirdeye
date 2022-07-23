a4e7ba1159563647b9ced3232d98a40c1d874ace
Cyril de Catheu <cyril@startree.ai>
2022-07-23 18:55:11 +0200

    [spi] add vectorized isAnomaly in Pattern

--- thirdeye-spi/src/main/java/ai/startree/thirdeye/spi/detection/Pattern.java
+++ thirdeye-spi/src/main/java/ai/startree/thirdeye/spi/detection/Pattern.java
@@ -13,6 +13,10 @@
  */
 package ai.startree.thirdeye.spi.detection;
 
+import ai.startree.thirdeye.spi.dataframe.BooleanSeries;
+import ai.startree.thirdeye.spi.dataframe.DoubleSeries;
+import ai.startree.thirdeye.spi.dataframe.Series.DoubleConditional;
+
 /**
  * Up or down detection pattern
  */
@@ -37,11 +41,21 @@ public enum Pattern {
     @Override
     public boolean isAnomaly(final double currentValue, final double lowerBound,
         final double upperBound) {
-      return UP.isAnomaly(currentValue, lowerBound, upperBound) ||
-          DOWN.isAnomaly(currentValue, lowerBound, upperBound);
+      return UP.isAnomaly(currentValue, lowerBound, upperBound) || DOWN.isAnomaly(currentValue,
+          lowerBound,
+          upperBound);
     }
   };
 
   public abstract boolean isAnomaly(final double currentValue, final double lowerBound,
       final double upperBound);
+
+  public BooleanSeries isAnomaly(final DoubleSeries currentValues, final DoubleSeries lowerBounds,
+      final DoubleSeries upperBounds) {
+    final DoubleConditional isAnomalyFn = doubles -> this.isAnomaly(doubles[0],
+        doubles[1],
+        doubles[2]);
+
+    return DoubleSeries.map(isAnomalyFn, currentValues, lowerBounds, upperBounds);
+  }
 }

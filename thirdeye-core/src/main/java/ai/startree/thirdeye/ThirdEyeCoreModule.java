21f8a67747443d098ae4339293c8778f4ff39d72
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2022-07-25 16:24:59 -0700

    [core] Removed ThirdEyeServerConfigurationModule.
    Goal ThirdEyeServerConfiguration will be moved to thirdeye-server module

--- thirdeye-core/src/main/java/ai/startree/thirdeye/ThirdEyeCoreModule.java
+++ thirdeye-core/src/main/java/ai/startree/thirdeye/ThirdEyeCoreModule.java
@@ -13,15 +13,28 @@
  */
 package ai.startree.thirdeye;
 
+import ai.startree.thirdeye.auth.AuthConfiguration;
+import ai.startree.thirdeye.auth.OAuthConfiguration;
 import ai.startree.thirdeye.config.ThirdEyeServerConfiguration;
-import ai.startree.thirdeye.config.ThirdEyeServerConfigurationModule;
+import ai.startree.thirdeye.config.TimeConfiguration;
+import ai.startree.thirdeye.config.UiConfiguration;
 import ai.startree.thirdeye.datalayer.ThirdEyePersistenceModule;
+import ai.startree.thirdeye.datasource.AutoOnboardConfiguration;
 import ai.startree.thirdeye.datasource.loader.DefaultAggregationLoader;
 import ai.startree.thirdeye.detection.DefaultDataProvider;
+import ai.startree.thirdeye.detection.anomaly.monitor.MonitorConfiguration;
+import ai.startree.thirdeye.detection.cache.CacheConfig;
+import ai.startree.thirdeye.events.HolidayEventsLoaderConfiguration;
+import ai.startree.thirdeye.notification.NotificationConfiguration;
+import ai.startree.thirdeye.rootcause.configuration.RcaConfiguration;
+import ai.startree.thirdeye.scheduler.ThirdEyeSchedulerConfiguration;
 import ai.startree.thirdeye.spi.datasource.loader.AggregationLoader;
 import ai.startree.thirdeye.spi.detection.DataProvider;
+import ai.startree.thirdeye.task.TaskDriverConfiguration;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 import com.google.inject.Scopes;
+import com.google.inject.Singleton;
 import org.apache.tomcat.jdbc.pool.DataSource;
 
 public class ThirdEyeCoreModule extends AbstractModule {
@@ -38,9 +51,68 @@ public class ThirdEyeCoreModule extends AbstractModule {
   @Override
   protected void configure() {
     install(new ThirdEyePersistenceModule(dataSource));
-    install(new ThirdEyeServerConfigurationModule(configuration));
 
     bind(DataProvider.class).to(DefaultDataProvider.class).in(Scopes.SINGLETON);
     bind(AggregationLoader.class).to(DefaultAggregationLoader.class).in(Scopes.SINGLETON);
+
+    bind(CacheConfig.class)
+        .toProvider(configuration::getCacheConfig)
+        .in(Scopes.SINGLETON);
+
+    bind(RcaConfiguration.class)
+        .toProvider(configuration::getRcaConfiguration)
+        .in(Scopes.SINGLETON);
+
+    bind(ThirdEyeSchedulerConfiguration.class)
+        .toProvider(configuration::getSchedulerConfiguration)
+        .in(Scopes.SINGLETON);
+
+    bind(TaskDriverConfiguration.class)
+        .toProvider(configuration::getTaskDriverConfiguration)
+        .in(Scopes.SINGLETON);
+
+    bind(UiConfiguration.class)
+        .toProvider(configuration::getUiConfiguration)
+        .in(Scopes.SINGLETON);
+
+    bind(AuthConfiguration.class)
+        .toProvider(configuration::getAuthConfiguration)
+        .in(Scopes.SINGLETON);
+
+    bind(NotificationConfiguration.class)
+        .toProvider(configuration::getNotificationConfiguration)
+        .in(Scopes.SINGLETON);
+
+    bind(TimeConfiguration.class)
+        .toProvider(configuration::getTimeConfiguration)
+        .in(Scopes.SINGLETON);
+  }
+
+  @Singleton
+  @Provides
+  public HolidayEventsLoaderConfiguration getHolidayEventsLoaderConfiguration(
+      ThirdEyeSchedulerConfiguration configuration) {
+    return configuration.getHolidayEventsLoaderConfiguration();
+  }
+
+  @Singleton
+  @Provides
+  public AutoOnboardConfiguration getAutoOnboardConfiguration(
+      ThirdEyeSchedulerConfiguration configuration) {
+    return configuration.getAutoOnboardConfiguration();
+  }
+
+  @Singleton
+  @Provides
+  public MonitorConfiguration getMonitorConfiguration(
+      ThirdEyeSchedulerConfiguration schedulerConfiguration) {
+    return schedulerConfiguration.getMonitorConfiguration();
+  }
+
+  @Singleton
+  @Provides
+  public OAuthConfiguration getOAuthConfig(
+      AuthConfiguration authConfiguration) {
+    return authConfiguration.getOAuthConfig();
   }
 }

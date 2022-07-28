201795b17450ef4d42b507d5b3274810681b5f48
Cyril de Catheu <cyril@startree.ai>
2022-07-28 21:42:31 +0200

    -10 seconds

--- thirdeye-persistence/src/test/java/ai/startree/thirdeye/datalayer/bao/TestRootcauseTemplateManager.java
+++ thirdeye-persistence/src/test/java/ai/startree/thirdeye/datalayer/bao/TestRootcauseTemplateManager.java
@@ -19,7 +19,8 @@ import ai.startree.thirdeye.spi.datalayer.dto.RootcauseTemplateDTO;
 import java.util.ArrayList;
 import java.util.List;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class TestRootcauseTemplateManager {
@@ -27,9 +28,14 @@ public class TestRootcauseTemplateManager {
   private final static String TEMPLATE_NAME = "test_template_";
   private RootcauseTemplateManager templateDao;
 
-  @BeforeMethod
-  void beforeMethod() {
+  @BeforeClass
+  void beforeClass() {
     templateDao = new MySqlTestDatabase().createInjector().getInstance(RootcauseTemplateManager.class);
+  }
+
+  @AfterMethod
+  void cleanCreatedEntities() {
+    templateDao.findAll().forEach(templateDao::delete);
   }
 
   @Test

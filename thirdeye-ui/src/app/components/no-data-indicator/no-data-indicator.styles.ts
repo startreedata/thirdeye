9d34ce4ebde5076642f1b904b115e130d3bc09ff
Steven Pham <2080348+spham92@users.noreply.github.com>
2022-09-22 13:49:59 -0700

    fix(ui): cetner no data indicator

--- thirdeye-ui/src/app/components/no-data-indicator/no-data-indicator.styles.ts
+++ thirdeye-ui/src/app/components/no-data-indicator/no-data-indicator.styles.ts
@@ -12,26 +12,11 @@
  * the License.
  */
 import { makeStyles } from "@material-ui/core";
-import { Border } from "../../utils/material-ui/border.util";
 
-export const useNoDataIndicatorStyles = makeStyles((theme) => ({
+export const useNoDataIndicatorStyles = makeStyles(() => ({
     noDataIndicator: {
         height: "100%",
         width: "100%",
-        display: "flex",
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    icon: {
-        display: "flex",
-    },
-    iconWithText: {
-        display: "flex",
-        borderRight: Border.BORDER_DEFAULT,
-        paddingRight: theme.spacing(2),
-    },
-    text: {
-        paddingLeft: theme.spacing(2),
+        textAlign: "center",
     },
 }));
--- thirdeye-ui/webpack.config.dev.js
+++ thirdeye-ui/webpack.config.dev.js
@@ -134,8 +134,7 @@ module.exports = {
         proxy: [
             {
                 context: "/api",
-                target:
-                    process.env.TE_DEV_PROXY_SERVER || "http://localhost:8080/",
+                target: "http://localhost:8080/",
                 changeOrigin: true,
             },
         ],

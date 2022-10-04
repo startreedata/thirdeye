894abed859e33192265a9a00bc9ebb0a6613197d
Steven Pham <2080348+spham92@users.noreply.github.com>
2022-10-03 17:31:23 -0700

    refactor(ui): make alert editing callback functions reuseable

--- thirdeye-ui/src/app/components/alert-wizard-v2/alert-template/alert-template.interfaces.ts
+++ thirdeye-ui/src/app/components/alert-wizard-v2/alert-template/alert-template.interfaces.ts
@@ -17,7 +17,7 @@ import { EditableAlert } from "../../../rest/dto/alert.interfaces";
 export interface AlertTemplateProps {
     alert: EditableAlert;
     onAlertPropertyChange: (contents: Partial<EditableAlert>) => void;
-    selectedAlertTemplate: AlertTemplate;
+    selectedAlertTemplate: AlertTemplate | null;
     setSelectedAlertTemplate: (newAlertTemplate: AlertTemplate | null) => void;
     alertTemplateOptions: AlertTemplate[];
 }

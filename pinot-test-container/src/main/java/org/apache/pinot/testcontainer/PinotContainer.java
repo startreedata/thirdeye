c41e2207701651370d9426b5c69f05634cea060e
Cyril de Catheu <cyril@startree.ai>
2022-10-20 10:58:35 +0200

    [pinot-test-container] upgrade pinot version from 0.9.3 to 0.11.0

--- pinot-test-container/src/main/java/org/apache/pinot/testcontainer/PinotContainer.java
+++ pinot-test-container/src/main/java/org/apache/pinot/testcontainer/PinotContainer.java
@@ -23,112 +23,146 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.containers.wait.strategy.WaitAllStrategy;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
 
 public class PinotContainer extends GenericContainer<PinotContainer> {
 
-    public static final Logger log = LoggerFactory.getLogger(PinotContainer.class);
+  public static final Logger log = LoggerFactory.getLogger(PinotContainer.class);
 
-    public static final int DEFAULT_ZOOKEEPER_PORT = 2123;
-    public static final int DEFAULT_CONTROLLER_HTTP_PORT = 9000;
-    public static final int DEFAULT_BROKER_HTTP_PORT = 8000;
+  public static final int DEFAULT_ZOOKEEPER_PORT = 2123;
+  public static final int DEFAULT_CONTROLLER_HTTP_PORT = 9000;
+  public static final int DEFAULT_BROKER_HTTP_PORT = 8000;
 
+  private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse(
+      "apachepinot/pinot");
+  private static final String DEFAULT_TAG = "0.11.0";
+  public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+  private List<AddTable> addTables;
+  private List<ImportData> importDataList;
+  private Network network;
 
-    private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse("apachepinot/pinot");
-    private static final String DEFAULT_TAG = "0.9.3-jdk11";
-    private List<AddTable> addTables;
-    private List<ImportData> importDataList;
-    private Network network;
+  public PinotContainer() {
+    this(DEFAULT_IMAGE_NAME.withTag(DEFAULT_TAG));
+  }
 
-    public PinotContainer() {
-        this(DEFAULT_IMAGE_NAME.withTag(DEFAULT_TAG));
+  public PinotContainer(String pinotVersion) {
+    this(DEFAULT_IMAGE_NAME.withTag(pinotVersion));
+  }
+
+  public PinotContainer(final DockerImageName dockerImageName) {
+    super(dockerImageName);
+    dockerImageName.assertCompatibleWith(DEFAULT_IMAGE_NAME);
+    withExposedPorts(DEFAULT_ZOOKEEPER_PORT, DEFAULT_CONTROLLER_HTTP_PORT,
+        DEFAULT_BROKER_HTTP_PORT);
+  }
+
+  public PinotContainer(List<AddTable> addTables, List<ImportData> importDataList) {
+    this();
+    this.addTables = addTables;
+    this.importDataList = importDataList;
+  }
+
+  @Override
+  protected void configure() {
+    super.configure();
+
+    if (this.addTables != null) {
+      for (AddTable addTable : this.addTables) {
+        withCopyFileToContainer(
+            MountableFile.forHostPath(addTable.getSchemaFile().getAbsolutePath()),
+            "/tmp/" + addTable.getSchemaFile().getName());
+        withCopyFileToContainer(
+            MountableFile.forHostPath(addTable.getTableConfigFile().getAbsolutePath()),
+            "/tmp/" + addTable.getTableConfigFile().getName());
+      }
     }
 
-    public PinotContainer(String pinotVersion) {
-        this(DEFAULT_IMAGE_NAME.withTag(pinotVersion));
+    if (this.importDataList != null) {
+      for (ImportData importData : this.importDataList) {
+        withCopyFileToContainer(
+            MountableFile.forHostPath(importData.getBatchJobSpecFile().getAbsolutePath()),
+            "/tmp/" + importData.getBatchJobSpecFile().getName());
+        withCopyFileToContainer(
+            MountableFile.forHostPath(importData.getDataFile().getAbsolutePath()),
+            "/tmp/" + importData.getDataFile().getName());
+      }
     }
 
-    public PinotContainer(final DockerImageName dockerImageName) {
-        super(dockerImageName);
-        dockerImageName.assertCompatibleWith(DEFAULT_IMAGE_NAME);
-        withExposedPorts(DEFAULT_ZOOKEEPER_PORT, DEFAULT_CONTROLLER_HTTP_PORT, DEFAULT_BROKER_HTTP_PORT);
+    withCreateContainerCmdModifier(
+        createContainerCmd -> createContainerCmd.withName("pinot-quickstart-test"));
+    withCommand("QuickStart", "-type", "EMPTY");
+    waitingFor(
+        new WaitAllStrategy()
+            .withStartupTimeout(Duration.ofMinutes(5))
+            .withStrategy(waitStrategy)
+            .withStrategy(Wait.forLogMessage(".*Offline quickstart setup complete.*", 1))
+    );
+  }
+
+  public void addTables() throws IOException, InterruptedException {
+    for (AddTable table : this.addTables) {
+      final Container.ExecResult addTableResult = this.execInContainer(
+          "/opt/pinot/bin/pinot-admin.sh",
+          "AddTable",
+          "-tableConfigFile", "/tmp/" + table.getTableConfigFile().getName(),
+          "-schemaFile", "/tmp/" + table.getSchemaFile().getName(),
+          "-controllerHost", "localhost",
+          "-controllerPort", "9000",
+          "-exec"
+      );
+      final String stdout = addTableResult.getStdout();
+      log.info(stdout);
     }
-
-    public PinotContainer(List<AddTable> addTables, List<ImportData> importDataList) {
-        this();
-        this.addTables = addTables;
-        this.importDataList = importDataList;
+    for (ImportData importData : this.importDataList) {
+      final Container.ExecResult launchDataIngestionJobResult = this.execInContainer(
+          "/opt/pinot/bin/pinot-admin.sh",
+          "LaunchDataIngestionJob",
+          "-jobSpecFile", "/tmp/" + importData.getBatchJobSpecFile().getName(),
+          "-values", "-controllerHost", "localhost",
+          "-controllerPort", "9000"
+      );
+      final String stdout = launchDataIngestionJobResult.getStdout();
+      log.info(stdout);
     }
-
-    @Override
-    protected void configure() {
-        super.configure();
-
-        if(this.addTables != null) {
-            for (AddTable addTable : this.addTables) {
-                withCopyFileToContainer(MountableFile.forHostPath(addTable.getSchemaFile().getAbsolutePath()), "/tmp/");
-                withCopyFileToContainer(MountableFile.forHostPath(addTable.getTableConfigFile().getAbsolutePath()), "/tmp/");
-            }
-        }
-
-        if(this.importDataList != null) {
-            for (ImportData importData : this.importDataList) {
-                withCopyFileToContainer(MountableFile.forHostPath(importData.getBatchJobSpecFile().getAbsolutePath()), "/tmp/");
-                withCopyFileToContainer(MountableFile.forHostPath(importData.getDataFile().getAbsolutePath()), "/tmp/");
-            }
-        }
-
-        withCreateContainerCmdModifier(createContainerCmd -> createContainerCmd.withName("pinot-quickstart-test"));
-        withCommand("QuickStart", "-type", "batch");
-        waitingFor(
-            new WaitAllStrategy()
-                .withStartupTimeout(Duration.ofMinutes(5))
-                .withStrategy(waitStrategy)
-                .withStrategy(Wait.forLogMessage(".*Bootstrap baseballStats table.*", 1))
-        );
+    for (AddTable table : this.addTables) {
+      final String tableName = findTableName(table);
+      long tableSize = getTableSize(tableName);
+      while (tableSize == 0) {
+        log.info(String.format("Waiting for %s ingestion to be complete.", tableName));
+        Thread.sleep(1000);
+        tableSize = getTableSize(tableName);
+      }
     }
+  }
 
-    public void addTables() throws IOException, InterruptedException {
-        for(AddTable table: this.addTables) {
-            Container.ExecResult addTableResult = this.execInContainer(
-                "/opt/pinot/bin/pinot-admin.sh",
-                "AddTable",
-                "-tableConfigFile", "/tmp/"+ table.getTableConfigFile().getName(),
-                "-schemaFile", "/tmp/"+ table.getSchemaFile().getName(),
-                "-controllerHost", "localhost",
-                "-controllerPort", "9000",
-                "-exec"
-            );
-            String stdout = addTableResult.getStdout();
-            log.info(stdout);
-        }
-        for(ImportData importData: this.importDataList) {
-            Container.ExecResult launchDataIngestionJobResult = this.execInContainer(
-                    "/opt/pinot/bin/pinot-admin.sh",
-                    "LaunchDataIngestionJob",
-                    "-jobSpecFile", "/tmp/" + importData.getBatchJobSpecFile().getName(),
-                    "-values", "-controllerHost", "localhost",
-                "-controllerPort", "9000"
-            );
-            String stdout = launchDataIngestionJobResult.getStdout();
-            log.info(stdout);
-        }
-    }
+  private long getTableSize(final String tableName) throws IOException, InterruptedException {
+    final ExecResult checkIngestionIsFinished = this.execInContainer(
+        "curl", "-X", "GET", String.format("http://localhost:9000/tables/%s_OFFLINE/size?detailed=false", tableName)
+    );
+    final String stdout = checkIngestionIsFinished.getStdout();
+    return OBJECT_MAPPER.readTree(stdout).get("estimatedSizeInBytes").longValue();
+  }
 
-    public String getPinotBrokerUrl() {
-        return String.format("http://%s:%s", getHost(), getBrokerPort());
-    }
+  private String findTableName(final AddTable table) throws IOException {
+    // assumes the tableName is the same as the schema name
+    return OBJECT_MAPPER.readTree(table.getSchemaFile()).get("schemaName").asText();
+  }
 
-    public Integer getBrokerPort() {
-        return getMappedPort(DEFAULT_BROKER_HTTP_PORT);
-    }
+  public String getPinotBrokerUrl() {
+    return String.format("http://%s:%s", getHost(), getBrokerPort());
+  }
 
-    public Integer getControllerPort() {
-        return getMappedPort(DEFAULT_CONTROLLER_HTTP_PORT);
-    }
+  public Integer getBrokerPort() {
+    return getMappedPort(DEFAULT_BROKER_HTTP_PORT);
+  }
 
-    public Integer getZookeeperPort() {
-        return getMappedPort(DEFAULT_ZOOKEEPER_PORT);
-    }
+  public Integer getControllerPort() {
+    return getMappedPort(DEFAULT_CONTROLLER_HTTP_PORT);
+  }
+
+  public Integer getZookeeperPort() {
+    return getMappedPort(DEFAULT_ZOOKEEPER_PORT);
+  }
 }

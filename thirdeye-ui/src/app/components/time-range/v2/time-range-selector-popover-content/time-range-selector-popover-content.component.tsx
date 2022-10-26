58726f8abcc8a72900d8eafe3ba285344055a88e
Steven Pham <2080348+spham92@users.noreply.github.com>
2022-10-26 12:48:07 -0700

    feat(ui): time range components for just start and end data types

---
+++ thirdeye-ui/src/app/components/time-range/v2/time-range-selector-popover-content/time-range-selector-popover-content.component.tsx
@@ -0,0 +1,318 @@
+/**
+ * Copyright 2022 StarTree Inc
+ *
+ * Licensed under the StarTree Community License (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at http://www.startree.ai/legal/startree-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT * WARRANTIES OF ANY KIND,
+ * either express or implied.
+ * See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+import LuxonUtils from "@date-io/luxon";
+import {
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    Grid,
+    Hidden,
+    Typography,
+} from "@material-ui/core";
+import { DateTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import { MaterialUiPickersDate } from "@material-ui/pickers/typings/date";
+import classnames from "classnames";
+import React, { FunctionComponent, useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useCommonStyles } from "../../../../utils/material-ui/common.styles";
+import { getTimeRangeDuration } from "../../../../utils/time-range/time-range.util";
+import { TimeRangeList } from "../../time-range-list/time-range-list.component";
+import {
+    TimeRange,
+    TimeRangeDuration,
+} from "../../time-range-provider/time-range-provider.interfaces";
+import { TimeRangeSelect } from "../../time-range-select/time-range-select.component";
+import { DateTimePickerToolbar } from "../../time-range-selector/date-time-picker-toolbar/date-time-picker-toolbar.component";
+import { TimeRangeSelectorControls } from "../../time-range-selector/time-range-selector-controls/time-range-selector-controls.component";
+import { TimeRangeSelectorPopoverProps } from "./time-range-selector-popover-content.interfaces";
+import { useTimeRangeSelectorPopoverStyles } from "./time-range-selector-popover-content.styles";
+
+export const TimeRangeSelectorPopoverContent: FunctionComponent<
+    TimeRangeSelectorPopoverProps
+> = ({
+    start,
+    end,
+    onClose,
+    minDate,
+    maxDate,
+    ...props
+}: TimeRangeSelectorPopoverProps) => {
+    const [componentStartTime, setComponentStartTime] = useState<number>(start);
+    const [componentEndTime, setComponentEndTime] = useState<number>(end);
+    const timeRangeSelectorPopoverClasses = useTimeRangeSelectorPopoverStyles();
+    const commonClasses = useCommonStyles();
+    const { t } = useTranslation();
+
+    /**
+     * Update if start and end externally change
+     */
+    useEffect(() => {
+        if (start !== componentStartTime) {
+            setComponentStartTime(start);
+        }
+        if (end !== componentEndTime) {
+            setComponentEndTime(end);
+        }
+    }, [start, end]);
+
+    const setTimeRangeFromString = (timeRange: TimeRange): void => {
+        const generatedTimeRange = getTimeRangeDuration(timeRange);
+
+        setComponentStartTime(generatedTimeRange.startTime);
+        setComponentEndTime(generatedTimeRange.endTime);
+    };
+
+    const handleTimeRangeChange = (
+        eventObject: TimeRangeDuration | TimeRange
+    ): void => {
+        if (typeof eventObject === "string") {
+            setTimeRangeFromString(eventObject);
+
+            return;
+        }
+
+        setComponentStartTime(eventObject.startTime);
+        setComponentEndTime(eventObject.endTime);
+    };
+
+    const handleStartTimeChange = (date: MaterialUiPickersDate): void => {
+        if (!date) {
+            return;
+        }
+
+        // End time to be later than, or equal to start time
+        if (componentEndTime - date.toMillis() < 0) {
+            setComponentEndTime(date.toMillis());
+        }
+
+        setComponentStartTime(date.toMillis());
+    };
+
+    const handleEndTimeChange = (date: MaterialUiPickersDate): void => {
+        if (!date) {
+            return;
+        }
+
+        // Update component time range duration
+        setComponentEndTime(date.toMillis());
+    };
+
+    const handleApplyClick = (): void => {
+        if (!componentStartTime || !componentEndTime) {
+            handleCancel();
+
+            return;
+        }
+
+        // Notify that component time range duration has changed
+        props.onChange && props.onChange(componentStartTime, componentEndTime);
+        handleCancel();
+    };
+
+    const handleCancel = (): void => {
+        onClose && onClose();
+    };
+
+    return (
+        <Card elevation={0}>
+            {/* Header */}
+            <CardHeader
+                className={
+                    timeRangeSelectorPopoverClasses.timeRangeSelectorHeader
+                }
+                title={t("label.customize-time-range")}
+                titleTypographyProps={{ variant: "h6" }}
+            />
+
+            <CardContent
+                className={classnames(
+                    timeRangeSelectorPopoverClasses.timeRangeSelectorContents,
+                    commonClasses.cardContentBottomPaddingRemoved
+                )}
+            >
+                <Grid container spacing={0}>
+                    {/* Time range list */}
+                    <Hidden xsDown>
+                        <Grid item md={3} sm={4}>
+                            <div
+                                className={
+                                    timeRangeSelectorPopoverClasses.timeRangeList
+                                }
+                            >
+                                <TimeRangeList
+                                    maxDate={maxDate}
+                                    minDate={minDate}
+                                    recentCustomTimeRangeDurations={
+                                        props.recentCustomTimeRangeDurations
+                                    }
+                                    onClick={handleTimeRangeChange}
+                                />
+                            </div>
+                        </Grid>
+                    </Hidden>
+
+                    <Grid item md={9} sm={8} xs={12}>
+                        <Grid container>
+                            {/* Time range select */}
+                            <Hidden smUp>
+                                <Grid item xs={12}>
+                                    <TimeRangeSelect
+                                        recentCustomTimeRangeDurations={
+                                            props.recentCustomTimeRangeDurations
+                                        }
+                                        onChange={handleTimeRangeChange}
+                                    />
+                                </Grid>
+                            </Hidden>
+
+                            <MuiPickersUtilsProvider utils={LuxonUtils}>
+                                {/* Start time calendar */}
+                                <Grid
+                                    item
+                                    className={
+                                        timeRangeSelectorPopoverClasses.startTimeCalendarContainer
+                                    }
+                                    md={6}
+                                    xs={12}
+                                >
+                                    <div
+                                        className={
+                                            timeRangeSelectorPopoverClasses.calendar
+                                        }
+                                    >
+                                        {/* Start time label */}
+                                        <Typography
+                                            className={
+                                                timeRangeSelectorPopoverClasses.startTimeCalendarLabel
+                                            }
+                                            color="textSecondary"
+                                            display="block"
+                                            variant="overline"
+                                        >
+                                            {t("label.from")}
+                                        </Typography>
+
+                                        {/* Calendar */}
+                                        <DateTimePicker
+                                            disableFuture
+                                            hideTabs
+                                            ToolbarComponent={
+                                                DateTimePickerToolbar
+                                            }
+                                            maxDate={
+                                                maxDate
+                                                    ? new Date(maxDate)
+                                                    : undefined
+                                            }
+                                            minDate={
+                                                minDate
+                                                    ? new Date(minDate)
+                                                    : undefined
+                                            }
+                                            value={new Date(componentStartTime)}
+                                            variant="static"
+                                            onChange={handleStartTimeChange}
+                                        />
+                                    </div>
+                                </Grid>
+
+                                {/* End time calendar */}
+                                <Grid
+                                    item
+                                    className={
+                                        timeRangeSelectorPopoverClasses.endTimeCalendarContainer
+                                    }
+                                    md={6}
+                                    xs={12}
+                                >
+                                    <div
+                                        className={
+                                            timeRangeSelectorPopoverClasses.calendar
+                                        }
+                                    >
+                                        {/* End time label */}
+                                        <Typography
+                                            className={
+                                                timeRangeSelectorPopoverClasses.endTimeCalendarLabel
+                                            }
+                                            color="textSecondary"
+                                            display="block"
+                                            variant="overline"
+                                        >
+                                            {t("label.to")}
+                                        </Typography>
+
+                                        {/* Calendar */}
+                                        <DateTimePicker
+                                            disableFuture
+                                            hideTabs
+                                            ToolbarComponent={
+                                                DateTimePickerToolbar
+                                            }
+                                            maxDate={
+                                                maxDate
+                                                    ? new Date(maxDate)
+                                                    : undefined
+                                            }
+                                            minDate={
+                                                new Date(componentStartTime)
+                                            }
+                                            value={new Date(componentEndTime)}
+                                            variant="static"
+                                            onChange={handleEndTimeChange}
+                                        />
+                                    </div>
+                                </Grid>
+                            </MuiPickersUtilsProvider>
+
+                            {/* Controls when screen width is sm and up */}
+                            <Hidden xsDown>
+                                <Grid item xs={12}>
+                                    <CardActions>
+                                        <Grid
+                                            container
+                                            justifyContent="flex-end"
+                                        >
+                                            <Grid item>
+                                                <TimeRangeSelectorControls
+                                                    onApply={handleApplyClick}
+                                                    onCancel={handleCancel}
+                                                />
+                                            </Grid>
+                                        </Grid>
+                                    </CardActions>
+                                </Grid>
+                            </Hidden>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </CardContent>
+
+            {/* Controls when screen width is xs */}
+            <Hidden smUp>
+                <CardActions>
+                    <Grid container justifyContent="flex-end">
+                        <Grid item>
+                            <TimeRangeSelectorControls
+                                onApply={handleApplyClick}
+                                onCancel={handleCancel}
+                            />
+                        </Grid>
+                    </Grid>
+                </CardActions>
+            </Hidden>
+        </Card>
+    );
+};
---
+++ thirdeye-ui/src/app/components/time-range/v2/time-range-selector-popover-content/time-range-selector-popover-content.styles.ts
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2022 StarTree Inc
+ *
+ * Licensed under the StarTree Community License (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at http://www.startree.ai/legal/startree-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT * WARRANTIES OF ANY KIND,
+ * either express or implied.
+ * See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+import { makeStyles } from "@material-ui/core";
+import { Border } from "../../../../utils/material-ui/border.util";
+
+const HEIGHT_TIME_RANGE_SELECTOR_MD_UP = 427;
+
+export const useTimeRangeSelectorPopoverStyles = makeStyles((theme) => ({
+    timeRangeSelectorHeader: {
+        borderBottom: Border.BORDER_DEFAULT,
+    },
+    timeRangeSelectorContents: {
+        [theme.breakpoints.only("xs")]: {
+            width: 342,
+            paddingBottom: theme.spacing(1),
+        },
+        [theme.breakpoints.only("sm")]: {
+            width: 465,
+        },
+        [theme.breakpoints.up("sm")]: {
+            padding: 0,
+        },
+        [theme.breakpoints.up("md")]: {
+            height: HEIGHT_TIME_RANGE_SELECTOR_MD_UP,
+            width: 828,
+        },
+    },
+    timeRangeList: {
+        borderRight: Border.BORDER_DEFAULT,
+        [theme.breakpoints.only("sm")]: {
+            height: "100%",
+        },
+        [theme.breakpoints.up("md")]: {
+            height: HEIGHT_TIME_RANGE_SELECTOR_MD_UP,
+            overflowY: "auto",
+        },
+    },
+    startTimeCalendarContainer: {
+        [theme.breakpoints.up("md")]: {
+            paddingRight: "0px !important",
+        },
+    },
+    endTimeCalendarContainer: {
+        [theme.breakpoints.up("md")]: {
+            paddingLeft: "0px !important",
+        },
+    },
+    startTimeCalendarLabel: {
+        [theme.breakpoints.up("sm")]: {
+            marginTop: 12,
+            marginLeft: 12,
+        },
+    },
+    endTimeCalendarLabel: {
+        [theme.breakpoints.up("sm")]: {
+            marginLeft: 12,
+        },
+        [theme.breakpoints.up("md")]: {
+            marginTop: 12,
+        },
+    },
+    calendar: {
+        width: 310,
+    },
+}));

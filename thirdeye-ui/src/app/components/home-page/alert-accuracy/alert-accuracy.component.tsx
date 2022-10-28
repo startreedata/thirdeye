0bb21e2cf22de59b878383ee5d2a442473a8143e
Steven Pham <2080348+spham92@users.noreply.github.com>
2022-10-28 10:45:06 -0700

    fix(ui): show n/a when there are no feedback for anomalies yet

--- thirdeye-ui/src/app/components/home-page/alert-accuracy/alert-accuracy.component.tsx
+++ thirdeye-ui/src/app/components/home-page/alert-accuracy/alert-accuracy.component.tsx
@@ -31,16 +31,22 @@ export const AlertAccuracy: FunctionComponent<AlertAccuracyProps> = ({
         if (!appAnalytics) {
             return null;
         }
+        appAnalytics.anomalyStats.countWithFeedback = 0;
         const totalAnomaliesLabeledAsAnomaly =
             appAnalytics.anomalyStats.feedbackStats.ANOMALY +
             appAnalytics.anomalyStats.feedbackStats.ANOMALY_EXPECTED +
             appAnalytics.anomalyStats.feedbackStats.ANOMALY_NEW_TREND;
 
-        return Math.round(
+        // If there are no anomalies with feedback return n/a
+        if (appAnalytics.anomalyStats.countWithFeedback === 0) {
+            return "n/a";
+        }
+
+        return `${Math.round(
             (totalAnomaliesLabeledAsAnomaly /
                 appAnalytics.anomalyStats.countWithFeedback) *
                 100
-        );
+        )}%`;
     }, [appAnalytics]);
 
     return (
@@ -79,7 +85,7 @@ export const AlertAccuracy: FunctionComponent<AlertAccuracyProps> = ({
                 >
                     <Typography variant="h2">
                         {appAnalytics && precision !== null && (
-                            <span>{precision}%</span>
+                            <span>{precision}</span>
                         )}
                     </Typography>
                 </LoadingErrorStateSwitch>

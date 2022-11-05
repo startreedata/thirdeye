fa15de8000ad46e099a2d77ad24981775a3eb1d7
Gaurav Joshi <68965367+joshigaurava@users.noreply.github.com>
2022-11-04 20:30:53 -0700

    chore(ui): TE-345 remove previous license header

--- thirdeye-ui/src/app/pages/logout-page/logout-page.component.tsx
+++ thirdeye-ui/src/app/pages/logout-page/logout-page.component.tsx
@@ -1,16 +1,3 @@
-/**
- * Copyright 2022 StarTree Inc
- *
- * Licensed under the StarTree Community License (the "License"); you may not use
- * this file except in compliance with the License. You may obtain a copy of the
- * License at http://www.startree.ai/legal/startree-community-license
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT * WARRANTIES OF ANY KIND,
- * either express or implied.
- * See the License for the specific language governing permissions and limitations under
- * the License.
- */
 import React, { FunctionComponent, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { PageHeader } from "../../components/page-header/page-header.component";

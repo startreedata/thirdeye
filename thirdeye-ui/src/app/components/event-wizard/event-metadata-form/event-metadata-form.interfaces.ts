52a893f6c78d29e2640750869d79769edae2eb41
Shounak kulkarni <shounak@startree.ai>
2022-11-05 20:29:41 +0530

    chore(ui): TE-345 format license headers and exclude generated files

--- thirdeye-ui/src/app/components/event-wizard/event-metadata-form/event-metadata-form.interfaces.ts
+++ thirdeye-ui/src/app/components/event-wizard/event-metadata-form/event-metadata-form.interfaces.ts
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright 2022 StarTree Inc
  *
  * Licensed under the StarTree Community License (the "License"); you may not use
@@ -8,11 +8,9 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the
  * License is distributed on an "AS IS" BASIS, WITHOUT * WARRANTIES OF ANY KIND,
  * either express or implied.
- *
  * See the License for the specific language governing permissions and limitations under
  * the License.
  */
-
 export interface EventMetadataFormProps {
     initialPropertiesData: PropertyData[];
     onChange: (newPropertiesData: PropertyData[]) => void;

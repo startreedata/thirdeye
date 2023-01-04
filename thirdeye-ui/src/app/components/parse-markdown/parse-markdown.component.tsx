368e3bc9f30b01afeec34769f4a43e549ba5fe02
Aakarsh Chopra <aakarsh@startree.ai>
2023-01-04 16:28:03 +0530

    feat(ui): TE-1149 added ParseMarkdown component

---
+++ thirdeye-ui/src/app/components/parse-markdown/parse-markdown.component.tsx
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2022 StarTree Inc
+ *
+ * Licensed under the StarTree Community License (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at http://www.startree.ai/legal/startree-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT * WARRANTIES OF ANY KIND,
+ * either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import { Typography } from "@material-ui/core";
+import React, { FunctionComponent } from "react";
+import {
+    ReactMarkdown,
+    ReactMarkdownOptions,
+} from "react-markdown/lib/react-markdown";
+import { LinkV1 } from "../../platform/components";
+
+export const ParseMarkdown: FunctionComponent<{
+    children: string;
+    customOptions?: Partial<ReactMarkdownOptions>;
+}> = ({ children, customOptions }) => {
+    const options: ReactMarkdownOptions = {
+        components: {
+            h1: ({ children }) => (
+                <Typography variant="h1">{children}</Typography>
+            ),
+            h2: ({ children }) => (
+                <Typography variant="h2">{children}</Typography>
+            ),
+            h3: ({ children }) => (
+                <Typography variant="h3">{children}</Typography>
+            ),
+            h4: ({ children }) => (
+                <Typography variant="h4">{children}</Typography>
+            ),
+            h5: ({ children }) => (
+                <Typography variant="h5">{children}</Typography>
+            ),
+            h6: ({ children }) => (
+                <Typography variant="h6">{children}</Typography>
+            ),
+            a: ({ children, href, ...props }) => (
+                <LinkV1
+                    {...props}
+                    color="primary"
+                    href={`//${href}`}
+                    target="_blank"
+                    variant="body2"
+                >
+                    {children}
+                </LinkV1>
+            ),
+        },
+        children,
+        ...customOptions,
+    };
+
+    return <ReactMarkdown {...options} />;
+};

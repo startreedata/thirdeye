a6b0a3d3a2100b585034c53536ff43dff7154568
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2023-02-24 15:41:26 -0800

    [detectionpipeline] Move enumeration item management to EnumeratorOperator

--- thirdeye-detectionpipeline/src/main/java/ai/startree/thirdeye/detectionpipeline/operator/EnumeratorOperator.java
+++ thirdeye-detectionpipeline/src/main/java/ai/startree/thirdeye/detectionpipeline/operator/EnumeratorOperator.java
@@ -13,13 +13,20 @@
  */
 package ai.startree.thirdeye.detectionpipeline.operator;
 
+import static ai.startree.thirdeye.spi.detection.DetectionPipelineUsage.DETECTION;
+import static ai.startree.thirdeye.spi.detection.DetectionPipelineUsage.EVALUATION;
 import static ai.startree.thirdeye.spi.util.SpiUtils.optional;
 import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
 
+import ai.startree.thirdeye.detectionpipeline.DetectionPipelineContext;
 import ai.startree.thirdeye.detectionpipeline.DetectionRegistry;
 import ai.startree.thirdeye.detectionpipeline.OperatorContext;
+import ai.startree.thirdeye.detectionpipeline.PlanNodeContext;
+import ai.startree.thirdeye.spi.datalayer.bao.EnumerationItemManager;
+import ai.startree.thirdeye.spi.datalayer.dto.AlertDTO;
 import ai.startree.thirdeye.spi.datalayer.dto.EnumerationItemDTO;
+import ai.startree.thirdeye.spi.detection.DetectionPipelineUsage;
 import ai.startree.thirdeye.spi.detection.Enumerator;
 import ai.startree.thirdeye.spi.detection.Enumerator.Context;
 import ai.startree.thirdeye.spi.detection.v2.OperatorResult;
@@ -28,6 +35,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class EnumeratorOperator extends DetectionPipelineOperator {
 
@@ -36,16 +44,24 @@ public class EnumeratorOperator extends DetectionPipelineOperator {
   public static final String DEFAULT_ENUMERATOR_TYPE = "default";
   private DetectionRegistry detectionRegistry;
   private EnumerationItemDTO enumerationItemFromContext;
+  private DetectionPipelineContext detectionPipelineContext;
 
   public EnumeratorOperator() {
     super();
   }
 
+  private static AlertDTO newAlert(final Long alertId) {
+    final AlertDTO alert = new AlertDTO();
+    alert.setId(alertId);
+    return alert;
+  }
+
   @Override
   public void init(final OperatorContext context) {
     super.init(context);
-    detectionRegistry = requireNonNull(
-        context.getPlanNodeContext().getApplicationContext().getDetectionRegistry());
+    final PlanNodeContext nodeContext = context.getPlanNodeContext();
+    detectionRegistry = requireNonNull(nodeContext.getApplicationContext().getDetectionRegistry());
+    detectionPipelineContext = nodeContext.getDetectionPipelineContext();
 
     enumerationItemFromContext = context.getEnumerationItem();
   }
@@ -76,7 +92,11 @@ public class EnumeratorOperator extends DetectionPipelineOperator {
         .filter(ei -> ei.getName() == null)
         .forEach(ei -> ei.setName(generator.generateName(ei)));
 
-    setOutput(DEFAULT_OUTPUT_KEY, new EnumeratorResult(items));
+    /* Sync with existing enumeration items */
+    final List<EnumerationItemDTO> prepared = prepare(items);
+
+    /* Set output */
+    setOutput(DEFAULT_OUTPUT_KEY, new EnumeratorResult(prepared));
   }
 
   private void validate(final EnumeratorOperatorParams params,
@@ -87,6 +107,34 @@ public class EnumeratorOperator extends DetectionPipelineOperator {
       items.forEach(ei -> checkArgument(ei.getParams().keySet().containsAll(idKeys),
           "Enumeration item " + ei + " does not contain all the id keys: " + idKeys));
     }
+  }
+
+  private List<EnumerationItemDTO> prepare(final List<EnumerationItemDTO> enumerationItems) {
+    final DetectionPipelineUsage usage = requireNonNull(detectionPipelineContext.getUsage(),
+        "Detection pipeline usage is not set");
+    if (EVALUATION.equals(usage)) {
+      // do nothing - no need to persist enumerationItems nor fetch existing one downstream
+      return enumerationItems;
+    }
+    checkArgument(DETECTION.equals(usage), "DetectionPipelineUsage not implemented: " + usage);
+
+    // Add alert id to enumeration items
+    final Long alertId = requireNonNull(detectionPipelineContext.getAlertId(),
+        "alert ID is not set");
+
+    /* decorate enumeration item with alert id */
+    final var decorated = enumerationItems.stream()
+        .map(e -> e.setAlert(newAlert(alertId)))
+        .collect(Collectors.toList());
+
+    /* find existing or create new enumeration item */
+    final EnumerationItemManager enumerationItemManager = detectionPipelineContext
+        .getApplicationContext()
+        .getEnumerationItemManager();
+
+    return decorated.stream()
+        .map(enumerationItemManager::findExistingOrCreate)
+        .collect(Collectors.toList());
   }
 
   @Override

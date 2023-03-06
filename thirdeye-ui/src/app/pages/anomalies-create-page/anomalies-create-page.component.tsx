afd98dd3f4ec5a6718b8d362bf572fc9d6e71bd6
Aakarsh Chopra <aakarsh@startree.ai>
2023-03-06 19:56:43 +0530

    feat(ui): TE-840 showing selected alert name in breadcrumb

--- thirdeye-ui/src/app/pages/anomalies-create-page/anomalies-create-page.component.tsx
+++ thirdeye-ui/src/app/pages/anomalies-create-page/anomalies-create-page.component.tsx
@@ -15,7 +15,7 @@
 
 import React, { FunctionComponent, useEffect, useMemo } from "react";
 import { useTranslation } from "react-i18next";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { CreateAnomalyWizard } from "../../components/anomalies-create/create-anomaly-wizard/create-anomaly-wizard.component";
 import { PageHeader } from "../../components/page-header/page-header.component";
 import { PageHeaderProps } from "../../components/page-header/page-header.interfaces";
@@ -32,6 +32,7 @@ import { Alert } from "../../rest/dto/alert.interfaces";
 import { notifyIfErrors } from "../../utils/notifications/notifications.util";
 import { getErrorMessages } from "../../utils/rest/rest.util";
 import {
+    getAlertsAlertViewPath,
     getAnomaliesAllPath,
     getAnomaliesCreatePath,
     getAnomaliesViewPath,
@@ -44,10 +45,24 @@ export const AnomaliesCreatePage: FunctionComponent = () => {
     const navigate = useNavigate();
     const { notify } = useNotificationProviderV1();
     const { alerts, getAlerts, status: alertsStatus } = useGetAlerts();
+    const { id: selectedAlertId } = useParams<{ id?: string }>();
 
     useEffect(() => {
         getAlerts();
     }, []);
+
+    const selectedAlert = useMemo<Alert | null>(() => {
+        if (alerts) {
+            const searchedAlert = alerts.find(
+                (a) => a.id === Number(selectedAlertId)
+            );
+            if (searchedAlert) {
+                return searchedAlert;
+            }
+        }
+
+        return null;
+    }, [alerts, selectedAlertId]);
 
     const handleCancelClick = (): void => {
         navigate(getAnomaliesAllPath());
@@ -58,7 +73,7 @@ export const AnomaliesCreatePage: FunctionComponent = () => {
             .then((data) => {
                 notify(
                     NotificationTypeV1.Success,
-                    t("message.create-success", { entity: t("label.alert") })
+                    t("message.create-success", { entity: t("label.anomaly") })
                 );
                 // TODO: Remove
                 window.open(getAnomaliesViewPath(data.id), "_blank");
@@ -88,6 +103,14 @@ export const AnomaliesCreatePage: FunctionComponent = () => {
                 link: getAnomaliesCreatePath(),
                 label: t("label.create"),
             },
+            ...(selectedAlert
+                ? [
+                      {
+                          link: getAlertsAlertViewPath(selectedAlert.id),
+                          label: selectedAlert.name,
+                      },
+                  ]
+                : []),
         ],
     };
 

77992aac3677d8a82df2cb10f7c41b0c0356393e
Aakarsh Chopra <aakarsh@startree.ai>
2023-03-17 22:32:06 +0530

    feat(ui): TE-1388 added anomaly create helper card

--- thirdeye-ui/src/app/components/help-drawer-v1/help-drawer-card-contents.utils.tsx
+++ thirdeye-ui/src/app/components/help-drawer-v1/help-drawer-card-contents.utils.tsx
@@ -162,6 +162,65 @@ export const anomaliesBasicHelpCards: HelpDrawerV1Card[] = [
     },
 ];
 
+export const anomalyCreateBasicHelpCards: HelpDrawerV1Card[] = [
+    {
+        title: i18next.t("message.how-to-action-entity", {
+            action: i18next.t("label.report-missed"),
+            entity: i18next.t("label.anomalies"),
+        }),
+        body: (
+            <>
+                <IframeVideoPlayerContainer>
+                    <iframe
+                        allowFullScreen
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        frameBorder="0"
+                        src={i18next.t("url.report-missed-anomaly-video-link")}
+                    />
+                </IframeVideoPlayerContainer>
+                <LinkButtonV1
+                    externalLink
+                    color="primary"
+                    href={i18next.t("url.report-missed-anomaly-video-link")}
+                    target="_blank"
+                    variant="outlined"
+                >
+                    <Box component="span" display="flex" mr={1}>
+                        <Icon icon="material-symbols:slow-motion-video-sharp" />
+                    </Box>
+                    <Box component="span">
+                        {i18next.t("label.view-entity", {
+                            entity: i18next.t("label.video"),
+                        })}
+                    </Box>
+                </LinkButtonV1>
+            </>
+        ),
+    },
+    {
+        title: i18next.t("message.what-is-an-entity", {
+            entity: i18next.t("label.anomaly"),
+        }),
+        body: (
+            <>
+                <Trans i18nKey="message.anomaly-helper-description" />{" "}
+                <LinkV1
+                    externalLink
+                    href={i18next.t("url.how-to-investigate-anomaly")}
+                    target="_blank"
+                    variant="body2"
+                >
+                    {capitalize(
+                        i18next.t("message.learn-more-about-entity", {
+                            entity: i18next.t("label.anomalies"),
+                        })
+                    )}
+                </LinkV1>
+            </>
+        ),
+    },
+];
+
 export const anomaliesInvestigateBasicHelpCards: HelpDrawerV1Card[] = [
     {
         title: i18next.t("message.how-to-action-entity", {

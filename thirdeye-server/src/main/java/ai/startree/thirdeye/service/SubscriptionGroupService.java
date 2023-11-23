df58c8cc578cfb1452e3bcfb987a1e53a3a20c8f
Cyril de Catheu <cyril@startree.ai>
2023-11-23 15:48:56 +0100

    simplify SubscriptionGroupService

--- thirdeye-server/src/main/java/ai/startree/thirdeye/service/SubscriptionGroupService.java
+++ thirdeye-server/src/main/java/ai/startree/thirdeye/service/SubscriptionGroupService.java
@@ -16,11 +16,9 @@ package ai.startree.thirdeye.service;
 
 import static ai.startree.thirdeye.spi.ThirdEyeStatus.ERR_CRON_INVALID;
 import static ai.startree.thirdeye.spi.ThirdEyeStatus.ERR_DUPLICATE_NAME;
-import static ai.startree.thirdeye.spi.ThirdEyeStatus.ERR_ID_UNEXPECTED_AT_CREATION;
 import static ai.startree.thirdeye.spi.util.SpiUtils.optional;
 import static ai.startree.thirdeye.util.ResourceUtils.ensure;
 import static ai.startree.thirdeye.util.ResourceUtils.ensureExists;
-import static ai.startree.thirdeye.util.ResourceUtils.ensureNull;
 
 import ai.startree.thirdeye.auth.AuthorizationManager;
 import ai.startree.thirdeye.auth.ThirdEyeServerPrincipal;
@@ -29,7 +27,6 @@ import ai.startree.thirdeye.spi.api.AlertApi;
 import ai.startree.thirdeye.spi.api.SubscriptionGroupApi;
 import ai.startree.thirdeye.spi.datalayer.bao.SubscriptionGroupManager;
 import ai.startree.thirdeye.spi.datalayer.dto.SubscriptionGroupDTO;
-import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.Singleton;
 import javax.inject.Inject;
@@ -39,7 +36,6 @@ import org.quartz.CronExpression;
 public class SubscriptionGroupService extends
     CrudService<SubscriptionGroupApi, SubscriptionGroupDTO> {
 
-  private static final String CRON_EVERY_5MIN = "0 */5 * * * ?";
   private final SubscriptionGroupManager subscriptionGroupManager;
 
   @Inject
@@ -53,24 +49,19 @@ public class SubscriptionGroupService extends
   @Override
   protected SubscriptionGroupDTO createDto(final ThirdEyeServerPrincipal principal,
       final SubscriptionGroupApi api) {
-    ensureNull(api.getId(), ERR_ID_UNEXPECTED_AT_CREATION);
-    if (Strings.isNullOrEmpty(api.getCron())) {
-      api.setCron(CRON_EVERY_5MIN);
-    }
     return toDto(api);
   }
 
   @Override
   protected void validate(final SubscriptionGroupApi api, final SubscriptionGroupDTO existing) {
     super.validate(api, existing);
-    String cron = api.getCron();
-    ensure(Strings.isNullOrEmpty(cron) || CronExpression.isValidExpression(cron),
-        ERR_CRON_INVALID,
-        cron);
+    ensureExists(api.getName(), "name value must be set.");
+    ensureExists(api.getCron(), "cron value must be set.");
+    ensure(CronExpression.isValidExpression(api.getCron()), ERR_CRON_INVALID, api.getCron());
 
     // For new Subscription Group or existing Subscription Group with different name
     if (existing == null || !existing.getName().equals(api.getName())) {
-      ensure(dtoManager.findByName(api.getName()).size() == 0, ERR_DUPLICATE_NAME, api.getName());
+      ensure(dtoManager.findByName(api.getName()).isEmpty(), ERR_DUPLICATE_NAME, api.getName());
     }
     optional(api.getAlertAssociations())
         .ifPresent(l -> l.forEach(alertAssociation -> {
@@ -78,16 +69,6 @@ public class SubscriptionGroupService extends
           ensureExists(alert, "alert missing in alert association");
           ensureExists(alert.getId(), "alert.id is missing in alert association");
         }));
-  }
-
-  @Override
-  protected void prepareUpdatedDto(final ThirdEyeServerPrincipal principal,
-      final SubscriptionGroupDTO existing,
-      final SubscriptionGroupDTO updated) {
-    // Always set a default cron if not present.
-    if (updated.getCronExpression() == null) {
-      updated.setCronExpression(CRON_EVERY_5MIN);
-    }
   }
 
   @Override

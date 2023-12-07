8205a4bfd5c2c81f5d6c3efd69cdfdfc660d5bfd
Steven Pham <2080348+spham92@users.noreply.github.com>
2023-12-07 13:09:09 -0800

    refactor(ui): refactor DatasourceListV1 to use new datagrid

--- thirdeye-ui/src/app/components/datasource-list-v1/datasource-list-v1.component.tsx
+++ thirdeye-ui/src/app/components/datasource-list-v1/datasource-list-v1.component.tsx
@@ -16,12 +16,7 @@ import { Button, Grid, Link } from "@material-ui/core";
 import React, { FunctionComponent, ReactElement, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
-import {
-    DataGridScrollV1,
-    DataGridSelectionModelV1,
-    DataGridV1,
-    PageContentsCardV1,
-} from "../../platform/components";
+import { PageContentsCardV1 } from "../../platform/components";
 import { UiDatasource } from "../../rest/dto/ui-datasource.interfaces";
 import {
     getDatasourcesUpdatePath,
@@ -32,120 +27,119 @@ import {
     TEST_IDS,
 } from "./datasource-list-v1.interfaces";
 import { DatasourceVerification } from "./datasource-verification/datasource-verification.component";
+import { StyledDataGrid } from "../data-grid/styled-data-grid.component";
+import {
+    GridColumns,
+    GridRenderCellParams,
+    GridSelectionModel,
+} from "@mui/x-data-grid";
 
-export const DatasourceListV1: FunctionComponent<DatasourceListV1Props> = (
-    props: DatasourceListV1Props
-) => {
+export const DatasourceListV1: FunctionComponent<DatasourceListV1Props> = ({
+    datasources,
+    onDelete,
+}) => {
     const { t } = useTranslation();
-    const [selectedDatasource, setSelectedDatasource] =
-        useState<DataGridSelectionModelV1<UiDatasource>>();
+    const [selectedDatasourceIds, setSelectedDatasourceIds] =
+        useState<GridSelectionModel>();
     const navigate = useNavigate();
 
     const handleDatasourceDelete = (): void => {
-        if (!selectedDatasource) {
+        if (!selectedDatasourceIds) {
             return;
         }
 
-        const selectedSubScriptingGroupId = selectedDatasource
-            .rowKeyValues[0] as number;
+        const selectedSubScriptingGroupId = selectedDatasourceIds[0] as number;
         const uiDatasource = getUiDatasource(selectedSubScriptingGroupId);
         if (!uiDatasource) {
             return;
         }
 
-        props.onDelete && props.onDelete(uiDatasource);
+        onDelete && onDelete(uiDatasource);
     };
 
     const getUiDatasource = (id: number): UiDatasource | null => {
-        if (!props.datasources) {
+        if (!datasources) {
             return null;
         }
 
-        return (
-            props.datasources.find((datasource) => datasource.id === id) || null
-        );
+        return datasources.find((datasource) => datasource.id === id) || null;
     };
 
     const handleDatasourceEdit = (): void => {
-        if (!selectedDatasource) {
+        if (!selectedDatasourceIds) {
             return;
         }
-        const selectedSubScriptionGroupId = selectedDatasource
-            .rowKeyValues[0] as number;
+        const selectedSubScriptionGroupId = selectedDatasourceIds[0] as number;
 
         navigate(getDatasourcesUpdatePath(selectedSubScriptionGroupId));
     };
 
     const isActionButtonDisable = !(
-        selectedDatasource && selectedDatasource.rowKeyValues.length === 1
+        selectedDatasourceIds && selectedDatasourceIds.length === 1
     );
 
     const handleDatasourceViewDetailsById = (id: number): void => {
         navigate(getDatasourcesViewPath(id));
     };
 
-    const renderLink = (
-        cellValue: Record<string, unknown>,
-        data: UiDatasource
-    ): ReactElement => {
+    const renderLink = (params: GridRenderCellParams): ReactElement => {
         return (
-            <Link onClick={() => handleDatasourceViewDetailsById(data.id)}>
-                {cellValue}
+            <Link
+                onClick={() => handleDatasourceViewDetailsById(params.row.id)}
+            >
+                {params.row.name}
             </Link>
         );
     };
 
-    const renderStatusCheck = (
-        _: Record<string, unknown>,
-        data: UiDatasource
-    ): ReactElement => {
-        return <DatasourceVerification datasourceName={data.name} />;
+    const renderStatusCheck = (params: GridRenderCellParams): ReactElement => {
+        return <DatasourceVerification datasourceName={params.row.name} />;
     };
 
-    const datasourceColumns = [
+    const datasourceColumns: GridColumns = [
         {
-            key: "name",
-            dataKey: "name",
-            header: t("label.name"),
-            minWidth: 0,
+            field: "name",
+            headerName: t("label.name"),
             flex: 1.5,
             sortable: true,
-            customCellRenderer: renderLink,
+            renderCell: renderLink,
         },
         {
-            key: "type",
-            dataKey: "type",
+            field: "type",
             sortable: true,
-            header: t("label.type"),
-            minWidth: 0,
+            headerName: t("label.type"),
             flex: 1,
         },
         {
-            key: "status",
-            dataKey: "status",
-            header: t("label.health-status"),
-            minWidth: 0,
+            field: "status",
+            headerName: t("label.health-status"),
             flex: 1,
             sortable: false,
-            cellTooltip: false,
-            customCellRenderer: renderStatusCheck,
+            renderCell: renderStatusCheck,
         },
     ];
 
     return (
         <Grid item xs={12}>
-            <PageContentsCardV1 disablePadding fullHeight>
-                <DataGridV1<UiDatasource>
-                    hideBorder
+            <PageContentsCardV1 disablePadding>
+                <StyledDataGrid
+                    autoHeight
+                    autoPageSize
+                    checkboxSelection
+                    disableColumnFilter
+                    disableColumnSelector
+                    disableSelectionOnClick
                     columns={datasourceColumns}
-                    data={props.datasources as UiDatasource[]}
-                    data-testId={TEST_IDS.TABLE}
-                    rowKey="id"
-                    scroll={DataGridScrollV1.Contents}
-                    searchPlaceholder={t("label.search-entity", {
-                        entity: t("label.datasources"),
-                    })}
-                    toolbarComponent={
+                    data-testid={TEST_IDS.TABLE}
+                    rows={datasources as UiDatasource[]}
+                    searchBarProps={{
+                        searchKey: "name",
+                        placeholder: t("label.search-entity", {
+                            entity: t("label.datasources"),
+                        }),
+                    }}
+                    selectionModel={selectedDatasourceIds}
+                    toolbar={
                         <Grid container alignItems="center" spacing={2}>
                             <Grid item>
                                 <Button
@@ -170,7 +164,7 @@ export const DatasourceListV1: FunctionComponent<DatasourceListV1Props> = (
                             </Grid>
                         </Grid>
                     }
-                    onSelectionChange={setSelectedDatasource}
+                    onSelectionModelChange={setSelectedDatasourceIds}
                 />
             </PageContentsCardV1>
         </Grid>

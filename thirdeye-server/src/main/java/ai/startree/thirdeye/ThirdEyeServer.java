249a051af244a23c8dbae6516ee88c6a1dbf5ecb
Cyril de Catheu <cyril@startree.ai>
2024-02-09 16:16:48 +0100

    test different tag value

--- thirdeye-server/src/main/java/ai/startree/thirdeye/ThirdEyeServer.java
+++ thirdeye-server/src/main/java/ai/startree/thirdeye/ThirdEyeServer.java
@@ -151,8 +151,11 @@ public class ThirdEyeServer extends Application<ThirdEyeServerConfiguration> {
       // new registry based on micrometers
       final PrometheusMeterRegistry registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
       // TODO CYRIL can be removed once migration from dw-metrics to micrometer is completed
-      // registry.config().commonTags("test", "micrometer");
       Metrics.globalRegistry.add(registry);
+      final String environmentUrl = optional(configuration.getUiConfiguration().getExternalUrl())
+          .map(s -> s.replaceAll("https?://", ""))
+          .orElse("unknown");
+      Metrics.globalRegistry.config().commonTags("environment_url", environmentUrl);
       // old registry based on dropwizard-metrics
       final CollectorRegistry legacyRegistry = new CollectorRegistry();
       legacyRegistry.register(new DropwizardExports(env.metrics()));

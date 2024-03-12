963d3b300f37637a7461565c6ab9b3be29f14007
Aakarsh Chopra <aakarsh@startree.ai>
2024-03-12 21:47:04 +0530

    fix(ui): TE-2112 enable copying the dimension filter

--- thirdeye-ui/src/app/components/rca/heat-map/dimension-search-automcomplete/dimension-search-autocomplete.component.tsx
+++ thirdeye-ui/src/app/components/rca/heat-map/dimension-search-automcomplete/dimension-search-autocomplete.component.tsx
@@ -12,20 +12,38 @@
  * See the License for the specific language governing permissions and limitations under
  * the License.
  */
-import { Chip, InputAdornment, TextField } from "@material-ui/core";
+import {
+    Box,
+    Chip,
+    IconButton,
+    InputAdornment,
+    TextField,
+    Tooltip,
+} from "@material-ui/core";
+import CheckIcon from "@material-ui/icons/CheckCircleOutlineOutlined";
+import CopyIcon from "@material-ui/icons/FileCopyOutlined";
 import SearchIcon from "@material-ui/icons/Search";
 import { Autocomplete } from "@material-ui/lab";
 import { isString, pull } from "lodash";
-import React, { FunctionComponent, useEffect, useState } from "react";
+import React, { FunctionComponent, useEffect, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { EMPTY_STRING_DISPLAY } from "../../../../utils/anomalies/anomalies.util";
+import { copyToClipboard } from "../../../../utils/browser/browser.utils";
 import { AnomalyFilterOption } from "../heat-map.interfaces";
 import { formatDimensionOptions } from "../heat-map.utils";
 import { DimensionSearchAutocompleteProps } from "./dimension-search-autocomplete.interfaces";
+import { LocalThemeProviderV1 } from "../../../../platform/components";
+import { lightV1 } from "../../../../platform/utils";
 
 export const DimensionSearchAutocomplete: FunctionComponent<DimensionSearchAutocompleteProps> =
     ({ heatMapData, anomalyFilters, onFilterChange }) => {
         const { t } = useTranslation();
+
+        // Keep track of the filter that was copied to the clipboard,
+        // to show and hide a checkmark in response
+        const [showCopyForFilter, setShowCopyForFilter] = useState<
+            string | null
+        >(null);
 
         const [anomalyFilterOptions, setAnomalyFilterOptions] = useState<
             AnomalyFilterOption[]
@@ -49,6 +67,28 @@ export const DimensionSearchAutocomplete: FunctionComponent<DimensionSearchAutoc
         const handleOnChangeFilter = (options: AnomalyFilterOption[]): void => {
             onFilterChange([...options]);
         };
+
+        const handleCopyFilterName = (filterName: string): void => {
+            copyToClipboard(filterName);
+            setShowCopyForFilter(filterName);
+
+            setTimeout(() => {
+                setShowCopyForFilter(null);
+            }, 2000);
+        };
+
+        const aggregateFilter = useMemo(
+            () =>
+                anomalyFilters
+                    .map(
+                        (option) =>
+                            `${option.key}=${
+                                option.value || EMPTY_STRING_DISPLAY
+                            }`
+                    )
+                    .join("&"),
+            [anomalyFilters]
+        );
 
         return (
             <Autocomplete
@@ -81,28 +121,130 @@ export const DimensionSearchAutocomplete: FunctionComponent<DimensionSearchAutoc
                                     {params.InputProps.startAdornment}
                                 </>
                             ),
+                            endAdornment: (
+                                <>
+                                    <InputAdornment position="start">
+                                        <Tooltip
+                                            title={
+                                                aggregateFilter ===
+                                                showCopyForFilter
+                                                    ? t(
+                                                          "label.copied-to-clipboard"
+                                                      )
+                                                    : t(
+                                                          "label.copy-dimension-filter"
+                                                      )
+                                            }
+                                        >
+                                            <IconButton
+                                                color="secondary"
+                                                size="small"
+                                                onClick={() => {
+                                                    handleCopyFilterName(
+                                                        aggregateFilter
+                                                    );
+                                                }}
+                                            >
+                                                {aggregateFilter ===
+                                                showCopyForFilter ? (
+                                                    <LocalThemeProviderV1
+                                                        primary={
+                                                            lightV1.palette
+                                                                .success
+                                                        }
+                                                    >
+                                                        <CheckIcon
+                                                            color="primary"
+                                                            fontSize="small"
+                                                        />
+                                                    </LocalThemeProviderV1>
+                                                ) : (
+                                                    <CopyIcon fontSize="small" />
+                                                )}
+                                            </IconButton>
+                                        </Tooltip>
+                                    </InputAdornment>
+                                    {params.InputProps.endAdornment}
+                                </>
+                            ),
                         }}
                         placeholder={t("message.anomaly-filter-search")}
                         variant="outlined"
                     />
                 )}
                 renderTags={() =>
-                    anomalyFilters.map((option: AnomalyFilterOption, index) => (
-                        <Chip
-                            className="filter-chip"
-                            key={`${index}_${option.value}`}
-                            label={`${option.key}=${
-                                option.value || EMPTY_STRING_DISPLAY
-                            }`}
-                            size="small"
-                            style={{
-                                marginTop: 0,
-                                marginBottom: 0,
-                                marginRight: 3,
-                            }}
-                            onDelete={() => handleNodeFilterOnDelete(option)}
-                        />
-                    ))
+                    anomalyFilters
+                        .map(
+                            (option) =>
+                                [
+                                    option,
+                                    `${option.key}=${
+                                        option.value || EMPTY_STRING_DISPLAY
+                                    }`,
+                                ] as const
+                        )
+                        .map(([option, filterName], index) => (
+                            <Chip
+                                className="filter-chip"
+                                key={`${index}_${option.value}`}
+                                label={
+                                    <Box
+                                        alignItems="center"
+                                        display="flex"
+                                        gridGap={8}
+                                    >
+                                        {filterName}
+                                        <Tooltip
+                                            title={
+                                                filterName === showCopyForFilter
+                                                    ? t(
+                                                          "label.copied-to-clipboard"
+                                                      )
+                                                    : t(
+                                                          "label.copy-dimension-filter"
+                                                      )
+                                            }
+                                        >
+                                            <IconButton
+                                                color="secondary"
+                                                size="small"
+                                                onClick={() => {
+                                                    handleCopyFilterName(
+                                                        filterName
+                                                    );
+                                                }}
+                                            >
+                                                {filterName ===
+                                                showCopyForFilter ? (
+                                                    <LocalThemeProviderV1
+                                                        primary={
+                                                            lightV1.palette
+                                                                .success
+                                                        }
+                                                    >
+                                                        <CheckIcon
+                                                            color="primary"
+                                                            fontSize="inherit"
+                                                        />
+                                                    </LocalThemeProviderV1>
+                                                ) : (
+                                                    <CopyIcon fontSize="inherit" />
+                                                )}
+                                            </IconButton>
+                                        </Tooltip>
+                                    </Box>
+                                }
+                                size="small"
+                                style={{
+                                    marginTop: 0,
+                                    marginBottom: 0,
+                                    marginRight: 3,
+                                }}
+                                onDelete={() =>
+                                    handleNodeFilterOnDelete(option)
+                                }
+                            />
+                        ))
                 }
                 value={anomalyFilters}
                 onChange={(_e, options) =>

47a9ca28378c787620b6c7ef31ea1549e58ee407
Cyril de Catheu <cyril@startree.ai>
2024-04-18 10:17:22 +0200

    inject namespace in evaluate

--- thirdeye-server/src/main/java/ai/startree/thirdeye/service/AlertService.java
+++ thirdeye-server/src/main/java/ai/startree/thirdeye/service/AlertService.java
@@ -262,12 +262,15 @@ public class AlertService extends CrudService<AlertApi, AlertDTO> {
     if (alertApi.getId() != null) {
       final AlertDTO alertDto = ensureExists(dtoManager.findById(alertApi.getId()));
       authorizationManager.ensureCanRead(principal, alertDto);
+      // inject namespace in the request - looks hacky todo cyril consider rewrite 
+      alertApi.setAuth(new AuthorizationConfigurationApi().setNamespace(alertDto.namespace()));
     } else {
       final AlertDTO alertDto = toDto(alertApi);
       authorizationManager.enrichNamespace(principal, alertDto);
       authorizationManager.ensureCanCreate(principal, alertDto);
+      // inject namespace in the request - looks hacky todo cyril consider rewrite
+      alertApi.setAuth(new AuthorizationConfigurationApi().setNamespace(alertDto.namespace()));
     }
-    // fixme cyril authz ASAP - inject namespace in the request (do this in the if-else above) 
     final AlertEvaluationApi results = alertEvaluator.evaluate(request);
     final Map<String, DetectionEvaluationApi> filtered = allowedEvaluations(principal,
         results.getDetectionEvaluations());

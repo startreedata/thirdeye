24d9197274c521cf19a73c7f461c18d54eb19ac1
Cyril de Catheu <cyril@startree.ai>
2024-07-11 10:18:34 +0200

    wip optimize template rendering

--- thirdeye-core/src/main/java/ai/startree/thirdeye/util/StringTemplateUtils.java
+++ thirdeye-core/src/main/java/ai/startree/thirdeye/util/StringTemplateUtils.java
@@ -14,25 +14,88 @@
 package ai.startree.thirdeye.util;
 
 import ai.startree.thirdeye.spi.datalayer.Templatable;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
 
 public class StringTemplateUtils {
 
-  @SuppressWarnings("unchecked")
-  public static <T> T applyContext(final T template,
-      final Map<String, Object> valuesMap)
-      throws IOException {
-    final Module module = new SimpleModule()
-        .addSerializer(Templatable.class, new TemplateEngineTemplatableSerializer(valuesMap))
-        .addSerializer(String.class, new TemplateEngineStringSerializer(valuesMap));
-    final ObjectMapper objectMapper = new ObjectMapper().registerModule(module);
+  // FIXME CYRIL - this is extremely slow because an object mapper is recreated at each time
+  // this means the ser/deserializer for are recreated by reflection at each call.  
+  // TODO v1: 
+  //   the templateEngineTemplatableSerializer values should be mutable
+  //   at call time, this means the function need to be synchronized - one instance should only be used by one thread at the same time
+  // v2: concurrent queue of object mappers, TemplatableSerializer, StringSerializer to avoid locks on this class
+  // a pool of 3-4 
 
-    // serialize as json - properties are applied during the serialization
-    final String jsonString = objectMapper.writeValueAsString(template);
-    return (T) objectMapper.readValue(jsonString, template.getClass());
+  private static final LinkedBlockingDeque<TemplateMapper> mapperQueue = new LinkedBlockingDeque<>();
+  private static final int mapperCount = 1;
+  public static final int MAX_MAPPERS = 4;
+
+  static {
+    mapperQueue.add(new TemplateMapper());
+  }
+
+  @SuppressWarnings("unchecked")
+  public static <T> T applyContext(final T template, final Map<String, Object> valuesMap)
+      throws IOException {
+    // should never block - if this blocks, the number of templatableMapper should be made bigger
+    // check size, if zero an num instance created is small  
+    TemplateMapper templateMapper = null;
+    try {
+      if (mapperQueue.isEmpty() && TemplateMapper.numInstanceCreated < MAX_MAPPERS) {
+        templateMapper = new TemplateMapper();
+      } else {
+        templateMapper = mapperQueue.pollLast(10, TimeUnit.MILLISECONDS); 
+      }
+      templateMapper.setValuesMap(valuesMap);
+      // serialize as json - properties are applied during the serialization
+      final String jsonString = templateMapper.writeValueAsString(template);
+      return (T) templateMapper.readValue(jsonString, template.getClass());
+    } finally {
+      if (templateMapper != null) {
+        mapperQueue.add(templateMapper);   
+      }
+    }
+  }
+
+  // not thread safe - should only be accessed by one thread at a time ! 
+  private static class TemplateMapper {
+
+    private static int numInstanceCreated = 0;
+
+    private final ObjectMapper objectMapper;
+    private final TemplateEngineTemplatableSerializer templatableSerializer;
+    private final TemplateEngineStringSerializer stringSerializer;
+
+    private TemplateMapper() {
+      templatableSerializer = new TemplateEngineTemplatableSerializer(Collections.emptyMap());
+      stringSerializer = new TemplateEngineStringSerializer(Collections.emptyMap());
+      final Module module = new SimpleModule().addSerializer(Templatable.class,
+          templatableSerializer).addSerializer(String.class, stringSerializer);
+      objectMapper = new ObjectMapper().registerModule(module);
+      numInstanceCreated++;
+    }
+
+    private TemplateMapper setValuesMap(final Map<String, Object> valuesMap) {
+      templatableSerializer.setValuesMap(valuesMap);
+      stringSerializer.setValuesMap(valuesMap);
+      return this;
+    }
+    
+    private String writeValueAsString(final Object value) throws JsonProcessingException {
+      return objectMapper.writeValueAsString(value);
+    }
+
+    private <T> T readValue(final String content, final Class<T> valueType)
+        throws JsonProcessingException {
+      return objectMapper.readValue(content, valueType);
+    } 
   }
 }

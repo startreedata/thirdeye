c2212325ef8c9050768ebb37453da4d0038f549f
Nalin Patidar <nalinpatidar@Nalins-MacBook-Pro.local>
2024-08-09 14:04:54 +0530

    fix(ui): TE-2386 build fix

--- thirdeye-ui/src/app/components/app-bar/app-header/app-header.component.tsx
+++ thirdeye-ui/src/app/components/app-bar/app-header/app-header.component.tsx
@@ -28,7 +28,7 @@ export const Appheader = ({
 }: AppHeaderprops): ReactElement => {
     const { t } = useTranslation();
     const [navBarMinimized] = useNavBarV1((state) => [state.navBarMinimized]);
-    const showWorkspaceSwitcher = Boolean(workspaces) && workspaces?.length > 1;
+    const showWorkspaceSwitcher = Boolean(workspaces && workspaces?.length > 1);
     const compoenentStyles = useAppHeaderStyles({
         showWorkspaceSwitcher,
         isFullScreen,

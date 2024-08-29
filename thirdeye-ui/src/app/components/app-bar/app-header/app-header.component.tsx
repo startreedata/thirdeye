e292d7d433c15bf3d3345ad3d4a30a9e6040a4c8
Nalin Patidar <nalinpatidar@Nalins-MacBook-Pro.local>
2024-08-29 15:23:12 +0530

    manual trigger added

--- thirdeye-ui/src/app/components/app-bar/app-header/app-header.component.tsx
+++ thirdeye-ui/src/app/components/app-bar/app-header/app-header.component.tsx
@@ -34,7 +34,7 @@ export const Appheader = ({
         isFullScreen,
         navBarMinimized,
     })();
-    const handleWorkspaceChange = (event: { target: { value: any } }): void => {
+    const handleChange = (event: { target: { value: any } }): void => {
         onWorkspaceChange({ id: event.target.value });
     };
 
@@ -49,7 +49,7 @@ export const Appheader = ({
                             displayEmpty
                             className={compoenentStyles.selectEmpty}
                             value={selectedWorkspace?.id}
-                            onChange={handleWorkspaceChange}
+                            onChange={handleChange}
                         >
                             {workspaces &&
                                 workspaces.map(({ id }): ReactElement => {

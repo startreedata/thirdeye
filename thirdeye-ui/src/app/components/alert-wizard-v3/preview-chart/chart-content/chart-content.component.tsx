3fc63acf67a1e5478960c28c4bf658b68f845b81
Nalin Patidar <nalinpatidar@Nalins-MacBook-Pro.local>
2024-08-30 15:25:00 +0530

    review comment fix

--- thirdeye-ui/src/app/components/alert-wizard-v3/preview-chart/chart-content/chart-content.component.tsx
+++ thirdeye-ui/src/app/components/alert-wizard-v3/preview-chart/chart-content/chart-content.component.tsx
@@ -193,8 +193,7 @@ export const ChartContent: FunctionComponent<ChartContentProps> = ({
                     )}
 
                 {workingDetectionEvaluations &&
-                    (workingDetectionEvaluations.length > 1 ||
-                        workingDetectionEvaluations[0]?.enumerationItem) && (
+                    workingDetectionEvaluations[0]?.enumerationItem && (
                         <Box marginTop={1}>
                             <EnumerationItemsTable
                                 alert={alert}

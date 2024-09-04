455d4796d86471a0efe2ce7f32cf925811f826ae
Nalin Patidar <nalinpatidar@Nalins-MacBook-Pro.local>
2024-09-04 12:42:39 +0530

    Feature Flag [WiP]

--- thirdeye-ui/src/app/app.tsx
+++ thirdeye-ui/src/app/app.tsx
@@ -15,13 +15,14 @@
 
 import * as Sentry from "@sentry/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React, { FunctionComponent, useEffect } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { AppBarConfigProvider } from "./components/app-bar/app-bar-config-provider/app-bar-config-provider.component";
 import { AnalyticsAndErrorReportingProviderV1 } from "./components/third-party-lib-setup-providers/analytics-and-error-reporting-provider-v1/analytics-and-error-reporting-provider-v1.component";
 import { AppCrashPage } from "./pages/app-crash-page/app-crash-page.component";
 import { AppContainerV1, useAuthProviderV1 } from "./platform/components";
 import { AppRouter } from "./routers/app/app.router";
+import { asyncWithLDProvider } from "launchdarkly-react-client-sdk";
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -31,11 +32,28 @@ const queryClient = new QueryClient({
         },
     },
 });
+type FFProviderType = React.ComponentType<{ children: React.ReactNode }>;
 
 // ThirdEye UI app
 export const App: FunctionComponent = () => {
     const { authDisabled, authDisabledNotification } = useAuthProviderV1();
+    const [FFProvider, setFFProvider] = useState<FFProviderType | null>(null);
     const { t } = useTranslation();
+
+    useEffect(() => {
+        const initializeFF = async (): Promise<void> => {
+            const FFProviderClient = await asyncWithLDProvider({
+                clientSideID: "66c5ae95c418190f8386905f",
+                context: {
+                    kind: "domain",
+                    key: "demo-prod-key",
+                    name: "localhost",
+                },
+            });
+            setFFProvider(() => FFProviderClient);
+        };
+        initializeFF();
+    }, []);
 
     useEffect(() => {
         if (authDisabled && authDisabledNotification) {
@@ -44,17 +62,23 @@ export const App: FunctionComponent = () => {
         }
     }, [authDisabled, authDisabledNotification]);
 
+    if (!FFProvider) {
+        return <div>Loading</div>;
+    }
+
     return (
-        <QueryClientProvider client={queryClient}>
-            <AnalyticsAndErrorReportingProviderV1>
-                <AppContainerV1 name={t("label.thirdeye")}>
-                    <Sentry.ErrorBoundary fallback={<AppCrashPage />}>
-                        <AppBarConfigProvider>
-                            <AppRouter />
-                        </AppBarConfigProvider>
-                    </Sentry.ErrorBoundary>
-                </AppContainerV1>
-            </AnalyticsAndErrorReportingProviderV1>
-        </QueryClientProvider>
+        <FFProvider>
+            <QueryClientProvider client={queryClient}>
+                <AnalyticsAndErrorReportingProviderV1>
+                    <AppContainerV1 name={t("label.thirdeye")}>
+                        <Sentry.ErrorBoundary fallback={<AppCrashPage />}>
+                            <AppBarConfigProvider>
+                                <AppRouter />
+                            </AppBarConfigProvider>
+                        </Sentry.ErrorBoundary>
+                    </AppContainerV1>
+                </AnalyticsAndErrorReportingProviderV1>
+            </QueryClientProvider>
+        </FFProvider>
     );
 };

318c823e4011f91bd828ffd5bea20eeb300879e9
Cyril de Catheu <cyril@startree.ai>
2024-10-02 17:40:08 +0200

    [persistence] add indexes to create_time, end_time and child in anomaly table

---
+++ thirdeye-persistence/src/main/resources/db/migration/mysql/V1_331_0__add_indexes_to_anomaly.sql
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2023 StarTree Inc
+ *
+ * Licensed under the StarTree Community License (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at http://www.startree.ai/legal/startree-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT * WARRANTIES OF ANY KIND,
+ * either express or implied.
+ * See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+CREATE INDEX create_time_idx ON merged_anomaly_result_index (create_time);
+CREATE INDEX end_time_idx ON merged_anomaly_result_index (end_time);
+CREATE INDEX child_idx ON merged_anomaly_result_index (child);

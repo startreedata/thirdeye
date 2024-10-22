acc2142295447c3710d351f710f01e3ec93393fd
Cyril de Catheu <cyril@startree.ai>
2024-10-22 14:12:16 +0200

    [pinot] fix potential race condition for controller http client (#1610)
    * remove DI usage for connection provider
    
    * refactor
    
    * fix potential connection bug in race and interruption edge cases

--- thirdeye-plugins/thirdeye-pinot/src/main/java/org/apache/pinot/client/PinotConnectionUtils.java
+++ thirdeye-plugins/thirdeye-pinot/src/main/java/org/apache/pinot/client/PinotConnectionUtils.java
@@ -13,16 +13,29 @@
  */
 package org.apache.pinot.client;
 
+import static ai.startree.thirdeye.plugins.datasource.pinot.PinotThirdEyeDataSource.HTTPS_SCHEME;
 import static ai.startree.thirdeye.spi.util.SpiUtils.optional;
 import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Collections.singletonList;
 import static java.util.Objects.requireNonNull;
 
 import ai.startree.thirdeye.plugins.datasource.pinot.PinotThirdEyeDataSourceConfig;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 import javax.net.ssl.SSLContext;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.TrustStrategy;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,7 +44,29 @@ public class PinotConnectionUtils {
 
   private static final Logger LOG = LoggerFactory.getLogger(PinotConnectionUtils.class);
 
-  public static Connection createConnection(final PinotThirdEyeDataSourceConfig config, final Map<String, String> additionalHeaders) {
+  public static CloseableHttpClient createHttpClient(
+      final PinotThirdEyeDataSourceConfig config, final @NonNull Map<String, String> additionalHeaders) {
+    final HttpClientBuilder builder = HttpClients.custom();
+    
+    // set headers
+    final Map<String, String> mergedHeaders = new HashMap<>();
+    optional(config.getHeaders()).ifPresent(mergedHeaders::putAll);
+    mergedHeaders.putAll(additionalHeaders);
+    builder.setDefaultHeaders(mergedHeaders.entrySet()
+        .stream()
+        .map(e -> new BasicHeader(e.getKey(), e.getValue()))
+        .collect(Collectors.toList()));
+
+    // set ssl context if necessary
+    if (HTTPS_SCHEME.equals(config.getControllerConnectionScheme())) {
+      builder.setSSLContext(httpsSslContext()).setSSLHostnameVerifier(new NoopHostnameVerifier());
+    }
+
+    return builder.build();
+  }
+
+  public static Connection createConnection(final PinotThirdEyeDataSourceConfig config,
+      final @NonNull Map<String, String> additionalHeaders) {
     final String brokerUrl = config.getBrokerUrl();
     final PinotClientTransport transport = buildTransport(config, additionalHeaders);
 
@@ -54,7 +89,8 @@ public class PinotConnectionUtils {
   }
 
   private static PinotClientTransport buildTransport(
-      final PinotThirdEyeDataSourceConfig config, final @NonNull Map<String, String> additionalHeaders) {
+      final PinotThirdEyeDataSourceConfig config,
+      final @NonNull Map<String, String> additionalHeaders) {
     final ThirdEyeJsonAsyncHttpPinotClientTransportFactory factory =
         new ThirdEyeJsonAsyncHttpPinotClientTransportFactory();
 
@@ -89,5 +125,31 @@ public class PinotConnectionUtils {
         .ifPresent(factory::setBrokerResponseTimeoutMs);
 
     return factory.buildTransport();
+  }
+
+  // SSL context that accepts all SSL certificate. 
+  // Assume that the Pinot brokers are setup in the same internal network
+  private static SSLContext httpsSslContext() {
+    try {
+      return new SSLContextBuilder()
+          .loadTrustMaterial(null, new AcceptAllTrustStrategy())
+          .build();
+    } catch (final NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
+      // This section shouldn't happen because we use Accept All Strategy
+      LOG.error("Failed to generate SSL context for Pinot in https.");
+      throw new RuntimeException(e);
+    }
+  }
+
+  /**
+   * This class accepts (i.e., ignores) all SSL certificate.
+   */
+  private static class AcceptAllTrustStrategy implements TrustStrategy {
+
+    @Override
+    public boolean isTrusted(final X509Certificate[] x509Certificates, final String s)
+        throws CertificateException {
+      return true;
+    }
   }
 }

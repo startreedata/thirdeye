4f854673634b47333e27efb364952c09b06f408d
Cyril de Catheu <cyril@startree.ai>
2024-10-22 13:21:19 +0200

    refactorings

--- thirdeye-plugins/thirdeye-pinot/src/main/java/ai/startree/thirdeye/plugins/datasource/pinot/restclient/PinotControllerHttpClientProvider.java
+++ thirdeye-plugins/thirdeye-pinot/src/main/java/ai/startree/thirdeye/plugins/datasource/pinot/restclient/PinotControllerHttpClientProvider.java
@@ -13,36 +13,19 @@
  */
 package ai.startree.thirdeye.plugins.datasource.pinot.restclient;
 
-import static ai.startree.thirdeye.plugins.datasource.pinot.PinotThirdEyeDataSource.HTTPS_SCHEME;
-import static ai.startree.thirdeye.spi.util.SpiUtils.optional;
+import static ai.startree.thirdeye.spi.Constants.TWO_DIGITS_FORMATTER;
 import static java.util.Objects.requireNonNull;
 
 import ai.startree.thirdeye.plugins.datasource.pinot.PinotOauthUtils;
 import ai.startree.thirdeye.plugins.datasource.pinot.PinotThirdEyeDataSourceConfig;
 import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
-import javax.inject.Inject;
-import javax.inject.Singleton;
-import javax.net.ssl.SSLContext;
 import org.apache.http.HttpHeaders;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.apache.http.ssl.TrustStrategy;
+import org.apache.pinot.client.PinotConnectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -53,7 +36,7 @@ public class PinotControllerHttpClientProvider {
   
   private final PinotThirdEyeDataSourceConfig config;
   private CloseableHttpClient pinotControllerClient = null;
-  private String prevToken = null;
+  private String currentToken = null;
 
   public PinotControllerHttpClientProvider(final PinotThirdEyeDataSourceConfig config) {
     this.config = config;
@@ -63,69 +46,42 @@ public class PinotControllerHttpClientProvider {
     if (config.isOAuthEnabled()) {
       // fixme cyril - at every call this reads a file 
       final String newToken = requireNonNull(PinotOauthUtils.getOauthToken(config.getOauth()), "token supplied is null");
-      if (pinotControllerClient == null || !Objects.equals(prevToken, newToken)) {
-        // close former client asynchronously
-        executorService.submit(() -> this.closeClient(pinotControllerClient));
-        pinotControllerClient = buildPinotControllerClient();
+      if (pinotControllerClient == null || !Objects.equals(currentToken, newToken)) {
+        // need to update the authorization token 
+        // closing old connection is a lower priority - do it async
+        closeClientAsync(pinotControllerClient);
+        
+        currentToken = newToken;
+        final Map<String, String> additionalHeaders = Map.of(HttpHeaders.AUTHORIZATION, currentToken);
+        pinotControllerClient = PinotConnectionUtils.createHttpClient(config, additionalHeaders);
       }
     } else {
       if (pinotControllerClient == null) {
-        pinotControllerClient = buildPinotControllerClient();
+        pinotControllerClient = PinotConnectionUtils.createHttpClient(config, Map.of());
       }
     }
     
     return pinotControllerClient;
   }
 
+  private void closeClientAsync(final CloseableHttpClient pinotControllerClient) {
+    if (pinotControllerClient != null) {
+      executorService.submit(() -> this.closeClient(pinotControllerClient)); 
+    }
+  }
+
   private void closeClient(final CloseableHttpClient client) {
-    if (client != null) {
-      try {
+    try {
+      final long startTime = System.nanoTime();
+      if (client != null) {
         client.close();
-      } catch (IOException e) {
-        LOG.error("Failed to close pinot controller client", e);
-      }  
-    }
-  }
-
-  private CloseableHttpClient buildPinotControllerClient() {
-    final HttpClientBuilder builder = HttpClients.custom();
-    configureHeaders(builder);
-    configureHttps(builder);
-
-    return builder.build();
-  }
-
-  private void configureHeaders(final HttpClientBuilder builder) {
-    final Map<String, String> headers = new HashMap<>(
-        optional(config.getHeaders()).orElse(Collections.emptyMap()));
-    if (config.isOAuthEnabled()) {
-      final String value = PinotOauthUtils.getOauthToken(config.getOauth());
-      headers.put(HttpHeaders.AUTHORIZATION, value);
-      prevToken = value;
-    }
-    if (!headers.isEmpty()) {
-      builder.setDefaultHeaders(headers.entrySet()
-          .stream()
-          .map(e -> new BasicHeader(e.getKey(), e.getValue()))
-          .collect(Collectors.toList()));
-    }
-  }
-
-  private void configureHttps(final HttpClientBuilder builder) {
-    if (HTTPS_SCHEME.equals(config.getControllerConnectionScheme())) {
-      try {
-        // Accept all SSL certificate because we assume that the Pinot broker are setup in the
-        // same internal network
-        final SSLContext sslContext = new SSLContextBuilder()
-            .loadTrustMaterial(null, new AcceptAllTrustStrategy())
-            .build();
-        builder.setSSLContext(sslContext)
-            .setSSLHostnameVerifier(new NoopHostnameVerifier());
-      } catch (final NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
-        // This section shouldn't happen because we use Accept All Strategy
-        LOG.error("Failed to start auto onboard for Pinot data source.");
-        throw new RuntimeException(e);
+        LOG.info("Successfully closed pinot controller client. took {}ms",
+            TWO_DIGITS_FORMATTER.format((System.nanoTime() - startTime) / 1e6));
       }
+    } catch (final IOException e) {
+      LOG.warn("Failed to close pinot controller client", e);
+    } catch (final Exception e) {
+      LOG.error("Failed to close pinot controller client", e);
     }
   }
 
@@ -136,18 +92,6 @@ public class PinotControllerHttpClientProvider {
       } catch (IOException e) {
         LOG.error("Exception closing pinotControllerClient", e);
       }
-    }
-  }
-
-  /**
-   * This class accepts (i.e., ignores) all SSL certificate.
-   */
-  private static class AcceptAllTrustStrategy implements TrustStrategy {
-
-    @Override
-    public boolean isTrusted(final X509Certificate[] x509Certificates, final String s)
-        throws CertificateException {
-      return true;
     }
   }
 }

503fa69b715d38c248b63ff9629563c8b6d3278c
Cyril de Catheu <cyril@startree.ai>
2024-12-13 11:46:10 +0100

    clean doc

--- thirdeye-worker/src/main/java/ai/startree/thirdeye/worker/task/TaskDriverRunnable.java
+++ thirdeye-worker/src/main/java/ai/startree/thirdeye/worker/task/TaskDriverRunnable.java
@@ -114,7 +114,8 @@ public class TaskDriverRunnable implements Runnable {
           .scheduleAtFixedRate(() -> taskExecutionHeartbeat(taskDTO),
               // a heartbeat is recorded at the same time the task is acquired 
               // assuming runTask is run just after the task is acquired 
-              // we can run the next heartbeat at the next heartbeat interval. Taking 1 millisecond of margin just in case  
+              // we can run the first next heartbeat with a delay of heartbeat interval. 
+              // Taking 1 millisecond of margin just in case to account for the delay between the task acquisition and the start of this schedule  
               config.getHeartbeatInterval().toMillis() - 1,
               config.getHeartbeatInterval().toMillis(),
               TimeUnit.MILLISECONDS);

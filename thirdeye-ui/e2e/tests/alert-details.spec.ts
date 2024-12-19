53723254f6692a5db5dacac0a4af960a3672da5b
Harshil Shah <harshil.shah@velotio.com>
2024-12-19 17:15:23 +0530

    fix(ui): Added Test cases for Alert Details and anomaly

---
+++ thirdeye-ui/e2e/tests/alert-details.spec.ts
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2024 StarTree Inc
+ *
+ * Licensed under the StarTree Community License (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at http://www.startree.ai/legal/startree-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT * WARRANTIES OF ANY KIND,
+ * either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+import { test } from "@playwright/test";
+import { AlertDetailsPage } from "../pages/alert-detail";
+
+test("View anomalies for alerts", async ({ page }) => {
+    const alertDetailsPage = new AlertDetailsPage(page);
+    await alertDetailsPage.goToAlertDetailsPage();
+    await alertDetailsPage.resolveApis();
+    await alertDetailsPage.checkHeader();
+    await alertDetailsPage.openFirstAlert();
+    await alertDetailsPage.checkAlertHeader();
+    await alertDetailsPage.openAnomalies();
+    await alertDetailsPage.resolveAnomaliesApis();
+    await alertDetailsPage.openAnomalieFromTable();
+    await alertDetailsPage.assertPageComponents();
+});
+
+test("Active and Deactivate alerts", async ({ page }) => {
+    const alertDetailsPage = new AlertDetailsPage(page);
+    await alertDetailsPage.goToAlertDetailsPage();
+    await alertDetailsPage.resolveApis();
+    await alertDetailsPage.checkHeader();
+    await alertDetailsPage.openFirstAlert();
+    await alertDetailsPage.checkAlertHeader();
+    await alertDetailsPage.checkAlertIsActiveOrDeactive(true);
+    await alertDetailsPage.activeDeactiveAlert(false);
+    await alertDetailsPage.resolveApis();
+    await alertDetailsPage.checkAlertIsActiveOrDeactive(false);
+    await alertDetailsPage.activeDeactiveAlert(true);
+    await alertDetailsPage.resolveApis();
+    await alertDetailsPage.checkAlertIsActiveOrDeactive(true);
+});

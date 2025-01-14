2ad7e58529d01584208aaea723acd3623281a0eb
Cyril de Catheu <cyril@startree.ai>
2025-01-14 15:58:19 +0100

    add recommended status code to ThirdEyeStatus

--- thirdeye-spi/src/main/java/ai/startree/thirdeye/spi/ThirdEyeStatus.java
+++ thirdeye-spi/src/main/java/ai/startree/thirdeye/spi/ThirdEyeStatus.java
@@ -15,63 +15,80 @@ package ai.startree.thirdeye.spi;
 
 public enum ThirdEyeStatus {
 
-  ERR_UNAUTHENTICATED("User authentication failed!"),
-  ERR_ALERT_PIPELINE_EXECUTION("Pipeline Failed! Error: %s"),
-  ERR_AUTH_SERVER_NOT_RESPONDING("Auth server is not responding. Auth Server URL : %s"),
-  ERR_CONFIG("Configuration Error! %s"),
-  ERR_CRON_INVALID("Failed to parse cron expression: %s"),
-  ERR_CRON_FREQUENCY_TOO_HIGH("Invalid cron: %s. This cron can trigger up to %s times per minute. This is not allowed. Please use a cron that is triggered at most %s times per minute."),
-  ERR_DATASET_NOT_FOUND("Dataset not found: %s. Dataset is not onboarded?"),
-  ERR_DATASET_NOT_FOUND_IN_NAMESPACE("Dataset not found: %s in namespace: %s. Dataset is not onboarded?"),
-  ERR_DATASOURCE_NOT_FOUND("Data Source not found: %s. Data Source is not created?"),
-  ERR_DATASOURCE_NOT_FOUND_IN_NAMESPACE("Data Source not found: %s in namespace %s. Data Source is not created?"),
-  ERR_DATASOURCE_NOT_LOADED("Data source cannot be instantiated! %s"),
-  ERR_DATASOURCE_VALIDATION_FAILED("Data source validation failed! name or id: %s. %s"),
-  ERR_DATA_UNAVAILABLE("Data not available! %s"),
-  ERR_DETECTION_INTERVAL_COMPUTATION("Failed to compute detection interval. Error caused by : %s"),
-  ERR_DUPLICATE_NAME("Please provide a unique name. '%s' already exists."),
-  ERR_DUPLICATE_ENTITY("Entity already exists. %s"),
-  ERR_ID_UNEXPECTED_AT_CREATION("ID should be null at creation time."),
-  ERR_INVALID_JSON_FORMAT("Invalid json format"),
-  ERR_INVALID_QUERY_PARAM_OPERATOR("Invalid operator for query param. Allowed Values:"),
-  ERR_MISSING_CONFIGURATION_FIELD("Missing configuration field in alert: %s"),
-  ERR_MISSING_ID("ID is null!"),
-  ERR_MISSING_NAME("name is null!"),
+  ERR_UNAUTHENTICATED(null,"User authentication failed!"),
+  ERR_ALERT_PIPELINE_EXECUTION(null,"Pipeline Failed! Error: %s"),
+  ERR_AUTH_SERVER_NOT_RESPONDING(null,"Auth server is not responding. Auth Server URL : %s"),
+  ERR_CONFIG(null,"Configuration Error! %s"),
+  ERR_CRON_INVALID(null,"Failed to parse cron expression: %s"),
+  ERR_CRON_FREQUENCY_TOO_HIGH(
+      null,"Invalid cron: %s. This cron can trigger up to %s times per minute. This is not allowed. Please use a cron that is triggered at most %s times per minute."),
+  ERR_DATASET_NOT_FOUND(null,"Dataset not found: %s. Dataset is not onboarded?"),
+  ERR_DATASET_NOT_FOUND_IN_NAMESPACE(
+      null,"Dataset not found: %s in namespace: %s. Dataset is not onboarded?"),
+  ERR_DATASOURCE_NOT_FOUND(null,"Data Source not found: %s. Data Source is not created?"),
+  ERR_DATASOURCE_NOT_FOUND_IN_NAMESPACE(
+      null,"Data Source not found: %s in namespace %s. Data Source is not created?"),
+  ERR_DATASOURCE_NOT_LOADED(null,"Data source cannot be instantiated! %s"),
+  ERR_DATASOURCE_VALIDATION_FAILED(null,"Data source validation failed! name or id: %s. %s"),
+  ERR_DATA_UNAVAILABLE(null,"Data not available! %s"),
+  ERR_DETECTION_INTERVAL_COMPUTATION(null,"Failed to compute detection interval. Error caused by : %s"),
+  ERR_DUPLICATE_NAME(null,"Please provide a unique name. '%s' already exists."),
+  ERR_DUPLICATE_ENTITY(null,"Entity already exists. %s"),
+  ERR_ID_UNEXPECTED_AT_CREATION(null,"ID should be null at creation time."),
+  ERR_INVALID_JSON_FORMAT(null,"Invalid json format"),
+  ERR_INVALID_QUERY_PARAM_OPERATOR(null,"Invalid operator for query param. Allowed Values:"),
+  ERR_MISSING_CONFIGURATION_FIELD(null,"Missing configuration field in alert: %s"),
+  ERR_MISSING_ID(null,"ID is null!"),
+  ERR_MISSING_NAME(null,"name is null!"),
   ERR_MULTIPLE_DATASETS_FOUND(
-      "Multiple datasets found based on the dataset's display name %s, candidates: %s"),
+      null,"Multiple datasets found based on the dataset's display name %s, candidates: %s"),
   ERR_MULTIPLE_DATASOURCES_FOUND(
-      "Multiple data sources found with the same name: %s"),
+      null,"Multiple data sources found with the same name: %s"),
   ERR_NAMESPACE_CONFIGURATION_VALIDATION_FAILED(
-      "Namespace Configuration validation failed! namespace: %s. %s"),
-  ERR_NOTIFICATION_DISPATCH("%s"),
-  ERR_NOT_ENOUGH_DATA_FOR_RCA("Not enough data for RCA algorithm: %s"),
-  ERR_OBJECT_DOES_NOT_EXIST("Object does not exist! %s"),
-  ERR_OBJECT_UNEXPECTED("Object should be null/empty! %s"),
-  ERR_OPERATION_UNSUPPORTED("Operation not supported. %s"),
+      null,"Namespace Configuration validation failed! namespace: %s. %s"),
+  ERR_NOTIFICATION_DISPATCH(null,"%s"),
+  ERR_NOT_ENOUGH_DATA_FOR_RCA(null,"Not enough data for RCA algorithm: %s"),
+  ERR_OBJECT_DOES_NOT_EXIST(null,"Object does not exist! %s"),
+  ERR_OBJECT_UNEXPECTED(null,"Object should be null/empty! %s"),
+  ERR_OPERATION_UNSUPPORTED(null,"Operation not supported. %s"),
   ERR_TEMPLATE_MISSING_PROPERTY(
-      "Failed to apply templateProperties to template. Missing property: %s"),
-  ERR_TIMEOUT("Operation timed out!"),
-  ERR_UNEXPECTED_QUERY_PARAM("Unexpected Query Param. Allowed values: %s"),
-  ERR_UNKNOWN("%s"),
-  ERR_EXECUTION_RCA_ALGORITHM("RCA Algorithm Execution Failed. Error: %s"),
-  ERR_UNKNOWN_RCA_ALGORITHM("Unknown error running the rca algorithm: %s"),
-  ERR_CALCITE_FILTERING("Failed running Calcite filtering query with filter: %s"),
-  ERR_INVALID_PARAMS_COMPONENTS("Invalid param components: %s for Class %s"),
-  ERR_INVALID_DETECTION_REGRESSORS("Invalid regressors configuration. Too few or too many regressors?"),
-  ERR_INVALID_SQL("Invalid SQL:\n%s"),
-  ERR_NEGATIVE_LIMIT_VALUE("Negative 'limit' value provided."),
-  ERR_NEGATIVE_OFFSET_VALUE("Negative 'offset' value provided."),
-  ERR_OFFSET_WITHOUT_LIMIT("'offset' value provided without 'limit' value."),
-  ERR_PINOT_QUERY_QUOTA_EXCEEDED("Pinot Query Quota Exceeded. Error: %s. SQL: %s"),
-  ERR_PINOT_QUERY_EXECUTION("Pinot Query Execution Failed. Error: %s. SQL: %s"),
+      null,"Failed to apply templateProperties to template. Missing property: %s"),
+  ERR_TIMEOUT(null,"Operation timed out!"),
+  ERR_UNEXPECTED_QUERY_PARAM(null,"Unexpected Query Param. Allowed values: %s"),
+  ERR_UNKNOWN(500, "%s"),
+  ERR_EXECUTION_RCA_ALGORITHM(null,"RCA Algorithm Execution Failed. Error: %s"),
+  ERR_UNKNOWN_RCA_ALGORITHM(null,"Unknown error running the rca algorithm: %s"),
+  ERR_CALCITE_FILTERING(null,"Failed running Calcite filtering query with filter: %s"),
+  ERR_INVALID_PARAMS_COMPONENTS(null,"Invalid param components: %s for Class %s"),
+  ERR_INVALID_DETECTION_REGRESSORS(
+      null,"Invalid regressors configuration. Too few or too many regressors?"),
+  ERR_INVALID_SQL(null,"Invalid SQL:\n%s"),
+  ERR_NEGATIVE_LIMIT_VALUE(null,"Negative 'limit' value provided."),
+  ERR_NEGATIVE_OFFSET_VALUE(null,"Negative 'offset' value provided."),
+  ERR_OFFSET_WITHOUT_LIMIT(null,"'offset' value provided without 'limit' value."),
+  ERR_PINOT_QUERY_QUOTA_EXCEEDED(null,"Pinot Query Quota Exceeded. Error: %s. SQL: %s"),
+  ERR_PINOT_QUERY_EXECUTION(null,"Pinot Query Execution Failed. Error: %s. SQL: %s"),
+  ERR_DATASOURCE_DEMO_TABLE_CREATION_CONFLICT_ERROR(409,"Failed to create demo table %s. Error: %s"),
+  ERR_DATASOURCE_DEMO_TABLE_CREATION_UNKNOWN_ERROR(500,"Failed to create demo table %s. Error: %s"),
 
-  OK("OK"),
+  OK(null,"OK"),
   ;
 
+  /**
+   * Plugins and module don't have to be aware of the server context when they return exception with particular ThirdEyeStatus.  
+   * It is the responsibility of the server to translate the ThirdEyeExceptions in HTTP exception based on the ThirdEyeStatus.  
+   * This is optional and the server may decide to return another type of exception/status code based on its context.
+   */
+  final Integer recommendedStatusCode;
   final String message;
 
-  ThirdEyeStatus(final String message) {
+  ThirdEyeStatus(final Integer recommendedStatusCode, final String message) {
+    this.recommendedStatusCode = recommendedStatusCode;
     this.message = message;
+  }
+
+  public Integer getRecommendedStatusCode() {
+    return recommendedStatusCode;
   }
 
   public String getMessage() {
